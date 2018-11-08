2be27d89398176bff58a96488be2a46ad181389b
Sun Jian <921465802@qq.com>
2018-11-08 16:53:37 +0800

    add conf file for start

--- src/sqle/cmd/main.go
+++ src/sqle/cmd/main.go
@@ -7,10 +7,7 @@ import (
 	"sqle"
 	"sqle/api"
 	"sqle/model"
-)
-
-const (
-	PID_FILE = "sqle.pid"
+	"sqle/utils"
 )
 
 var version string
@@ -21,6 +18,8 @@ var mysqlPass string
 var mysqlHost string
 var mysqlPort string
 var mysqlSchema string
+var configPath string
+var pidFile string
 
 func main() {
 	var rootCmd = &cobra.Command{
@@ -35,13 +34,14 @@ func main() {
 			}
 		},
 	}
-	rootCmd.PersistentFlags().StringVarP(&user, "user", "u", "actiontech-universe", "run uagent by which user")
-	rootCmd.PersistentFlags().StringVarP(&mysqlUser, "mysql_user", "", "", "mysql user")
-	rootCmd.PersistentFlags().StringVarP(&mysqlPass, "mysql_password", "", "", "mysql password")
-	rootCmd.PersistentFlags().StringVarP(&mysqlHost, "mysql_host", "", "localhost", "mysql host")
-	rootCmd.PersistentFlags().StringVarP(&mysqlPort, "mysql_port", "", "3306", "mysql port")
-	rootCmd.PersistentFlags().StringVarP(&mysqlSchema, "mysql_schema", "", "sqle", "mysql schema")
 	rootCmd.PersistentFlags().IntVarP(&port, "port", "p", 5799, "http server port")
+	rootCmd.PersistentFlags().StringVarP(&mysqlUser, "mysql-user", "", "", "mysql user")
+	rootCmd.PersistentFlags().StringVarP(&mysqlPass, "mysql-password", "", "", "mysql password")
+	rootCmd.PersistentFlags().StringVarP(&mysqlHost, "mysql-host", "", "localhost", "mysql host")
+	rootCmd.PersistentFlags().StringVarP(&mysqlPort, "mysql-port", "", "3306", "mysql port")
+	rootCmd.PersistentFlags().StringVarP(&mysqlSchema, "mysql-schema", "", "sqle", "mysql schema")
+	rootCmd.PersistentFlags().StringVarP(&configPath, "config", "f", "", "config file path")
+	rootCmd.PersistentFlags().StringVarP(&pidFile, "pidfile", "", "", "config file path")
 	//rootCmd.SetHelpTemplate(ucobra.HELP_TEMPLATE)
 
 	var docsCmd = &cobra.Command{
@@ -58,21 +58,32 @@ func main() {
 }
 
 func run(cmd *cobra.Command, _ []string) error {
-	//stage := log.NewStage().Enter("run")
-	//go ubootstrap.DumpLoop()
 
-	//if err := ubootstrap.ChangeRunUser(user, true); nil != err {
-	//	return err
-	//}
+	// if conf path is exist, load option from conf
+	if configPath != "" {
+		conf, err := utils.LoadIniConf(configPath)
+		if err != nil {
+			return fmt.Errorf("load config path: %s failed", configPath)
+		}
+		mysqlUser = conf.GetString("server", "mysql_user", "")
+		mysqlPass = conf.GetString("server", "mysql_password", "")
+		mysqlHost = conf.GetString("server", "mysql_host", "")
+		mysqlPort = conf.GetString("server", "mysql_port", "")
+		mysqlSchema = conf.GetString("server", "mysql_schema", "")
+		port = conf.GetInt("server", "port", 12160)
+	}
 
-	//if err := ubootstrap.StartPid(PID_FILE); nil != err {
-	//	return err
-	//}
-	//defer ubootstrap.StopPid(PID_FILE)
-
-	//killChan := ubootstrap.ListenKillSignal()
-	//
-	//log.InitFileLoggerWithHouseKeep(100, 1024, user, true)
+	if pidFile != "" {
+		f, err := os.Create(pidFile)
+		if err != nil {
+			return err
+		}
+		fmt.Fprintf(f, "%d\n", os.Getpid())
+		f.Close()
+		defer func() {
+			os.Remove(pidFile)
+		}()
+	}
 
 	s, err := model.NewMysql(mysqlUser, mysqlPass, mysqlHost, mysqlPort, mysqlSchema)
 	if err != nil {
@@ -88,10 +99,10 @@ func run(cmd *cobra.Command, _ []string) error {
 	case <-exitChan:
 		//log.UserInfo(stage, "Beego exit unexpectly")
 		//case sig := <-killChan:
-
+		//
 		//case syscall.SIGUSR2:
 		//doesn't support graceful shutdown because beego uses its own graceful-way
-
+		//
 		//os.HaltIfShutdown(stage)
 		//log.UserInfo(stage, "Exit by signal %v", sig)
 	}
