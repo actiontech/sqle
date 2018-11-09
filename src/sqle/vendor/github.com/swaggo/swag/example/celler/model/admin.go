59c6b9392d3b10e2fd24325d9cd7be479c701c10
Sun Jian <921465802@qq.com>
2018-11-09 11:24:00 +0800

    change project dir

--- src/sqle/vendor/github.com/swaggo/swag/example/celler/model/admin.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type Admin struct {
-	ID   int    `json:"id" example:"1"`
-	Name string `json:"name" example:"admin name"`
-}
--- src/sqle/vendor/github.com/swaggo/swag/version.go
+++
@@ -1,4 +0,0 @@
-package swag
-
-// Version of swag
-const Version = "v1.4.0"
