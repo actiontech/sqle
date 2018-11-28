d19a7bfd2b7294857e787a54a4c2a08e2b4efb75
Sun Jian <921465802@qq.com>
2018-11-28 19:25:37 +0800

    fix #12

--- api/app.go
+++ api/app.go
@@ -41,7 +41,7 @@ func StartApi(port int, exitChan chan struct{}) {
 	e.GET("/tasks", controller.GetTasks)
 	e.POST("/tasks", controller.CreateTask)
 	e.GET("/tasks/:task_id/", controller.GetTask)
-	e.POST("/tasks/:task_id/", controller.DeleteTask)
+	e.DELETE("/tasks/:task_id/", controller.DeleteTask)
 	e.POST("/tasks/:task_id/inspection", controller.InspectTask)
 	e.POST("/tasks/:task_id/commit", controller.CommitTask)
 	e.POST("/tasks/:task_id/rollback", controller.RollbackTask)
