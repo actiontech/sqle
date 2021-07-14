7b36efa361d08c2895eefce3fa2c50777dc3b47c
deanglc <deanforgh@outlook.com>
2021-07-14 11:59:10 +0800

    add todo

--- sqle/model/rule_template_rule.go
+++ sqle/model/rule_template_rule.go
@@ -10,15 +10,16 @@ import (
 type RuleTemplateRule struct {
 	gorm.JoinTableHandler
 	RuleTemplateId uint   `json:"rule_template_id" gorm:"column:rule_template_id;primary_key;auto_increment:false"`
-	RuleName       string `json:"name" gorm:"column:rule_name;primary_key;auto_increment:false"`
-	RuleLevel      string `json:"level" gorm:"column:level; not null"`
-	RuleValue      string `json:"value" gorm:"column:value; not null" `
+	RuleName       string `json:"name" gorm:"column:rule_name;primary_key;"`
+	RuleLevel      string `json:"level" gorm:"column:level;"`
+	RuleValue      string `json:"value" gorm:"column:value;" `
 }
 
 func (rtr *RuleTemplateRule) TableName() string {
 	return "rule_template_rule"
 }
 
+// todo 增改时填充 来自rules表的level、value的默认值 如不需要可移除
 func (rtr *RuleTemplateRule) Add(_ gorm.JoinTableHandlerInterface, db *gorm.DB, foreignValue interface{}, associationValue interface{}) error {
 	association := db.NewScope(associationValue)
 	associationPrimaryKey := fmt.Sprint(association.PrimaryKeyValue())
@@ -38,7 +39,7 @@ func (rtr *RuleTemplateRule) Add(_ gorm.JoinTableHandlerInterface, db *gorm.DB, 
 	return db.Where(*ruleTR).Assign(attrMap).FirstOrCreate(ruleTR).Error
 }
 
-// 完成连接表的属性更新
+// 完成连接表的level value字段更新
 func (s *Storage) AfterUpdateRuleTemplateRules(tpl *RuleTemplate, rules ...RuleTemplateRule) error {
 	err := s.db.Model(tpl).Association("RTR").Replace(rules).Error
 	return errors.New(errors.CONNECT_STORAGE_ERROR, err)
