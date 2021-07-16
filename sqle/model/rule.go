ad82aa2178ee4e75e6897a0f35a4231b46839f70
deanglc <deanforgh@outlook.com>
2021-07-16 18:24:48 +0800

    add orderFunc

--- sqle/model/rule.go
+++ sqle/model/rule.go
@@ -74,7 +74,13 @@ func (s *Storage) GetRuleTemplateByName(name string) (*RuleTemplate, bool, error
 
 func (s *Storage) GetRuleTemplateDetailByName(name string) (*RuleTemplate, bool, error) {
 	t := &RuleTemplate{Name: name}
-	err := s.db.Preload("RuleList").Preload("RuleList.Rule").Preload("Instances").Where(t).First(t).Error
+	orderFunc := func(db *gorm.DB) *gorm.DB {
+		return db.Order("rule_template_rule.rule_name ASC")
+	}
+	err := s.db.Preload("RuleList", orderFunc).
+		Preload("RuleList.Rule").
+		Preload("Instances").
+		Where(t).First(t).Error
 	if err == gorm.ErrRecordNotFound {
 		return t, false, nil
 	}
