97ca3c4c72b7c066dd2ced68f37e942ddd306963
drivebyer <wuyangmuc@163.com>
2021-09-08 15:20:15 +0800

    scanner: add interface to abstract scanner common oparation

---
+++ sqle/cmd/scanner/scanners/scanner.go
@@ -0,0 +1,17 @@
+package scanners
+
+import (
+	"context"
+
+	"actiontech.cloud/sqle/sqle/sqle/model"
+)
+
+// Scanner is a interface for all Scanners.
+type Scanner interface {
+	// Run start scanner. It parse SQLs and sends
+	// it to the channel until ctx is canceled.
+	Run(ctx context.Context) error
+
+	// SQLs returns channel that should be read until it is closed.
+	SQLs() (chan *model.AuditPlanSQL, error)
+}
