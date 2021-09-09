4deca7b487701c0c7833796f7bf4bf4ed7de0ae2
drivebyer <wuyangmuc@163.com>
2021-09-09 17:35:13 +0800

    config: add ScannerType

--- sqle/cmd/scannerd/config/config.go
+++ sqle/cmd/scannerd/config/config.go
@@ -1,10 +1,30 @@
 package config
 
+import "fmt"
+
+type ScannerType int32
+
+const (
+	ScannerTypeSlowQuery ScannerType = 0
+	ScannerTypeMyBatis   ScannerType = 1
+)
+
+func (t ScannerType) String() string {
+	switch t {
+	case ScannerTypeMyBatis:
+		return "MyBatis"
+	case ScannerTypeSlowQuery:
+		return "SlowQuery"
+	default:
+		return fmt.Sprintf("%d", int(t))
+	}
+}
+
 type Config struct {
 	Host          string
 	Port          string
 	Dir           string
 	AuditPlanName string
-	Typ           string
 	Token         string
+	Typ           ScannerType
 }
