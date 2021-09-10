27fc618e74685364a53d23946f98c347f9dfa8a1
drivebyer <wuyangmuc@163.com>
2021-09-10 17:00:21 +0800

    scanner: adjust to scanner sub command

--- sqle/cmd/scannerd/scannerd.go
+++ sqle/cmd/scannerd/scannerd.go
@@ -5,7 +5,7 @@ import (
 	"os"
 
 	"actiontech.cloud/sqle/sqle/sqle/cmd/scannerd/config"
-	"actiontech.cloud/sqle/sqle/sqle/cmd/scannerd/scanners/mybatis"
+	"actiontech.cloud/sqle/sqle/sqle/cmd/scannerd/scanners/supervisor"
 
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
@@ -19,6 +19,12 @@ var (
 	typ           string
 	auditPlanName string
 	token         string
+
+	databaseHost    string
+	databasePort    string
+	databaseUser    string
+	databasePass    string
+	slowQuerySecond int
 )
 
 func main() {
@@ -26,20 +32,54 @@ func main() {
 		Use:   "SQLE Scanner",
 		Short: "SQLE Scanner",
 		Long:  "SQLE Scanner\nVersion:\n  " + "version",
+	}
+	rootCmd.PersistentFlags().StringVarP(&host, "host", "H", "127.0.0.1", "sqle host")
+	rootCmd.PersistentFlags().StringVarP(&port, "port", "P", "10000", "sqle port")
+	rootCmd.PersistentFlags().StringVarP(&auditPlanName, "name", "N", "", "audit plan name")
+	rootCmd.PersistentFlags().StringVarP(&token, "token", "A", "", "sqle token")
+	rootCmd.MarkPersistentFlagRequired("host")
+	rootCmd.MarkPersistentFlagRequired("port")
+	rootCmd.MarkPersistentFlagRequired("name")
+	rootCmd.MarkPersistentFlagRequired("token")
+
+	mybatisCmd := &cobra.Command{
+		Use:   "mybatis",
+		Short: "Parse MyBatis xml file",
+		Long:  "",
 		Run: func(cmd *cobra.Command, args []string) {
-			if err := run(cmd, args); err != nil {
-				fmt.Println(err.Error())
-				os.Exit(1)
+			err := run(cmd, args, config.ScannerTypeSlowQuery)
+			if err != nil {
+				fmt.Println(color.RedString("Error: %v", err))
 			}
 		},
 	}
+	mybatisCmd.Flags().StringVarP(&dir, "dir", "D", "", "xml directory")
+	mybatisCmd.MarkFlagRequired("dir")
+	rootCmd.AddCommand(mybatisCmd)
 
-	rootCmd.Flags().StringVarP(&host, "host", "H", "127.0.0.1", "sqle host")
-	rootCmd.Flags().StringVarP(&port, "port", "P", "10000", "sqle port")
-	rootCmd.Flags().StringVarP(&dir, "dir", "D", "", "xml directory")
-	rootCmd.Flags().StringVarP(&auditPlanName, "name", "N", "", "audit plan name")
-	rootCmd.Flags().StringVarP(&typ, "typ", "T", "", "scanner type")
-	rootCmd.Flags().StringVarP(&token, "token", "A", "", "sqle token")
+	slowlogCmd := &cobra.Command{
+		Use:   "slowquery",
+		Short: "Parse slow query",
+		Long:  "",
+		Run: func(cmd *cobra.Command, args []string) {
+			err := run(cmd, args, config.ScannerTypeSlowQuery)
+			if err != nil {
+				fmt.Println(color.RedString("Error: %v", err))
+			}
+		},
+	}
+	slowlogCmd.Flags().StringVarP(&databaseHost, "dbHost", "", "127.0.0.1", "database host")
+	slowlogCmd.Flags().StringVarP(&databasePort, "dbPort", "", "3306", "database port")
+	slowlogCmd.Flags().StringVarP(&databaseUser, "dbUser", "", "", "database user")
+	// todo: need prompt, try https://github.com/manifoldco/promptui
+	slowlogCmd.Flags().StringVarP(&databasePass, "dbPass", "", "", "database password")
+	// default value keep consistent with MySQL long-query-time
+	slowlogCmd.Flags().IntVarP(&slowQuerySecond, "slow-query-second", "", 10, "slow query second")
+	slowlogCmd.MarkFlagRequired("dbHost")
+	slowlogCmd.MarkFlagRequired("dbPort")
+	slowlogCmd.MarkFlagRequired("dbUser")
+	slowlogCmd.MarkFlagRequired("dbPass")
+	rootCmd.AddCommand(slowlogCmd)
 
 	code := 0
 
@@ -55,19 +95,19 @@ func main() {
 	}
 }
 
-func run(_ *cobra.Command, _ []string) error {
+func run(_ *cobra.Command, _ []string, typ config.ScannerType) error {
 	cfg := &config.Config{
-		Host:          host,
-		Port:          port,
-		Dir:           dir,
-		Typ:           typ,
-		AuditPlanName: auditPlanName,
-		Token:         token,
+		Host:            host,
+		Port:            port,
+		Dir:             dir,
+		Typ:             typ,
+		AuditPlanName:   auditPlanName,
+		Token:           token,
+		ScannerDBHost:   databaseHost,
+		ScannerDBPort:   databasePort,
+		ScannerDBUser:   databaseUser,
+		ScannerDBPass:   databasePass,
+		SlowQuerySecond: slowQuerySecond,
 	}
-	switch cfg.Typ {
-	case "mybatis":
-		return mybatis.MybatisScanner(cfg)
-	default:
-		return nil
-	}
+	return supervisor.Start(cfg)
 }
