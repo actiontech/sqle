aad8f2871a3b2baa7ac50c259ae4561fe80101e9
drivebyer <wuyangmuc@163.com>
2021-09-13 13:36:15 +0800

    api: move JWT middleware to middleware package

---
+++ sqle/api/middleware/jwt.go
@@ -0,0 +1,24 @@
+package middleware
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+// JWTTokenAdapter is a `echo` middleware,　by rewriting the header, the jwt token support header
+// "Authorization: {token}" and "Authorization: Bearer {token}".
+func JWTTokenAdapter() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			auth := c.Request().Header.Get(echo.HeaderAuthorization)
+			if auth != "" && !strings.HasPrefix(auth, middleware.DefaultJWTConfig.AuthScheme) {
+				c.Request().Header.Set(echo.HeaderAuthorization,
+					fmt.Sprintf("%s %s", middleware.DefaultJWTConfig.AuthScheme, auth))
+			}
+			return next(c)
+		}
+	}
+}
