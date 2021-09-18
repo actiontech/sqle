82d5cbf3d24682579898e89725ec9a0ea573cd3e
drivebyer <wuyangmuc@163.com>
2021-09-18 10:36:53 +0800

    fix: ScannerVerifier should handle token with auth schema

--- sqle/api/middleware/jwt_test.go
+++ sqle/api/middleware/jwt_test.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -8,61 +9,63 @@ import (
 
 	"github.com/actiontech/sqle/sqle/utils"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAuditPlanVerifyAdapter(t *testing.T) {
+func TestScannerVerifier(t *testing.T) {
 	e := echo.New()
 
 	jwt := utils.NewJWT([]byte(utils.JWTSecret))
 	apName := "test_audit_plan"
+	testUser := "test_user"
 
 	h := func(c echo.Context) error {
 		return c.HTML(http.StatusOK, "hello, world")
 	}
 
-	mw := AuditPlanVerifyAdapter()
+	mw := ScannerVerifier()
 
-	{
-		token, err := jwt.CreateToken("test_user", time.Now().Add(1*time.Hour).Unix(), utils.WithAuditPlanName(apName))
-		assert.NoError(t, err)
-
+	newContextFunc := func(token, apName string) (echo.Context, *httptest.ResponseRecorder) {
 		req := httptest.NewRequest(http.MethodGet, "/:audit_plan_name/", nil)
 		req.Header.Set(echo.HeaderAuthorization, token)
 		res := httptest.NewRecorder()
 		ctx := e.NewContext(req, res)
 		ctx.SetParamNames("audit_plan_name")
-		ctx.SetParamValues("test_audit_plan_modified")
+		ctx.SetParamValues(apName)
+		return ctx, res
+	}
 
+	{
+		token, err := jwt.CreateToken(testUser, time.Now().Add(1*time.Hour).Unix(), utils.WithAuditPlanName(apName))
+		assert.NoError(t, err)
+		ctx, _ := newContextFunc(token, fmt.Sprintf("%s_modified", apName))
 		err = mw(h)(ctx)
 		assert.Contains(t, err.Error(), errAuditPlanMisMatch.Error())
 	}
 
 	{
-		token, err := jwt.CreateToken("test_user", time.Now().Add(1*time.Hour).Unix())
+		token, err := jwt.CreateToken(testUser, time.Now().Add(1*time.Hour).Unix())
 		assert.NoError(t, err)
-
-		req := httptest.NewRequest(http.MethodGet, "/:audit_plan_name/", nil)
-		req.Header.Set(echo.HeaderAuthorization, token)
-		res := httptest.NewRecorder()
-		ctx := e.NewContext(req, res)
-
+		ctx, _ := newContextFunc(token, apName)
 		err = mw(h)(ctx)
 		assert.Contains(t, err.Error(), "unknown token")
 	}
 
 	{
-		token, err := jwt.CreateToken("test_user", time.Now().Add(1*time.Hour).Unix(), utils.WithAuditPlanName(apName))
+		token, err := jwt.CreateToken(testUser, time.Now().Add(1*time.Hour).Unix(), utils.WithAuditPlanName(apName))
 		assert.NoError(t, err)
+		ctx, res := newContextFunc(token, apName)
+		err = mw(h)(ctx)
+		assert.NoError(t, err)
+		assert.Contains(t, string(res.Body.Bytes()), "hello, world")
+	}
 
-		req := httptest.NewRequest(http.MethodGet, "/:audit_plan_name/", nil)
-		req.Header.Set(echo.HeaderAuthorization, token)
-		res := httptest.NewRecorder()
-		ctx := e.NewContext(req, res)
-		ctx.SetPath("/:audit_plan_name/")
-		ctx.SetParamNames("audit_plan_name")
-		ctx.SetParamValues(apName)
-
+	{
+		token, err := jwt.CreateToken(testUser, time.Now().Add(1*time.Hour).Unix(), utils.WithAuditPlanName(apName))
+		tokenWithSchema := fmt.Sprintf("%s %s", middleware.DefaultJWTConfig.AuthScheme, token)
+		assert.NoError(t, err)
+		ctx, res := newContextFunc(tokenWithSchema, apName)
 		err = mw(h)(ctx)
 		assert.NoError(t, err)
 		assert.Contains(t, string(res.Body.Bytes()), "hello, world")
