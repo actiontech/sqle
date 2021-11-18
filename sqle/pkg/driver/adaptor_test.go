9673f61b7ac20d98f45448f387faabd331d96fd7
drivebyer <wuyangmuc@163.com>
2021-11-18 18:06:59 +0800

    fix: trim space around SQL for classifySQL

--- sqle/pkg/driver/adaptor_test.go
+++ sqle/pkg/driver/adaptor_test.go
@@ -3,10 +3,6 @@ package driver
 import (
 	"testing"
 
-	_ "github.com/denisenkom/go-mssqldb"
-	_ "github.com/go-sql-driver/mysql"
-	_ "github.com/jackc/pgx/v4"
-	_ "github.com/sijms/go-ora/v2"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -19,6 +15,7 @@ func Test_splitSQL(t *testing.T) {
 		{sqls: "select * from t1;select * from t1;", want: []string{"select * from t1;", "select * from t1;"}},
 		{sqls: "select * from t1;select * from t1;", want: []string{"select * from t1;", "select * from t1;"}},
 		{sqls: "select * from t1;select * from t1", want: []string{"select * from t1;", "select * from t1"}},
+		{sqls: "select * from t1; select * from t1", want: []string{"select * from t1;", "select * from t1"}},
 		{sqls: "select * from `t;1`;select * from `t;1`", want: []string{"select * from `t;1`;", "select * from `t;1`"}},
 		{sqls: "insert into t1(name) values ('hello;world');insert into t1(name) values ('hello;world')", want: []string{"insert into t1(name) values ('hello;world');", "insert into t1(name) values ('hello;world')"}},
 		{sqls: "insert into t1(name) values (\"hello;world\");insert into t1(name) values (\"hello;world\")", want: []string{"insert into t1(name) values (\"hello;world\");", "insert into t1(name) values (\"hello;world\")"}},
