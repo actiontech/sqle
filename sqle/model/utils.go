67deb8713f623b7634ff88c08f535b790916bd4a
Sun Jian <921465802@qq.com>
2021-12-03 15:56:55 +0800

    fix panic with auto test, project "existedRule" can't be used if not exist

--- sqle/model/utils.go
+++ sqle/model/utils.go
@@ -159,13 +159,20 @@ func (s *Storage) CreateRulesIfNotExist(rules map[string][]*driver.Rule) error {
 			}
 			// rule will be created or update if:
 			// 1. rule not exist;
-			// 2. rule no params in db, and has params in code.
-			existedRuleHasParams := existedRule.Params != nil && len(existedRule.Params.Params) > 0
-			ruleHasParams := len(rule.Params) > 0
-			if !exist || (!existedRuleHasParams && ruleHasParams) {
-				err = s.Save(GenerateRuleByDriverRule(rule, dbType))
+			if !exist {
+				err := s.Save(GenerateRuleByDriverRule(rule, dbType))
 				if err != nil {
 					return err
+				}
+			} else {
+				// 2. rule no params in db, and has params in code.
+				existedRuleHasParams := existedRule.Params != nil && len(existedRule.Params.Params) > 0
+				ruleHasParams := len(rule.Params) > 0
+				if !exist || (!existedRuleHasParams && ruleHasParams) {
+					err := s.Save(GenerateRuleByDriverRule(rule, dbType))
+					if err != nil {
+						return err
+					}
 				}
 			}
 		}
