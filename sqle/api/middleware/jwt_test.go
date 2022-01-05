2780ad0fe9a5771b9c497070a50c296bf74a5433
HuangWeiCen <37464327+HuangWeiCen@users.noreply.github.com>
2022-01-05 15:00:14 +0800

    adjustment unit test

--- sqle/api/middleware/jwt_test.go
+++ sqle/api/middleware/jwt_test.go
@@ -1,13 +1,18 @@
 package middleware
 
 import (
+	"database/sql/driver"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 	"time"
 
+	"github.com/actiontech/sqle/sqle/model"
 	"github.com/actiontech/sqle/sqle/utils"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/stretchr/testify/assert"
@@ -36,7 +41,7 @@ func TestScannerVerifier(t *testing.T) {
 		return ctx, res
 	}
 
-	{
+	{ // test audit plan name don't match the token
 		token, err := jwt.CreateToken(testUser, time.Now().Add(1*time.Hour).Unix(), utils.WithAuditPlanName(apName))
 		assert.NoError(t, err)
 		ctx, _ := newContextFunc(token, fmt.Sprintf("%s_modified", apName))
@@ -44,7 +49,7 @@ func TestScannerVerifier(t *testing.T) {
 		assert.Contains(t, err.Error(), errAuditPlanMisMatch.Error())
 	}
 
-	{
+	{ // test unknown token
 		token, err := jwt.CreateToken(testUser, time.Now().Add(1*time.Hour).Unix())
 		assert.NoError(t, err)
 		ctx, _ := newContextFunc(token, apName)
@@ -52,22 +57,90 @@ func TestScannerVerifier(t *testing.T) {
 		assert.Contains(t, err.Error(), "unknown token")
 	}
 
-	{
+	{ // test audit plan token incorrect
 		token, err := jwt.CreateToken(testUser, time.Now().Add(1*time.Hour).Unix(), utils.WithAuditPlanName(apName))
 		assert.NoError(t, err)
+
+		mockDB, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+		assert.NoError(t, err)
+		model.InitMockStorage(mockDB)
+		mock.ExpectQuery("SELECT * FROM `audit_plans` WHERE `audit_plans`.`deleted_at` IS NULL AND ((name = ?))").
+			WithArgs(apName).
+			WillReturnRows(sqlmock.NewRows([]string{"name", "token"}).AddRow(driver.Value(testUser), "test-token"))
+		mock.ExpectClose()
+
+		ctx, _ := newContextFunc(token, apName)
+		err = mw(h)(ctx)
+		assert.Contains(t, err.Error(), errAuditPlanTokenIncorrect.Error())
+
+		mockDB.Close()
+		err = mock.ExpectationsWereMet()
+		assert.NoError(t, err)
+	}
+
+	{ // test audit plan not found
+		token, err := jwt.CreateToken(testUser, time.Now().Add(1*time.Hour).Unix(), utils.WithAuditPlanName(apName))
+		assert.NoError(t, err)
+
+		mockDB, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+		mock.ExpectQuery("SELECT * FROM `audit_plans` WHERE `audit_plans`.`deleted_at` IS NULL AND ((name = ?))").
+			WithArgs(apName).
+			WillReturnError(gorm.ErrRecordNotFound)
+		assert.NoError(t, err)
+		model.InitMockStorage(mockDB)
+		mock.ExpectClose()
+
+		ctx, _ := newContextFunc(token, apName)
+		err = mw(h)(ctx)
+		assert.Contains(t, err.Error(), errAuditPlanNotFound.Error())
+
+		mockDB.Close()
+		err = mock.ExpectationsWereMet()
+		assert.NoError(t, err)
+	}
+
+	{ // test check success
+		token, err := jwt.CreateToken(testUser, time.Now().Add(1*time.Hour).Unix(), utils.WithAuditPlanName(apName))
+		assert.NoError(t, err)
+
+		mockDB, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+		assert.NoError(t, err)
+		model.InitMockStorage(mockDB)
+		mock.ExpectQuery("SELECT * FROM `audit_plans` WHERE `audit_plans`.`deleted_at` IS NULL AND ((name = ?))").
+			WithArgs(apName).
+			WillReturnRows(sqlmock.NewRows([]string{"name", "token"}).AddRow(testUser, token))
+		mock.ExpectClose()
+
 		ctx, res := newContextFunc(token, apName)
 		err = mw(h)(ctx)
 		assert.NoError(t, err)
 		assert.Contains(t, res.Body.String(), "hello, world")
+
+		mockDB.Close()
+		err = mock.ExpectationsWereMet()
+		assert.NoError(t, err)
 	}
 
-	{
+	{ // test default auth scheme success
 		token, err := jwt.CreateToken(testUser, time.Now().Add(1*time.Hour).Unix(), utils.WithAuditPlanName(apName))
-		tokenWithSchema := fmt.Sprintf("%s %s", middleware.DefaultJWTConfig.AuthScheme, token)
 		assert.NoError(t, err)
+
+		mockDB, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+		assert.NoError(t, err)
+		model.InitMockStorage(mockDB)
+		mock.ExpectQuery("SELECT * FROM `audit_plans` WHERE `audit_plans`.`deleted_at` IS NULL AND ((name = ?))").
+			WithArgs(apName).
+			WillReturnRows(sqlmock.NewRows([]string{"name", "token"}).AddRow(testUser, token))
+		mock.ExpectClose()
+
+		tokenWithSchema := fmt.Sprintf("%s %s", middleware.DefaultJWTConfig.AuthScheme, token)
 		ctx, res := newContextFunc(tokenWithSchema, apName)
 		err = mw(h)(ctx)
 		assert.NoError(t, err)
 		assert.Contains(t, res.Body.String(), "hello, world")
+
+		mockDB.Close()
+		err = mock.ExpectationsWereMet()
+		assert.NoError(t, err)
 	}
 }
