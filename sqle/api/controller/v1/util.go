94a55e485152acf92323a90e0baa51738cf1db3c
jessun <jessun.zhang@outlook.com>
2022-01-24 14:17:38 +0800

    Reafactor.

--- sqle/api/controller/v1/util.go
+++ sqle/api/controller/v1/util.go
@@ -1,10 +1,8 @@
 package v1
 
 import (
-	"github.com/actiontech/sqle/sqle/api/controller"
 	"github.com/actiontech/sqle/sqle/driver"
 	"github.com/actiontech/sqle/sqle/model"
-	"github.com/labstack/echo/v4"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
@@ -29,8 +27,4 @@ func newDriverWithoutAudit(l *logrus.Entry, inst *model.Instance, database strin
 	}
 
 	return driver.NewDriver(l, inst.DbType, cfg)
-}
-
-func JSONNewNotImplementedErr(c echo.Context) error {
-	return controller.JSONBaseErrorReq(c, errors.New("not implemented yet"))
 }
