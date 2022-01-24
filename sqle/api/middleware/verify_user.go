1e8aa6f62a22435e4b1a95d1cb43bdbcf5f3b15f
jessun <jessun.zhang@outlook.com>
2022-01-24 17:08:30 +0800

    Feat: Add middleware to check if user is disabled.

---
+++ sqle/api/middleware/verify_user.go
@@ -0,0 +1,28 @@
+package middleware
+
+import (
+	"github.com/actiontech/sqle/sqle/api/controller"
+	"github.com/actiontech/sqle/sqle/errors"
+	"github.com/actiontech/sqle/sqle/model"
+
+	"github.com/labstack/echo/v4"
+)
+
+func VerifyUserIsDisabled() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			userName := controller.GetUserName(c)
+			user, isExist, err := model.GetStorage().GetUserDetailByName(userName)
+			if err != nil {
+				return controller.JSONBaseErrorReq(c, err)
+			}
+			if !isExist {
+				return controller.JSONBaseErrorReq(c, errors.NewDataInvalidErr("user is not exist"))
+			}
+			if user.IsDisabled() {
+				return controller.JSONBaseErrorReq(c, errors.NewUserDisabledErr("current user is disabled."))
+			}
+			return next(c)
+		}
+	}
+}
