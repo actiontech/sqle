5483f0d15df809add3c36d14fa3efd17400ce7ec
jessun <jessun.zhang@outlook.com>
2022-02-09 14:01:41 +0800

    Feat: add definition of API GET:"/v1/operations".

---
+++ sqle/api/controller/v1/operation.go
@@ -0,0 +1,30 @@
+package v1
+
+import (
+	"github.com/actiontech/sqle/sqle/api/controller"
+
+	"github.com/labstack/echo/v4"
+)
+
+type GetOperationsResV1 struct {
+	controller.BaseRes
+	Data      []*OperationResV1 `json:"data"`
+	TotalNums uint              `json:"total_nums"`
+}
+
+type OperationResV1 struct {
+	Code uint   `json:"op_code"`
+	Desc string `json:"op_desc"`
+}
+
+// @Summary 获取权限动作列表
+// @Description get permission operations
+// @Id GetOperationsV1
+// @Tags operation
+// @Security ApiKeyAuth
+// @Success 200 {object} v1.GetOperationsResV1
+// @Router /v1/operations [get]
+func GetOperations(c echo.Context) error {
+	// TODO: implementation
+	return controller.JSONNewNotImplementedErr(c)
+}
