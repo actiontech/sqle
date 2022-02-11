3dbecca494dcb01a8ce920fb22d9d48942483173
Sun Jian <921465802@qq.com>
2022-02-11 17:22:14 +0800

    model refactor for audit plan report sql

--- sqle/model/utils.go
+++ sqle/model/utils.go
@@ -2,6 +2,8 @@ package model
 
 import (
 	"database/sql"
+	sqlDriver "database/sql/driver"
+	"encoding/json"
 	"fmt"
 	"strings"
 	"sync"
@@ -103,7 +105,7 @@ func (s *Storage) AutoMigrate() error {
 	err := s.db.AutoMigrate(
 		&AuditPlanReportSQLV2{},
 		&AuditPlanReportV2{},
-		&AuditPlanSQL{},
+		&AuditPlanSQLV2{},
 		&AuditPlan{},
 		&ExecuteSQL{},
 		&Instance{},
@@ -137,7 +139,8 @@ func (s *Storage) AutoMigrate() error {
 	if err != nil {
 		return errors.New(errors.ConnectStorageError, err)
 	}
-	err = s.db.Model(AuditPlanSQL{}).AddIndex("idx_audit_plan_sqls_audit_plan_id_fingerprint", "audit_plan_id", "fingerprint(255)").Error
+	err = s.db.Model(AuditPlanSQLV2{}).AddUniqueIndex("uniq_audit_plan_sqls_v2_audit_plan_id_fingerprint",
+		"audit_plan_id", "fingerprint(255)").Error
 	if err != nil {
 		return errors.New(errors.ConnectStorageError, err)
 	}
@@ -367,4 +370,37 @@ func (r *RowList) Scan(src interface{}) error {
 		}
 	}
 	return nil
+}
+
+type JSON json.RawMessage
+
+// Value impl sql.driver.Valuer interface
+func (j JSON) Value() (sqlDriver.Value, error) {
+	if len(j) == 0 {
+		return nil, nil
+	}
+	bytes, err := json.RawMessage(j).MarshalJSON()
+	return string(bytes), err
+}
+
+// Scan impl sql.Scanner interface
+func (j *JSON) Scan(value interface{}) error {
+	if value == nil {
+		*j = JSON("null")
+		return nil
+	}
+	var bytes []byte
+	switch v := value.(type) {
+	case []byte:
+		bytes = v
+	case string:
+		bytes = []byte(v)
+	default:
+		return fmt.Errorf("failed to unmarshal JSONB value: %s", value)
+	}
+
+	result := json.RawMessage{}
+	err := json.Unmarshal(bytes, &result)
+	*j = JSON(result)
+	return err
 }
