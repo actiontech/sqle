b9ae6c652374c66730e8b2fd7b1001d99af2c556
jessun <jessun.zhang@outlook.com>
2022-02-16 16:08:41 +0800

    Feat: Add permission check
    Feat:
    - Add permission to API GET:"/v1/instances"

--- sqle/model/instance_list.go
+++ sqle/model/instance_list.go
@@ -1,6 +1,13 @@
 package model
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/actiontech/sqle/sqle/api/request"
+	"github.com/actiontech/sqle/sqle/errors"
+	"github.com/actiontech/sqle/sqle/utils"
+)
 
 type InstanceDetail struct {
 	Name                 string         `json:"name"`
@@ -48,12 +55,7 @@ LEFT JOIN instance_rule_template AS inst_rt ON inst.id = inst_rt.instance_id
 LEFT JOIN rule_templates AS rt ON inst_rt.rule_template_id = rt.id AND rt.deleted_at IS NULL
 LEFT JOIN workflow_templates AS wt ON inst.workflow_template_id = wt.id AND wt.deleted_at IS NULL
 
-{{- if .check_user_can_access }}
-JOIN user_role AS ur ON roles.id = ur.role_id
-JOIN users ON ur.user_id = users.id AND users.id = :current_user_id
-{{- end }}
-WHERE
-inst.deleted_at IS NULL
+WHERE inst.deleted_at IS NULL
 
 {{- if .filter_instance_name }}
 AND inst.name = :filter_instance_name
@@ -86,16 +88,101 @@ AND rt.name = :filter_rule_template_name
 {{- if .filter_workflow_template_name }}
 AND wt.name = :filter_workflow_template_name
 {{- end }}
+
+{{- if .check_user_can_access }}
+
+AND inst.id IN ( {{ .available_instance_id_list_str }} )
+
+{{- end }}
+
 {{- end }}
 `
 
 func (s *Storage) GetInstancesByReq(data map[string]interface{}) (
 	result []*InstanceDetail, count uint64, err error) {
 
+	if request.IsNeedToCheckAccessByData(data) {
+
+		// get user id
+		var userID int
+		{
+			var exist bool
+			userID, exist = request.GetUserIDFromRequest(data)
+			if !exist {
+				err = errors.NewDataInvalidErr("user id not exist")
+				return result, 0, err
+			}
+		}
+
+		// get available instance id list
+		var availableInstanceIDs []uint
+		{
+			insts, err := s.GetUserAvailableInstancesByUserID(userID)
+			if err != nil {
+				return nil, 0, err
+			}
+			if len(insts) == 0 { // user has no access to any instance
+				return result, 0, nil
+			}
+			availableInstanceIDs = GetInstanceIDsFromInstances(insts)
+		}
+		data = request.UpdateDataWithAvailableInstanceIDList(data, availableInstanceIDs)
+	}
+
 	err = s.getListResult(instancesQueryBodyTpl, instancesQueryTpl, data, &result)
 	if err != nil {
 		return result, 0, err
 	}
+
 	count, err = s.getCountResult(instancesQueryBodyTpl, instancesCountTpl, data)
 	return result, count, err
+
+}
+
+func (s *Storage) GetUserAvailableInstancesByUserID(userID int) (
+	availabeInsts []*Instance, err error) {
+
+	roles, err := s.GetRolesByUserID(userID)
+	if err != nil {
+		return availabeInsts, err
+	}
+
+	if len(roles) == 0 { // user has no roles
+		return availabeInsts, nil
+	}
+
+	roleIDs := GetRoleIDsFromRoles(roles)
+
+	return s.GetInstancesByRoleIDs(roleIDs)
+
+}
+
+var instancesQueryByRoleIDs = `
+SELECT inst.id, inst.name, inst.db_type, inst.db_host, inst.db_port, inst.db_user, inst.desc 
+FROM instances AS inst
+LEFT JOIN instance_role AS ir ON inst.id = ir.instance_id
+LEFT JOIN roles ON ir.role_id = roles.id AND roles.deleted_at IS NULL
+WHERE roles.id in (%s)
+`
+
+func (s *Storage) GetInstancesByRoleIDs(roleIds []uint) (insts []*Instance, err error) {
+
+	if len(roleIds) == 0 {
+		return insts, nil
+	}
+
+	query := fmt.Sprintf(instancesQueryByRoleIDs, utils.JoinUintSliceToString(roleIds, ", "))
+	if err := s.db.Unscoped().Raw(query).Find(&insts).Error; err != nil {
+		return insts, err
+	}
+
+	return insts, err
+}
+
+func GetInstanceIDsFromInstances(insts []*Instance) (ids []uint) {
+	ids = make([]uint, len(insts))
+	for i := range insts {
+		ids[i] = insts[i].ID
+	}
+	return ids
 }
