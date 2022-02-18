48e983395cca9f58aa176c30334e2eb567ca7939
Sun Jian <921465802@qq.com>
2022-02-18 13:23:39 +0800

    fix spell

--- sqle/api/controller/v1/instance.go
+++ sqle/api/controller/v1/instance.go
@@ -125,7 +125,7 @@ func CreateInstance(c echo.Context) error {
 }
 
 // 1. admin user have all access to all instance
-// 2. non-admin user have access to instance which is binded to one of his roles
+// 2. non-admin user have access to instance which is bound to one of his roles
 func checkCurrentUserCanAccessInstance(c echo.Context, instance *model.Instance) error {
 	if controller.GetUserName(c) == model.DefaultAdminUser {
 		return nil
