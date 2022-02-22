26bd64d6d9d8036ef61b0094d3e9bbcc2e1d3806
jessun <jessun.zhang@outlook.com>
2022-02-22 10:47:59 +0800

    Fix: fix query bug of users

--- sqle/model/user_list.go
+++ sqle/model/user_list.go
@@ -1,22 +1,29 @@
 package model
 
 type UserDetail struct {
-	Id        int
-	Name      string `json:"login_name"`
-	Email     string
-	LoginType string  `json:"user_authentication_type"`
-	RoleNames RowList `json:"role_names"`
-	Stat      int     `json:"stat"`
+	Id             int
+	Name           string `json:"login_name"`
+	Email          string
+	LoginType      string  `json:"user_authentication_type"`
+	Stat           int     `json:"stat"`
+	RoleNames      RowList `json:"role_names"`
+	UserGroupNames RowList `json:"user_group_names"`
 }
 
 func (u *UserDetail) IsDisabled() bool {
 	return u.Stat == Disabled
 }
 
-var usersQueryTpl = `SELECT users.id, users.login_name, users.email, users.user_authentication_type, users.stat, GROUP_CONCAT(DISTINCT COALESCE(roles.name,'')) AS role_names
+var usersQueryTpl = `SELECT 
+users.id, users.login_name, users.email, 
+users.user_authentication_type, users.stat, 
+GROUP_CONCAT(DISTINCT COALESCE(roles.name,'')) AS role_names,
+GROUP_CONCAT(DISTINCT COALESCE(user_groups.name,'')) AS user_group_names
 FROM users 
 LEFT JOIN user_role ON users.id = user_role.user_id
 LEFT JOIN roles ON user_role.role_id = roles.id AND roles.deleted_at IS NULL
+LEFT JOIN user_group_users ON users.id = user_group_users.user_id
+LEFT JOIN user_groups ON user_group_users.user_group_id AND user_groups.deleted_at IS NULL
 WHERE
 users.id in (SELECT DISTINCT(users.id)
 
