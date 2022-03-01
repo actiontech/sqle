1e4dc0ba37763d589cb6d45e6ac52392d239aeb7
HuangWeiCen <1724659546@qq.com>
2022-03-01 10:37:00 +0800

    Optimized interface definition

---
+++ sqle/api/controller/v1/license_ce.go
@@ -0,0 +1,31 @@
+//go:build !enterprise
+// +build !enterprise
+
+package v1
+
+import (
+	e "errors"
+
+	"github.com/actiontech/sqle/sqle/api/controller"
+	"github.com/actiontech/sqle/sqle/errors"
+
+	"github.com/labstack/echo/v4"
+)
+
+var ErrNoLicenseRequired = errors.New(errors.ErrAccessDeniedError, e.New("sqle-ce no license required"))
+
+func getLicense(c echo.Context) error {
+	return controller.JSONBaseErrorReq(c, ErrNoLicenseRequired)
+}
+
+func getSQLELicenseInfo(c echo.Context) error {
+	return controller.JSONBaseErrorReq(c, ErrNoLicenseRequired)
+}
+
+func setLicense(c echo.Context) error {
+	return controller.JSONBaseErrorReq(c, ErrNoLicenseRequired)
+}
+
+func checkLicense(c echo.Context) error {
+	return controller.JSONBaseErrorReq(c, ErrNoLicenseRequired)
+}
