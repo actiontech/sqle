716486b2b1046a1f909ba933a39f280b4971654f
HuangWeiCen <1724659546@qq.com>
2022-03-10 18:05:51 +0800

    add proto util

---
+++ sqle/driver/proto/util.go
@@ -0,0 +1,37 @@
+package proto
+
+import (
+	"github.com/actiontech/sqle/sqle/pkg/params"
+)
+
+func ParamToProtoParam(p []*params.Param) []*Param {
+	pp := make([]*Param, len(p))
+	for i, v := range p {
+		if v == nil {
+			continue
+		}
+		pp[i] = &Param{
+			Key:   v.Key,
+			Value: v.Value,
+			Desc:  v.Desc,
+			Type:  string(v.Type),
+		}
+	}
+	return pp
+}
+
+func ProtoParamToParam(p []*Param) []*params.Param {
+	pp := make([]*params.Param, len(p))
+	for i, v := range p {
+		if v == nil {
+			continue
+		}
+		pp[i] = &params.Param{
+			Key:   v.Key,
+			Value: v.Value,
+			Desc:  v.Desc,
+			Type:  params.ParamType(v.Type),
+		}
+	}
+	return pp
+}
