26d21a401faf7bee779b8ed52bd5820b9d2f0412
HuangWeiCen <1724659546@qq.com>
2022-03-15 16:04:32 +0800

    ee definition

---
+++ sqle/api/controller/v1/configuration_ee.go
@@ -0,0 +1,20 @@
+//go:build enterprise
+// +build enterprise
+
+package v1
+
+import (
+	"github.com/labstack/echo/v4"
+)
+
+func testWeChatConfigurationV1(c echo.Context) error {
+	return nil
+}
+
+func updateWeChatConfigurationV1(c echo.Context) error {
+	return nil
+}
+
+func getWeChatConfiguration(c echo.Context) error {
+	return nil
+}
