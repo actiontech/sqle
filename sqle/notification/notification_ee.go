c2f6d83fc408b294350218443a74375a209246dd
HuangWeiCen <1724659546@qq.com>
2022-04-21 10:26:58 +0800

    issue 448 webhook

---
+++ sqle/notification/notification_ee.go
@@ -0,0 +1,52 @@
+//go:build enterprise
+// +build enterprise
+
+package notification
+
+import (
+	"bytes"
+	"net/http"
+	"strings"
+	"text/template"
+)
+
+func (n *AuditPlanNotifier) sendWebHook(notification Notification, webHookUrl, webHookTemplate string) error {
+	message, err := n.splicingWebHookMessage(webHookTemplate, notification)
+	if err != nil {
+		return err
+	}
+	return n.sendJsonReq(webHookUrl, message)
+}
+
+func (n *AuditPlanNotifier) splicingWebHookMessage(webHookTemplate string, notification Notification) (string, error) {
+	temp, err := template.New("webhook").Parse(webHookTemplate)
+	if err != nil {
+		return "", err
+	}
+	// Newline characters are not allowed in json message and need to be replaced with '\n' string
+	data := map[string]string{
+		"subject": strings.ReplaceAll(notification.NotificationSubject(), "\n", "\\n"),
+		"body":    strings.ReplaceAll(notification.NotificationBody(), "\n", "\\n"),
+	}
+	var buff bytes.Buffer
+	err = temp.Execute(&buff, data)
+	if err != nil {
+		return "", err
+	}
+
+	return buff.String(), err
+}
+
+func (n *AuditPlanNotifier) sendJsonReq(webHookUrl string, message string) error {
+
+	req, err := http.NewRequest(http.MethodPost, webHookUrl, bytes.NewBufferString(message))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := (&http.Client{}).Do(req)
+	if err != nil {
+		return err
+	}
+	return resp.Body.Close()
+}
