99b9ebbe2df59494c1b185f3b4f043380caa8f6c
HuangWeiCen <1724659546@qq.com>
2022-04-26 16:03:14 +0800

    issue 470 ee definition

---
+++ sqle/api/controller/v1/oauth2_ee.go
@@ -0,0 +1,20 @@
+//go:build enterprise
+// +build enterprise
+
+package v1
+
+import (
+	"github.com/labstack/echo/v4"
+)
+
+func oauth2Link(c echo.Context) error {
+	return nil
+}
+
+func oauth2Callback(c echo.Context) error {
+	return nil
+}
+
+func bindOauth2User(c echo.Context) error {
+	return nil
+}
