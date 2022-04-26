fe885328096ced5b1d0e14ca41a353c92d25b324
HuangWeiCen <1724659546@qq.com>
2022-04-26 13:59:14 +0800

    update api

--- sqle/api/controller/v1/oauth2.go
+++ sqle/api/controller/v1/oauth2.go
@@ -22,12 +22,21 @@ type BindOauth2UserReqV1 struct {
 	Oauth2UserID string `json:"oauth2_user_id" from:"oauth2_user_id" valid:"required"`
 }
 
+type BindOauth2UserResV1 struct {
+	controller.BaseRes
+	Data BindOauth2UserResDataV1 `json:"data"`
+}
+
+type BindOauth2UserResDataV1 struct {
+	Token string `json:"token"`
+}
+
 // @Summary 绑定 Oauth2 和 sqle用户
 // @Description bind Oauth2 user to sqle
 // @Id bindOauth2User
 // @Tags oauth2
 // @Param conf body v1.BindOauth2UserReqV1 true "bind oauth2 user req"
-// @Success 200 {object} controller.BaseRes
+// @Success 200 {object} v1.BindOauth2UserResDataV1
 // @router /v1/oauth2/user/bind [post]
 func BindOauth2User(c echo.Context) error {
 	return nil
