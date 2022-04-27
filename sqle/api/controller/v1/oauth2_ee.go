8b139bbd6f3e1407e3c6c9142c0c3fd2efff0d7a
HuangWeiCen <1724659546@qq.com>
2022-04-27 15:23:26 +0800

    callback accomplish

--- sqle/api/controller/v1/oauth2_ee.go
+++ sqle/api/controller/v1/oauth2_ee.go
@@ -4,9 +4,13 @@
 package v1
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strconv"
 
 	"github.com/actiontech/sqle/sqle/api/controller"
 	"github.com/actiontech/sqle/sqle/errors"
@@ -46,7 +50,7 @@ func generateOauth2Config(conf *model.Oauth2Configuration) *oauth2.Config {
 	return &oauth2.Config{
 		ClientID:     conf.ClientID,
 		ClientSecret: conf.ClientKey,
-		RedirectURL:  fmt.Sprintf("%v/v1/oauth2/callback", conf.ClientHost),
+		RedirectURL:  fmt.Sprintf("http://%v/v1/oauth2/callback", conf.ClientHost),
 		Scopes:       conf.GetScopes(),
 		Endpoint: oauth2.Endpoint{
 			AuthURL:  conf.ServerAuthUrl,
@@ -55,8 +59,103 @@ func generateOauth2Config(conf *model.Oauth2Configuration) *oauth2.Config {
 	}
 }
 
+type callbackRedirectData struct {
+	UserExist    bool
+	SqleToken    string
+	Oauth2UserId string
+	Error        string
+}
+
+func (c callbackRedirectData) generateQuery(uri string) string {
+	params := url.Values{}
+	params.Set("user_exist", strconv.FormatBool(c.UserExist))
+	if c.SqleToken != "" {
+		params.Set("sqle_token", c.SqleToken)
+	}
+	if c.Oauth2UserId != "" {
+		params.Set("oauth2_user_id", c.Oauth2UserId)
+	}
+	if c.Error != "" {
+		params.Set("error", c.Error)
+	}
+	return fmt.Sprintf("%v/user/bind?%v", uri, params.Encode())
+}
+
 func oauth2Callback(c echo.Context) error {
-	return nil
+	s := model.GetStorage()
+	oauth2C, _, err := s.GetOauth2Configuration()
+	if err != nil {
+		return controller.JSONBaseErrorReq(c, err)
+	}
+	// TODO sqle https should also support
+	uri := fmt.Sprintf("http://%v", oauth2C.ClientHost)
+	data := callbackRedirectData{}
+
+	// check callback request
+	state := c.QueryParam("state")
+	if state != oauthState {
+		data.Error = fmt.Sprintf("invalid state: %v", state)
+		return c.Redirect(http.StatusFound, data.generateQuery(uri))
+	}
+	code := c.QueryParam("code")
+	if code == "" {
+		data.Error = "code is nil"
+		return c.Redirect(http.StatusFound, data.generateQuery(uri))
+	}
+
+	// get oauth2 token
+	oauth2Token, err := generateOauth2Config(oauth2C).Exchange(context.Background(), code)
+	if err != nil {
+		data.Error = err.Error()
+		return c.Redirect(http.StatusFound, data.generateQuery(uri))
+	}
+
+	//get user id using oauth2 token
+	userID, err := getOauth2UserID(oauth2C, oauth2Token)
+	if err != nil {
+		data.Error = err.Error()
+		return c.Redirect(http.StatusFound, data.generateQuery(uri))
+	}
+	data.Oauth2UserId = userID
+
+	user, exist, err := s.GetUserByThirdPartyUserID(userID)
+	if err != nil {
+		data.Error = err.Error()
+		return c.Redirect(http.StatusFound, data.generateQuery(uri))
+	}
+	data.UserExist = exist
+
+	// the user has successfully logged in at the third party, and the token can be returned directly
+	if exist {
+		t, err := generateToken(user.Name)
+		if err != nil {
+			data.Error = err.Error()
+			return c.Redirect(http.StatusFound, data.generateQuery(uri))
+		}
+		data.SqleToken = t
+	}
+
+	return c.Redirect(http.StatusFound, data.generateQuery(uri))
+}
+
+func getOauth2UserID(conf *model.Oauth2Configuration, token *oauth2.Token) (userID string, err error) {
+	uri := fmt.Sprintf("%v?%v=%v", conf.ServerUserIdUrl, conf.AccessTokenTag, token.AccessToken)
+	resp, err := (&http.Client{}).Get(uri)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to get third-party user ID, unable to parse response")
+	}
+	data := map[string]interface{}{}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return "", fmt.Errorf("failed to get third-party user ID, unrecognized response format")
+	}
+	user, ok := data[conf.UserIdTag]
+	if !ok {
+		return "", fmt.Errorf("not found third-party user ID")
+	}
+	return fmt.Sprintf("%v", user), nil
 }
 
 func bindOauth2User(c echo.Context) error {
