f9c0cb7ccded8f5e258d858087f4251616322f06
HuangWeiCen <1724659546@qq.com>
2022-05-16 16:16:52 +0800

    issue 526 support for issuing sql

--- sqle/driver/rewrite_driver.go
+++ sqle/driver/rewrite_driver.go
@@ -5,32 +5,33 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-// SQLRewriteDriver is a SQL rewrite driver
-type SQLRewriteDriver interface {
+// SQLQueryDriver is a SQL rewrite and execute driver
+type SQLQueryDriver interface {
 	Rewrite(sql string, params params.Params) (newSql string, err error)
+	Query(sql string) ([]map[string]string, error)
 }
 
-// NewSQLRewriteDriver return a new instantiated SQLRewriteDriver.
-func NewSQLRewriteDriver(log *logrus.Entry, dbType string) (SQLRewriteDriver, error) {
+// NewSQLQueryDriver return a new instantiated SQLQueryDriver.
+func NewSQLQueryDriver(log *logrus.Entry, dbType string, cfg *DSN) (SQLQueryDriver, error) {
 	return nil, nil
 }
 
-type SQLRewriteType string
+type SQLQueryType string
 
 const (
-	SQLRewriteTypeQuery = "query" // rewrite query sql
+	SQLQueryTypeQuery = "query" // rewrite query sql
 )
 
 const (
-	SQLRewriteTypeKey = "sql-rewrite-type"
+	SQLQueryTypeKey = "sql-rewrite-type"
 )
 
 // rewrite query sql will perform limit processing on the basis of the original sql query result
-func GenerateQueryRewriteParams(limit, offset uint32) params.Params {
+func GenerateQueryQueryParams(limit, offset uint32) params.Params {
 	return params.Params{
 		&params.Param{
-			Key:   SQLRewriteTypeKey,
-			Value: SQLRewriteTypeQuery,
+			Key:   SQLQueryTypeKey,
+			Value: SQLQueryTypeQuery,
 		},
 	}
 }
