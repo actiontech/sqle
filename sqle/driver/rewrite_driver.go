50937bb3380f302341b63ee15d7238607757ec61
HuangWeiCen <1724659546@qq.com>
2022-05-16 18:01:13 +0800

    issue 526 optimize code

--- sqle/driver/rewrite_driver.go
+++ sqle/driver/rewrite_driver.go
@@ -1,37 +1,18 @@
 package driver
 
 import (
-	"github.com/actiontech/sqle/sqle/pkg/params"
 	"github.com/sirupsen/logrus"
 )
 
 // SQLQueryDriver is a SQL rewrite and execute driver
 type SQLQueryDriver interface {
-	Rewrite(sql string, params params.Params) (newSql string, err error)
-	Query(sql string) ([]map[string]string, error)
+	IsQuerySQL(sql string) (bool, error)
+	Repage(sql string, limit, offset uint32) (newSql string, err error)
+	// The data location in values should be consistent with that in column
+	Query(sql string, timeOutSecond uint32) (column []string, values [][]string, err error)
 }
 
 // NewSQLQueryDriver return a new instantiated SQLQueryDriver.
 func NewSQLQueryDriver(log *logrus.Entry, dbType string, cfg *DSN) (SQLQueryDriver, error) {
 	return nil, nil
-}
-
-type SQLRewriteType string
-
-const (
-	SQLRewriteTypeQuery = "query" // rewrite query sql
-)
-
-const (
-	SQLRewriteTypeKey = "sql-rewrite-type"
-)
-
-// rewrite query sql will perform limit processing on the basis of the original sql query result
-func GenerateQueryRewriteParams(limit, offset uint32) params.Params {
-	return params.Params{
-		&params.Param{
-			Key:   SQLRewriteTypeKey,
-			Value: SQLRewriteTypeQuery,
-		},
-	}
 }
