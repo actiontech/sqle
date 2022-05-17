95300cb64f5c33a616c5720821fe746bc4a787a3
luowei <ranshaodelengshui@gmail.com>
2022-05-17 11:18:21 +0800

    refactor operation.go to support edition ee

---
+++ sqle/model/operation_ce.go
@@ -0,0 +1,12 @@
+//go:build !enterprise
+// +build !enterprise
+
+package model
+
+func getConfigurableOperationCodeListForEE() []uint {
+	return []uint{}
+}
+
+func additionalOperationForEE(opCode uint) string {
+	return "未知动作"
+}
