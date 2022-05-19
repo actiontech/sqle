e9278cc98ce074d9631847a143e43376c2fbc2af
luowei <ranshaodelengshui@gmail.com>
2022-05-19 16:22:08 +0800

    implement get result

--- sqle/api/controller/v1/sql_query_ee.go
+++ sqle/api/controller/v1/sql_query_ee.go
@@ -23,6 +23,8 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+var errSqlQueryUserNoAccessToSql = errors.New(errors.DataNotExist, fmt.Errorf("current user has no access to this sql"))
+
 func prepareSQLQuery(c echo.Context) error {
 	instanceName := c.Param("instance_name")
 	req := new(PrepareSQLQueryReqV1)
@@ -129,7 +131,176 @@ func prepareSQLQuery(c echo.Context) error {
 }
 
 func getSQLResult(c echo.Context) error {
-	return nil
+	queryIdStr := c.Param("query_id")
+	queryId, err := strconv.Atoi(queryIdStr)
+	if err != nil {
+		return err
+	}
+	req := new(GetSQLResultReqV1)
+	if err := controller.BindAndValidateReq(c, req); err != nil {
+		return err
+	}
+
+	// get data from db
+	s := model.GetStorage()
+	singleSql, err := s.GetSqlQueryExecSqlByQueryId(queryId)
+	if err != nil {
+		return controller.JSONBaseErrorReq(c, err)
+	}
+
+	history, err := s.GetSqlQueryHistoryById(singleSql.SqlQueryHistoryId)
+	if err != nil {
+		return controller.JSONBaseErrorReq(c, err)
+	}
+
+	instanceId := strconv.FormatUint(uint64(history.InstanceId), 10)
+	instance, exist, err := s.GetInstanceById(instanceId)
+	if err != nil {
+		return controller.JSONBaseErrorReq(c, err)
+	}
+	if !exist {
+		return controller.JSONBaseErrorReq(c, errInstanceNoAccess)
+	}
+
+	// check user auth
+	user, err := controller.GetCurrentUser(c)
+	if err != nil {
+		return controller.JSONBaseErrorReq(c, err)
+	}
+
+	if user.Name != model.DefaultAdminUser {
+		exist, err = s.CheckUserHasOpToInstance(user, instance, []uint{model.OP_SQL_QUERY_QUERY})
+		if err != nil {
+			return controller.JSONBaseErrorReq(c, err)
+		}
+		if !exist {
+			return controller.JSONBaseErrorReq(c, errInstanceNoAccess)
+		}
+		if user.ID != history.CreateUserId {
+			return controller.JSONBaseErrorReq(c, errSqlQueryUserNoAccessToSql)
+		}
+	}
+
+	l := log.NewEntry().WithFields(logrus.Fields{
+		"user":          user.Name,
+		"host":          c.Request().Host,
+		"time":          time.Now(),
+		"instance_name": instance.Name,
+		"instance_addr": fmt.Sprintf("%v:%v", instance.Host, instance.Port),
+		"schema":        history.Schema,
+		"raw_sql":       singleSql.Sql,
+	})
+	l.Infoln("SQL Query begin")
+
+	// rewrite sql
+	queryDriver, err := driver.NewSQLQueryDriver(log.NewEntry(), instance.DbType, &driver.DSN{
+		Host:         instance.Host,
+		Port:         instance.Port,
+		User:         instance.User,
+		Password:     instance.Password,
+		DatabaseName: history.Schema,
+	})
+	if err != nil {
+		return controller.JSONBaseErrorReq(c, err)
+	}
+	limit := uint32(instance.SqlQueryConfig.MaxPreQueryRows)
+	if limit > req.PageSize {
+		limit = req.PageSize
+	}
+	offset := req.PageSize * (req.PageIndex - 1)
+	rewriteRes, err := queryDriver.QueryPrepare(context.TODO(), singleSql.Sql, &driver.QueryPrepareConf{
+		Limit:  limit,
+		Offset: offset,
+	})
+	if err != nil {
+		return controller.JSONBaseErrorReq(c, err)
+	}
+	if rewriteRes.ErrorType != driver.ErrorTypeNotError {
+		return controller.JSONBaseErrorReq(c, fmt.Errorf("the SQL[%s] is invalid: %w", singleSql.Sql, err))
+	}
+
+	startTime := time.Now()
+	singleSql.ExecStartAt = &startTime
+
+	// execute sql
+	queryRes, err := queryDriver.Query(context.TODO(), rewriteRes.NewSQL, &driver.QueryConf{TimeOutSecond: uint32(instance.SqlQueryConfig.QueryTimeoutSecond)})
+	if err != nil {
+		// update sql_query_execution_sqls table
+		singleSql.ExecResult = err.Error()
+		if err := s.Save(singleSql); err != nil {
+			log.Logger().Errorf("update result to sql_query_execution_sqls failed: %v", err)
+		}
+		return controller.JSONBaseErrorReq(c, err)
+	}
+	endTime := time.Now()
+	elapsedTime := endTime.Sub(startTime) / time.Millisecond // ms
+
+	l.WithFields(logrus.Fields{
+		"exec_start_time":   startTime,
+		"exec_sql":          rewriteRes.NewSQL,
+		"result_rows_count": len(queryRes.Rows),
+		"elapsed_time":      elapsedTime,
+	}).Infoln("SQL Query end")
+
+	// update sql_query_execution_sqls table
+	singleSql.ExecEndAt = &endTime
+	singleSql.ExecResult = "OK"
+	if err := s.Save(singleSql); err != nil {
+		return controller.JSONBaseErrorReq(c, fmt.Errorf("update result to sql_query_execution_sqls failed: %v", err))
+	}
+
+	// build response
+	data, err := buildSqlQueryRes(rewriteRes.NewSQL, int(req.PageIndex), int(limit), int(elapsedTime), queryRes)
+	if err != nil {
+		return controller.JSONBaseErrorReq(c, err)
+	}
+
+	return c.JSON(http.StatusOK, &GetSQLResultResV1{
+		BaseRes: controller.NewBaseReq(nil),
+		Data:    data,
+	})
+}
+
+func buildSqlQueryRes(sql string, pageIndex, pageSize, elapsedTime int, queryRes *driver.QueryResult) (GetSQLResultResDataV1, error) {
+	rows := make([]map[string]string, len(queryRes.Rows))
+	for i, r := range queryRes.Rows {
+		row := make(map[string]string)
+		for j, v := range r.Values {
+			column := queryRes.Column[j]
+			row[column.Value] = v.Value
+		}
+		rows[i] = row
+	}
+
+	head := make([]SQLResultItemHeadResV1, len(queryRes.Column))
+	for i, c := range queryRes.Column {
+		head[i].FieldName = c.Value
+	}
+
+	// calculate start-line
+	rowsCount := len(rows)
+	var startLine, endLine int
+	if rowsCount == 0 {
+		startLine = 0
+		endLine = 0
+	} else if rowsCount == pageSize {
+		startLine = pageSize*(pageIndex-1) + 1
+		endLine = pageSize * pageIndex
+	} else if rowsCount < pageSize {
+		startLine = pageSize*(pageIndex-1) + 1
+		endLine = pageSize*(pageIndex-1) + rowsCount
+	}
+
+	res := GetSQLResultResDataV1{
+		SQL:         sql,
+		StartLine:   startLine,
+		EndLine:     endLine,
+		CurrentPage: pageIndex,
+		ExecuteTime: elapsedTime,
+		Rows:        rows,
+		Head:        head,
+	}
+	return res, nil
 }
 
 func getSQLQueryHistory(c echo.Context) error {
