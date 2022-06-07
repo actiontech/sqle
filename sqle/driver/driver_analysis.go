5241a20b0fb9b343a8913a4fcd53f6751b87ad06
luowei <ranshaodelengshui@gmail.com>
2022-06-07 14:26:47 +0800

    define plugin apis

---
+++ sqle/driver/driver_analysis.go
@@ -0,0 +1,80 @@
+package driver
+
+import (
+	"context"
+
+	"github.com/actiontech/sqle/sqle/pkg/params"
+	"github.com/sirupsen/logrus"
+)
+
+// AnalysisDriver is a driver for SQL analysis and getting table metadata
+type AnalysisDriver interface {
+	ListTablesInSchema(ctx context.Context, conf *ListTablesInSchemaConf) (*ListTablesInSchemaResult, error)
+	GetTableMetas(ctx context.Context, conf *GetTableMetasConf) (*GetTableMetasResult, error)
+	Explain(ctx context.Context, conf *ExplainConf) (*ExplainResult, error)
+}
+
+type ListTablesInSchemaConf struct {
+	schema string
+}
+
+type Table struct {
+	Name string
+}
+
+type ListTablesInSchemaResult struct {
+	Tables []Table
+}
+
+// AnalysisInfoInTableFormat
+// the field Column represents the column name of a table
+// the field Rows represents the data of the table
+// their relationship is as follows
+/*
+	| Column[0]  | Column[1]  | Column[2]  |
+	| Rows[0][0] | Rows[0][1] | Rows[0][2] |
+	| Rows[1][0] | Rows[1][1] | Rows[1][2] |
+*/
+type AnalysisInfoInTableFormat struct {
+	Column params.Params
+	Rows   [][]string
+}
+
+type GetTableMetasConf struct {
+	Schema string
+	Tables []string
+}
+
+type ColumnsInfo struct {
+	AnalysisInfoInTableFormat
+}
+
+type IndexesInfo struct {
+	AnalysisInfoInTableFormat
+}
+
+type GetTableMetaItem struct {
+	ColumnsInfo    ColumnsInfo
+	IndexesInfo    IndexesInfo
+	CreateTableSQL string
+}
+
+type GetTableMetasResult struct {
+	TableMetas map[string] /* table name */ GetTableMetaItem
+}
+
+type ExplainConf struct {
+	sql string
+}
+
+type ExplainClassicResult struct {
+	AnalysisInfoInTableFormat
+}
+
+type ExplainResult struct {
+	ClassicResult ExplainClassicResult
+}
+
+func NewAnalysisDriver(log *logrus.Entry, dbType string, cfg *DSN) (AnalysisDriver, error) {
+	return nil, nil
+}
