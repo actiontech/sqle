6e8b2dffb8a7c3c94e249ddb851404e4d7e324a7
taolx0 <taolx25@foxmail.com>
2022-06-09 14:14:53 +0800

    refactor new DSN

--- sqle/api/controller/v1/util.go
+++ sqle/api/controller/v1/util.go
@@ -12,14 +12,9 @@ func newDriverWithoutAudit(l *logrus.Entry, inst *model.Instance, database strin
 		return nil, errors.Errorf("instance is nil")
 	}
 
-	dsn := &driver.DSN{
-		Host:             inst.Host,
-		Port:             inst.Port,
-		User:             inst.User,
-		Password:         inst.Password,
-		AdditionalParams: inst.AdditionalParams,
-
-		DatabaseName: database,
+	dsn, err := newDSN(inst, database)
+	if err != nil {
+		return nil, errors.Wrap(err, "new dsn")
 	}
 
 	cfg, err := driver.NewConfig(dsn, nil)
