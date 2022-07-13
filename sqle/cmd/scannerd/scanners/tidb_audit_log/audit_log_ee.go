2e3dd57fcb2a5e78614f1a94c2802646d4519df6
HuangWeiCen <1724659546@qq.com>
2022-07-13 18:56:48 +0800

    ee_301 audit log Acquisition logic

---
+++ sqle/cmd/scannerd/scanners/tidb_audit_log/audit_log_ee.go
@@ -0,0 +1,141 @@
+//go:build enterprise
+// +build enterprise
+
+package tidb_audit_log
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"strconv"
+	"time"
+
+	"github.com/actiontech/sqle/sqle/cmd/scannerd/scanners"
+	"github.com/actiontech/sqle/sqle/pkg/scanner"
+
+	"github.com/percona/go-mysql/query"
+	"github.com/sirupsen/logrus"
+)
+
+type AuditLog struct {
+	l *logrus.Entry
+	c *scanner.Client
+
+	logFilePath string
+
+	sqlCh chan scanners.SQL
+
+	apName string
+
+	parser TiDBAuditLogParser
+}
+
+type Params struct {
+	AuditLogPath string
+	APName       string
+}
+
+func New(params *Params, l *logrus.Entry, c *scanner.Client) (*AuditLog, error) {
+	return &AuditLog{
+		l:           l,
+		c:           c,
+		logFilePath: params.AuditLogPath,
+		apName:      params.APName,
+		sqlCh:       make(chan scanners.SQL, 10),
+		parser:      GetLexerParser(),
+	}, nil
+}
+
+func (s *AuditLog) Run(ctx context.Context) error {
+	reader, err := scanners.NewContinuousFileReader(s.logFilePath, logrus.WithField("filename", s.logFilePath), true)
+	if err != nil {
+		return err
+	}
+
+	sqls := make(chan *scanners.SQL, 1000)
+	go func() {
+		for {
+			line, err := reader.NextLine()
+			if err == io.EOF {
+				return
+			}
+			if err != nil {
+				s.l.Errorf("read log file error: %v", err)
+				return
+			}
+
+			sql, err := s.parser.Parse(line)
+			if err != nil {
+				s.l.Errorf("Malformed log, skip parsing: %v", line)
+				continue
+			}
+			schema, err := GetMissingSchema(sql.SQLText, sql.Databases)
+			if err != nil {
+				s.l.Warnf("sql execution location not found: %v", line)
+				continue
+			}
+			sqls <- &scanners.SQL{
+				RawText: sql.SQLText,
+				Schema:  schema,
+			}
+
+		}
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			err = reader.Close()
+			s.l.Infof("context done: %s. reader closed: %v.", ctx.Err(), err)
+			close(s.sqlCh)
+			return nil
+
+		case e, ok := <-sqls:
+			if !ok {
+				return nil
+			}
+			s.l.Debugf("parsed tidb audit log event: %+v.", e)
+			fingerprint := query.Fingerprint(e.RawText)
+			if e.Schema != "" {
+				fingerprint = fmt.Sprintf("%v -- current schema: %v", fingerprint, e.Schema)
+			}
+			s.sqlCh <- scanners.SQL{
+				Fingerprint: fingerprint,
+				RawText:     e.RawText,
+				Counter:     1,
+				Schema:      e.Schema,
+			}
+
+		}
+	}
+
+}
+
+func (sq *AuditLog) SQLs() <-chan scanners.SQL {
+	return sq.sqlCh
+}
+
+func (sq *AuditLog) Upload(ctx context.Context, sqls []scanners.SQL) error {
+	sqlsReq := []scanner.AuditPlanSQLReq{}
+	now := time.Now()
+
+	counter := map[string]int /*slice subscript*/ {}
+
+	for _, sql := range sqls {
+		if subscript, ok := counter[sql.Fingerprint]; ok {
+			count, _ := strconv.Atoi(sqlsReq[subscript].Counter)
+			sqlsReq[subscript].Counter = strconv.Itoa(count + 1)
+		} else {
+			sqlsReq = append(sqlsReq, scanner.AuditPlanSQLReq{
+				Fingerprint:          sql.Fingerprint,
+				LastReceiveText:      sql.RawText,
+				LastReceiveTimestamp: now.Format(time.RFC3339),
+				Counter:              "1",
+				Schema:               sql.Schema,
+			})
+			counter[sql.Fingerprint] = len(sqlsReq) - 1
+		}
+	}
+	return sq.c.UploadReq(scanner.PartialUpload, sq.apName, sqlsReq)
+
+}
