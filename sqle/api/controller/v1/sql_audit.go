89660e18f53afdbecb7fa145e59039b69e8cf337
HuangWeiCen <1724659546@qq.com>
2022-07-18 17:30:14 +0800

    issue 674 remove invalid information

--- sqle/api/controller/v1/sql_audit.go
+++ sqle/api/controller/v1/sql_audit.go
@@ -8,7 +8,6 @@ import (
 )
 
 type DirectAuditReqV1 struct {
-	// 实例类型可以是包括"/"在内的任何字符串, 当用户阅读未转义URL时可能会产生歧义
 	InstanceType string `json:"instance_type" form:"instance_type" example:"mysql" valid:"required"`
 	// 调用方不应该关心SQL是否被完美的拆分成独立的条目, 拆分SQL由SQLE实现
 	SQLContent string `json:"sql_content" form:"sql_content" example:"select * from t1; select * from t2;" valid:"required"`
@@ -31,7 +30,6 @@ type AuditSQLResV1 struct {
 	ExecSQL     string `json:"exec_sql"`
 	AuditResult string `json:"audit_result"`
 	AuditLevel  string `json:"audit_level"`
-	RollbackSQL string `json:"rollback_sql,omitempty"`
 }
 
 var ErrDirectAudit = errors.New(errors.GenericError, fmt.Errorf("audit failed, please confirm whether the type of audit plugin supports static audit, please check the log for details"))
