0d3e453d9947265cb6444543806c00241603f683
luowei <ranshaodelengshui@gmail.com>
2022-07-18 14:39:20 +0800

    display instance type in task detail page

--- sqle/docs/docs.go
+++ sqle/docs/docs.go
@@ -4762,6 +4762,9 @@ var doc = `{
                 "exec_start_time": {
                     "type": "string"
                 },
+                "instance_db_type": {
+                    "type": "string"
+                },
                 "instance_name": {
                     "type": "string"
                 },
