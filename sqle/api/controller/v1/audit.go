980b4d82b68d54411a70a5d58d996c28719a3a55
HuangWeiCen <1724659546@qq.com>
2022-07-18 17:08:29 +0800

    issue 674 api adjustment

--- sqle/api/controller/v1/audit.go
+++ sqle/api/controller/v1/audit.go
@@ -8,12 +8,10 @@ import (
 )
 
 type DirectAuditReqV1 struct {
-	// 实例类型可以是包括"/"在内的任何字符串, 不适合放到URI里面
+	// 实例类型可以是包括"/"在内的任何字符串, 当用户阅读未转义URL时可能会产生歧义
 	InstanceType string `json:"instance_type" form:"instance_type" example:"mysql" valid:"required"`
 	// 调用方不应该关心SQL是否被完美的拆分成独立的条目, 拆分SQL由SQLE实现
-	SQLs string `json:"sqls" form:"sqls" example:"select * from t1; select * from t2;" valid:"required"`
-	// 动态审核会同时使用静态审核和动态审核的规则
-	AuditType string `json:"audit_type" from:"audit_type" default:"static" enums:"static,dynamic" valid:"oneof=static dynamic"`
+	SQLContent string `json:"sql_content" form:"sql_content" example:"select * from t1; select * from t2;" valid:"required"`
 }
 
 type DirectAuditResV1 struct {
@@ -22,12 +20,10 @@ type DirectAuditResV1 struct {
 }
 
 type AuditResDataV1 struct {
-	InstanceType   string          `json:"instance_type"`
-	AuditLevel     string          `json:"audit_level" enums:"normal,notice,warn,error,"`
-	Score          int32           `json:"score"`
-	PassRate       float64         `json:"pass_rate"`
-	SQLResults     []AuditSQLResV1 `json:"sql_results"`
-	ResultTotalNum int             `json:"result_total_num"`
+	AuditLevel string          `json:"audit_level" enums:"normal,notice,warn,error,"`
+	Score      int32           `json:"score"`
+	PassRate   float64         `json:"pass_rate"`
+	SQLResults []AuditSQLResV1 `json:"sql_results"`
 }
 
 type AuditSQLResV1 struct {
@@ -43,11 +39,11 @@ var ErrDirectAudit = errors.New(errors.GenericError, fmt.Errorf("audit failed, p
 // @Summary 直接审核SQL
 // @Description Direct audit sql
 // @Id directAuditV1
-// @Tags audit
+// @Tags sql_audit
 // @Security ApiKeyAuth
 // @Param req body v1.DirectAuditReqV1 true "sqls that should be audited"
 // @Success 200 {object} v1.DirectAuditResV1
-// @router /v1/audit/direct_audit [post]
+// @router /v1/sql_audit [post]
 func DirectAudit(c echo.Context) error {
 	return ErrDirectAudit
 }
