a3e58ffaefb62adcb4c0af07507d8e0262183952
luowei <ranshaodelengshui@gmail.com>
2022-08-09 17:58:41 +0800

    implement instance type percents

---
+++ sqle/api/controller/v1/statistic_ee.go
@@ -0,0 +1,42 @@
+//go:build enterprise
+// +build enterprise
+
+package v1
+
+import (
+	"net/http"
+
+	"github.com/actiontech/sqle/sqle/model"
+
+	"github.com/actiontech/sqle/sqle/api/controller"
+	"github.com/labstack/echo/v4"
+)
+
+func getInstancesTypePercentV1(c echo.Context) error {
+	s := model.GetStorage()
+
+	typeCounts, err := s.GetAllInstanceCount()
+	if err != nil {
+		return controller.JSONBaseErrorReq(c, err)
+	}
+	var total uint
+	for _, count := range typeCounts {
+		total += uint(count.Count)
+	}
+
+	percents := make([]InstanceTypePercent, len(typeCounts))
+	for i, count := range typeCounts {
+		percents[i] = InstanceTypePercent{
+			Type:    count.DBType,
+			Percent: float64(count.Count) / float64(total) * 100,
+		}
+	}
+
+	return c.JSON(http.StatusOK, &GetInstancesTypePercentResV1{
+		BaseRes: controller.NewBaseReq(nil),
+		Data: &InstancesTypePercentV1{
+			InstanceTypePercents: percents,
+			InstanceTotalNum:     total,
+		},
+	})
+}
