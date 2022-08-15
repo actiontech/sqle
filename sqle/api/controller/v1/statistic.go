3e91b3c793e0be1c3753c77ff1e0c9778660605e
luowei <ranshaodelengshui@gmail.com>
2022-08-15 13:26:38 +0800

    the api is used for statistics of workflow not task

--- sqle/api/controller/v1/statistic.go
+++ sqle/api/controller/v1/statistic.go
@@ -5,122 +5,122 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-type TaskCountsV1 struct {
+type WorkflowCountsV1 struct {
 	Total      uint `json:"total"`
 	TodayCount uint `json:"today_count"`
 }
 
-type GetTaskCountsResV1 struct {
+type GetWorkflowCountsResV1 struct {
 	controller.BaseRes
-	Data *TaskCountsV1 `json:"data"`
+	Data *WorkflowCountsV1 `json:"data"`
 }
 
-// GetTaskCountsV1
+// GetWorkflowCountsV1
 // @Summary 获取工单数量统计数据
-// @Description get task counts
+// @Description get workflow counts
 // @Tags statistic
-// @Id getTaskCountV1
+// @Id getWorkflowCountV1
 // @Security ApiKeyAuth
-// @Success 200 {object} v1.GetTaskCountsResV1
-// @router /v1/statistic/tasks/counts [get]
-func GetTaskCountsV1(c echo.Context) error {
-	return getTaskCounts(c)
+// @Success 200 {object} v1.GetWorkflowCountsResV1
+// @router /v1/statistic/workflows/counts [get]
+func GetWorkflowCountsV1(c echo.Context) error {
+	return getWorkflowCounts(c)
 }
 
-type TaskStageDuration struct {
+type WorkflowStageDuration struct {
 	Minutes uint `json:"minutes"`
 }
 
-type GetTaskDurationOfWaitingForAuditResV1 struct {
+type GetWorkflowDurationOfWaitingForAuditResV1 struct {
 	controller.BaseRes
-	Data *TaskStageDuration `json:"data"`
+	Data *WorkflowStageDuration `json:"data"`
 }
 
-// GetTaskDurationOfWaitingForAuditV1
+// GetWorkflowDurationOfWaitingForAuditV1
 // @Summary 获取工单从创建到审核结束的平均时长
-// @Description get duration from task being created to audited
+// @Description get duration from workflow being created to audited
 // @Tags statistic
-// @Id getTaskDurationOfWaitingForAuditV1
+// @Id getWorkflowDurationOfWaitingForAuditV1
 // @Security ApiKeyAuth
-// @Success 200 {object} v1.GetTaskDurationOfWaitingForAuditResV1
-// @router /v1/statistic/tasks/duration_of_waiting_for_audit [get]
-func GetTaskDurationOfWaitingForAuditV1(c echo.Context) error {
-	return getTaskDurationOfWaitingForAuditV1(c)
+// @Success 200 {object} v1.GetWorkflowDurationOfWaitingForAuditResV1
+// @router /v1/statistic/workflows/duration_of_waiting_for_audit [get]
+func GetWorkflowDurationOfWaitingForAuditV1(c echo.Context) error {
+	return getWorkflowDurationOfWaitingForAuditV1(c)
 }
 
-type GetTaskDurationOfWaitingForExecutionResV1 struct {
+type GetWorkflowDurationOfWaitingForExecutionResV1 struct {
 	controller.BaseRes
-	Data *TaskStageDuration `json:"data"`
+	Data *WorkflowStageDuration `json:"data"`
 }
 
-// GetTaskDurationOfWaitingForExecutionV1
+// GetWorkflowDurationOfWaitingForExecutionV1
 // @Summary 获取工单各从审核完毕到执行上线的平均时长
-// @Description get duration from task being created to executed
+// @Description get duration from workflow being created to executed
 // @Tags statistic
-// @Id getTaskDurationOfWaitingForExecutionV1
+// @Id getWorkflowDurationOfWaitingForExecutionV1
 // @Security ApiKeyAuth
-// @Success 200 {object} v1.GetTaskDurationOfWaitingForExecutionResV1
-// @router /v1/statistic/tasks/duration_of_waiting_for_execution [get]
-func GetTaskDurationOfWaitingForExecutionV1(c echo.Context) error {
-	return getTaskDurationOfWaitingForExecutionV1(c)
+// @Success 200 {object} v1.GetWorkflowDurationOfWaitingForExecutionResV1
+// @router /v1/statistic/workflows/duration_of_waiting_for_execution [get]
+func GetWorkflowDurationOfWaitingForExecutionV1(c echo.Context) error {
+	return getWorkflowDurationOfWaitingForExecutionV1(c)
 }
 
-type TaskPassPercentV1 struct {
+type WorkflowPassPercentV1 struct {
 	AuditPassPercent        float64 `json:"audit_pass_percent"`
 	ExecutionSuccessPercent float64 `json:"execution_success_percent"`
 }
 
-type GetTaskPassPercentResV1 struct {
+type GetWorkflowPassPercentResV1 struct {
 	controller.BaseRes
-	Data *TaskPassPercentV1 `json:"data"`
+	Data *WorkflowPassPercentV1 `json:"data"`
 }
 
-// GetTaskPassPercentV1
+// GetWorkflowPassPercentV1
 // @Summary 获取工单通过率
-// @Description get task pass percent
+// @Description get workflow pass percent
 // @Tags statistic
-// @Id getTaskPassPercentV1
+// @Id getWorkflowPassPercentV1
 // @Security ApiKeyAuth
-// @Success 200 {object} v1.GetTaskPassPercentResV1
-// @router /v1/statistic/tasks/pass_percent [get]
-func GetTaskPassPercentV1(c echo.Context) error {
-	return getTaskPassPercentV1(c)
+// @Success 200 {object} v1.GetWorkflowPassPercentResV1
+// @router /v1/statistic/workflows/pass_percent [get]
+func GetWorkflowPassPercentV1(c echo.Context) error {
+	return getWorkflowPassPercentV1(c)
 }
 
-type GetTaskCreatedCountsEachDayReqV1 struct {
+type GetWorkflowCreatedCountsEachDayReqV1 struct {
 	FilterDateFrom string `json:"filter_date_from" query:"filter_date_from" valid:"required"`
 	FilterDateTo   string `json:"filter_date_to" query:"filter_date_to" valid:"required"`
 }
 
-type TaskCreatedCountsEachDayItem struct {
+type WorkflowCreatedCountsEachDayItem struct {
 	Date  string `json:"date" example:"2022-08-24"`
 	Value uint   `json:"value"`
 }
 
-type TaskCreatedCountsEachDayV1 struct {
-	Samples []TaskCreatedCountsEachDayItem `json:"samples"`
+type WorkflowCreatedCountsEachDayV1 struct {
+	Samples []WorkflowCreatedCountsEachDayItem `json:"samples"`
 }
 
-type GetTaskCreatedCountsEachDayResV1 struct {
+type GetWorkflowCreatedCountsEachDayResV1 struct {
 	controller.BaseRes
-	Data *TaskCreatedCountsEachDayV1 `json:"data"`
+	Data *WorkflowCreatedCountsEachDayV1 `json:"data"`
 }
 
-// GetTaskCreatedCountsEachDayV1
+// GetWorkflowCreatedCountsEachDayV1
 // @Summary 获取每天工单创建数量
-// @Description get counts of created task each day
+// @Description get counts of created workflow each day
 // @Tags statistic
-// @Id getTaskCreatedCountEachDayV1
+// @Id getWorkflowCreatedCountEachDayV1
 // @Security ApiKeyAuth
 // @Param filter_date_from query string true "filter date from.(format:yyyy-mm-dd)"
 // @Param filter_date_to query string true "filter date to.(format:yyyy-mm-dd)"
-// @Success 200 {object} v1.GetTaskCreatedCountsEachDayResV1
-// @router /v1/statistic/tasks/each_day_counts [get]
-func GetTaskCreatedCountsEachDayV1(c echo.Context) error {
-	return getTaskCreatedCountsEachDayV1(c)
+// @Success 200 {object} v1.GetWorkflowCreatedCountsEachDayResV1
+// @router /v1/statistic/workflows/each_day_counts [get]
+func GetWorkflowCreatedCountsEachDayV1(c echo.Context) error {
+	return getWorkflowCreatedCountsEachDayV1(c)
 }
 
-type TaskStatusCountV1 struct {
+type WorkflowStatusCountV1 struct {
 	ExecutionSuccessCount    int `json:"execution_success_count"`
 	ExecutingCount           int `json:"executing_count"`
 	ExecutingFailedCount     int `json:"executing_failed_count"`
@@ -130,105 +130,105 @@ type TaskStatusCountV1 struct {
 	ClosedCount              int `json:"closed_count"`
 }
 
-type GetTaskStatusCountResV1 struct {
+type GetWorkflowStatusCountResV1 struct {
 	controller.BaseRes
-	Data *TaskStatusCountV1 `json:"data"`
+	Data *WorkflowStatusCountV1 `json:"data"`
 }
 
-// GetTaskStatusPercentV1
+// GetWorkflowStatusCountV1
 // @Summary 获取各种状态工单的数量
-// @Description get count of task status
+// @Description get count of workflow status
 // @Tags statistic
-// @Id getTaskStatusCountV1
+// @Id getWorkflowStatusCountV1
 // @Security ApiKeyAuth
-// @Success 200 {object} v1.GetTaskStatusCountResV1
-// @router /v1/statistic/tasks/status_count [get]
-func GetTaskStatusCountV1(c echo.Context) error {
-	return getTaskStatusCountV1(c)
+// @Success 200 {object} v1.GetWorkflowStatusCountResV1
+// @router /v1/statistic/workflows/status_count [get]
+func GetWorkflowStatusCountV1(c echo.Context) error {
+	return getWorkflowStatusCountV1(c)
 }
 
-type TasksPercentCountedByInstanceType struct {
+type WorkflowPercentCountedByInstanceType struct {
 	InstanceType string  `json:"instance_type"`
 	Percent      float64 `json:"percent"`
 	Count        uint    `json:"count"`
 }
 
-type TasksPercentCountedByInstanceTypeV1 struct {
-	TaskPercents []TasksPercentCountedByInstanceType `json:"task_percents"`
-	TaskTotalNum uint                                `json:"task_total_num"`
+type WorkflowPercentCountedByInstanceTypeV1 struct {
+	WorkflowPercents []WorkflowPercentCountedByInstanceType `json:"workflow_percents"`
+	WorkflowTotalNum uint                                   `json:"workflow_total_num"`
 }
 
-type GetTasksPercentCountedByInstanceTypeResV1 struct {
+type GetWorkflowPercentCountedByInstanceTypeResV1 struct {
 	controller.BaseRes
-	Data *TasksPercentCountedByInstanceTypeV1 `json:"data"`
+	Data *WorkflowPercentCountedByInstanceTypeV1 `json:"data"`
 }
 
-// GetTasksPercentCountedByInstanceTypeV1
+// GetWorkflowPercentCountedByInstanceTypeV1
 // @Summary 获取按数据源类型统计的工单百分比
-// @Description get tasks percent counted by instance type
+// @Description get workflows percent counted by instance type
 // @Tags statistic
-// @Id getTasksPercentCountedByInstanceTypeV1
+// @Id getWorkflowPercentCountedByInstanceTypeV1
 // @Security ApiKeyAuth
-// @Success 200 {object} v1.GetTasksPercentCountedByInstanceTypeResV1
-// @router /v1/statistic/tasks/instance_type_percent [get]
-func GetTasksPercentCountedByInstanceTypeV1(c echo.Context) error {
-	return getTasksPercentCountedByInstanceTypeV1(c)
+// @Success 200 {object} v1.GetWorkflowPercentCountedByInstanceTypeResV1
+// @router /v1/statistic/workflows/instance_type_percent [get]
+func GetWorkflowPercentCountedByInstanceTypeV1(c echo.Context) error {
+	return getWorkflowPercentCountedByInstanceTypeV1(c)
 }
 
-type GetTaskRejectedPercentGroupByCreatorReqV1 struct {
+type GetWorkflowRejectedPercentGroupByCreatorReqV1 struct {
 	Limit uint `json:"limit" query:"limit" valid:"required"`
 }
 
-type TaskRejectedPercentGroupByCreator struct {
-	Creator         string  `json:"creator"`
-	TaskTotalNum    uint    `json:"task_total_num"`
-	RejectedPercent float64 `json:"rejected_percent"`
+type WorkflowRejectedPercentGroupByCreator struct {
+	Creator          string  `json:"creator"`
+	WorkflowTotalNum uint    `json:"workflow_total_num"`
+	RejectedPercent  float64 `json:"rejected_percent"`
 }
 
-type GetTaskRejectedPercentGroupByCreatorResV1 struct {
+type GetWorkflowRejectedPercentGroupByCreatorResV1 struct {
 	controller.BaseRes
-	Data []*TaskRejectedPercentGroupByCreator `json:"data"`
+	Data []*WorkflowRejectedPercentGroupByCreator `json:"data"`
 }
 
-// GetTaskRejectedPercentGroupByCreatorV1
+// GetWorkflowRejectedPercentGroupByCreatorV1
 // @Summary 获取各个用户提交的工单驳回率，按驳回率降序排列
-// @Description get task rejected percent group by creator. The result will be sorted by rejected percent in descending order
+// @Description get workflows rejected percent group by creator. The result will be sorted by rejected percent in descending order
 // @Tags statistic
-// @Id getTaskRejectedPercentGroupByCreatorV1
+// @Id getWorkflowRejectedPercentGroupByCreatorV1
 // @Security ApiKeyAuth
 // @Param limit query uint true "the limit of result item number"
-// @Success 200 {object} v1.GetTaskRejectedPercentGroupByCreatorResV1
-// @router /v1/statistic/task/rejected_percent_group_by_creator [get]
-func GetTaskRejectedPercentGroupByCreatorV1(c echo.Context) error {
-	return getTaskRejectedPercentGroupByCreatorV1(c)
+// @Success 200 {object} v1.GetWorkflowRejectedPercentGroupByCreatorResV1
+// @router /v1/statistic/workflows/rejected_percent_group_by_creator [get]
+func GetWorkflowRejectedPercentGroupByCreatorV1(c echo.Context) error {
+	return getWorkflowRejectedPercentGroupByCreatorV1(c)
 }
 
-type GetTaskRejectedPercentGroupByInstanceReqV1 struct {
+type GetWorkflowRejectedPercentGroupByInstanceReqV1 struct {
 	Limit uint `json:"limit" query:"limit" valid:"required"`
 }
 
-type TaskRejectedPercentGroupByInstance struct {
-	InstanceName    string  `json:"instance_name"`
-	TaskTotalNum    uint    `json:"task_total_num"`
-	RejectedPercent float64 `json:"rejected_percent"`
+type WorkflowRejectedPercentGroupByInstance struct {
+	InstanceName     string  `json:"instance_name"`
+	WorkflowTotalNum uint    `json:"workflow_total_num"`
+	RejectedPercent  float64 `json:"rejected_percent"`
 }
 
-type GetTaskRejectedPercentGroupByInstanceResV1 struct {
+type GetWorkflowRejectedPercentGroupByInstanceResV1 struct {
 	controller.BaseRes
-	Data []*TaskRejectedPercentGroupByInstance `json:"data"`
+	Data []*WorkflowRejectedPercentGroupByInstance `json:"data"`
 }
 
-// GetTaskRejectedPercentGroupByInstanceV1
+// GetWorkflowRejectedPercentGroupByInstanceV1
 // @Summary 获取各个数据源相关的工单驳回率，按驳回率降序排列
-// @Description get task rejected percent group by instance. The result will be sorted by rejected percent in descending order
+// @Description get workflow rejected percent group by instance. The result will be sorted by rejected percent in descending order
 // @Tags statistic
-// @Id getTaskRejectedPercentGroupByInstanceV1
+// @Id getWorkflowRejectedPercentGroupByInstanceV1
 // @Security ApiKeyAuth
 // @Param limit query uint true "the limit of result item number"
-// @Success 200 {object} v1.GetTaskRejectedPercentGroupByInstanceResV1
-// @router /v1/statistic/task/rejected_percent_group_by_instance [get]
-func GetTaskRejectedPercentGroupByInstanceV1(c echo.Context) error {
-	return getTaskRejectedPercentGroupByInstanceV1(c)
+// @Success 200 {object} v1.GetWorkflowRejectedPercentGroupByInstanceResV1
+// @router /v1/statistic/workflows/rejected_percent_group_by_instance [get]
+func GetWorkflowRejectedPercentGroupByInstanceV1(c echo.Context) error {
+	return getWorkflowRejectedPercentGroupByInstanceV1(c)
 }
 
 type InstanceTypePercent struct {
