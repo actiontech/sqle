64656024dbf578b53875b56759df2a3a63716c71
luowei <ranshaodelengshui@gmail.com>
2022-08-25 14:12:05 +0800

    deduplicate sqls before saving

--- sqle/server/auditplan/task_test.go
+++ sqle/server/auditplan/task_test.go
@@ -1,8 +1,10 @@
 package auditplan
 
 import (
-	"github.com/stretchr/testify/assert"
+	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestTidbCompletionSchema(t *testing.T) {
@@ -11,4 +13,72 @@ func TestTidbCompletionSchema(t *testing.T) {
 	newSQL, err := tidbCompletionSchema(sql, "test")
 	assert.NoError(t, err)
 	assert.Equal(t, "INSERT INTO `test`.`t1` (`a1`,`a2`,`a3`,`a4`) VALUES ('','','Y',CURRENT_DATE())", newSQL)
+}
+
+func TestDeduplicateSQLsByFingerprint(t *testing.T) {
+	tests := []struct {
+		sqls []string
+		want []*sqlInfo
+	}{
+		{
+			sqls: []string{`select sleep(2)`},
+			want: []*sqlInfo{
+				{
+					counter:     1,
+					fingerprint: "select sleep(?)",
+					sql:         "select sleep(2)",
+				},
+			},
+		}, {
+			sqls: []string{`select sleep(2)`, `select sleep(4)`, `select sleep(3)`},
+			want: []*sqlInfo{
+				{
+					counter:     3,
+					fingerprint: "select sleep(?)",
+					sql:         "select sleep(3)",
+				},
+			},
+		}, {
+			sqls: []string{`select * from tb1 where a=1`},
+			want: []*sqlInfo{
+				{
+					counter:     1,
+					fingerprint: "select * from tb1 where a=?",
+					sql:         "select * from tb1 where a=1",
+				},
+			},
+		}, {
+			sqls: []string{`select * from tb1 where a=1`, `select * from tb1 where a=2`, `select * from tb1 where a=3`},
+			want: []*sqlInfo{
+				{
+					counter:     3,
+					fingerprint: "select * from tb1 where a=?",
+					sql:         "select * from tb1 where a=3",
+				},
+			},
+		}, {
+			sqls: []string{`select * from tb1 where a=1`, `select sleep(2)`, `select sleep(4)`, `select * from tb1 where a=3`},
+			want: []*sqlInfo{
+				{
+					counter:     2,
+					fingerprint: "select * from tb1 where a=?",
+					sql:         "select * from tb1 where a=3",
+				},
+				{
+					counter:     2,
+					fingerprint: "select sleep(?)",
+					sql:         "select sleep(4)",
+				},
+			},
+		},
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("test sqls NO.%v", i), func(t *testing.T) {
+			actual := deduplicateSQLsByFingerprint(tt.sqls)
+			if !assert.EqualValues(t, actual, tt.want) {
+				t.Fatal("unexpected result")
+			}
+		})
+	}
 }
