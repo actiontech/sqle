e7eb8c903c44acbde67b93884d4de5a50b5e6b1b
Sun Jian <921465802@qq.com>
2022-09-15 18:26:12 +0800

    define api router v2 for schedule workflow and deprecate v1

--- sqle/api/controller/v1/workflow.go
+++ sqle/api/controller/v1/workflow.go
@@ -1049,68 +1049,7 @@ type UpdateWorkflowScheduleV1 struct {
 // @Success 200 {object} controller.BaseRes
 // @router /v1/workflows/{workflow_id}/schedule [put]
 func UpdateWorkflowSchedule(c echo.Context) error {
-	req := new(UpdateWorkflowScheduleV1)
-	if err := controller.BindAndValidateReq(c, req); err != nil {
-		return err
-	}
-	workflowId := c.Param("workflow_id")
-	id, err := FormatStringToInt(workflowId)
-	if err != nil {
-		return controller.JSONBaseErrorReq(c, err)
-	}
-	err = CheckCurrentUserCanAccessWorkflow(c, &model.Workflow{
-		Model: model.Model{ID: uint(id)},
-	}, []uint{})
-	if err != nil {
-		return controller.JSONBaseErrorReq(c, err)
-	}
-
-	user, err := controller.GetCurrentUser(c)
-	if err != nil {
-		return controller.JSONBaseErrorReq(c, err)
-	}
-
-	s := model.GetStorage()
-	workflow, exist, err := s.GetWorkflowDetailById(workflowId)
-	if err != nil {
-		return controller.JSONBaseErrorReq(c, err)
-	}
-	if !exist {
-		return controller.JSONBaseErrorReq(c, ErrWorkflowNoAccess)
-	}
-	currentStep := workflow.CurrentStep()
-	if currentStep == nil {
-		return fmt.Errorf("workflow current step not found")
-	}
-
-	if currentStep.Template.Typ != model.WorkflowStepTypeSQLExecute {
-		return controller.JSONBaseErrorReq(c, errors.New(errors.DataInvalid,
-			fmt.Errorf("workflow need to be approved first")))
-	}
-
-	err = CheckUserCanOperateStep(user, workflow, int(currentStep.ID))
-	if err != nil {
-		return controller.JSONBaseErrorReq(c, errors.New(errors.DataInvalid, err))
-	}
-
-	if req.ScheduleTime != nil && req.ScheduleTime.Before(time.Now()) {
-		return controller.JSONBaseErrorReq(c, errors.New(errors.DataInvalid, fmt.Errorf(
-			"request schedule time is too early")))
-	}
-
-	instance, err := s.GetInstancesByWorkflowID(workflow.ID)
-	if err != nil {
-		return controller.JSONBaseErrorReq(c, err)
-	}
-	if req.ScheduleTime != nil && len(instance.MaintenancePeriod) != 0 && !instance.MaintenancePeriod.IsWithinScope(*req.ScheduleTime) {
-		return controller.JSONBaseErrorReq(c, ErrWorkflowExecuteTimeIncorrect)
-	}
-
-	err = s.UpdateWorkflowSchedule(workflow, user.ID, req.ScheduleTime)
-	if err != nil {
-		return controller.JSONBaseErrorReq(c, err)
-	}
-	return c.JSON(http.StatusOK, controller.NewBaseReq(nil))
+	return nil
 }
 
 // @Summary 工单提交 SQL 上线
