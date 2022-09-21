ef6671d7fb5143752ccdda905e23dac0cf689620
luowei <ranshaodelengshui@gmail.com>
2022-09-21 16:24:58 +0800

    get information by one query

--- sqle/model/workflow.go
+++ sqle/model/workflow.go
@@ -900,3 +900,70 @@ func (s *Storage) GetWaitExecInstancesCountByWorkflowId(workflowId uint) (int, e
 	}
 	return count, nil
 }
+
+type WorkflowTasksSummaryDetail struct {
+	WorkflowRecordStatus      string     `json:"workflow_record_status"`
+	TaskId                    uint       `json:"task_id"`
+	TaskExecStartAt           *time.Time `json:"task_exec_start_at"`
+	TaskExecEndAt             *time.Time `json:"task_exec_end_at"`
+	TaskPassRate              float64    `json:"task_pass_rate"`
+	TaskScore                 int32      `json:"task_score"`
+	TaskStatus                string     `json:"task_status"`
+	InstanceName              string     `json:"instance_name"`
+	InstanceDeletedAt         *time.Time `json:"instance_deleted_at"`
+	InstanceMaintenancePeriod Periods    `json:"instance_maintenance_period" gorm:"text"`
+	InstanceScheduledAt       *time.Time `json:"instance_scheduled_at"`
+	ExecutionUserDeletedAt    *time.Time `json:"execution_user_deleted_at"`
+	ExecutionUserName         string     `json:"execution_user_name"`
+	CurrentStepAssigneeUsers  RowList    `json:"current_step_assignee_users"`
+}
+
+var workflowTasksSummaryQueryTpl = `
+SELECT wr.status                                                     AS workflow_record_status,
+       tasks.id                                                      AS task_id,
+       tasks.exec_start_at                                           AS task_exec_start_at,
+       tasks.exec_end_at                                             AS task_exec_end_at,
+       tasks.pass_rate                                               AS task_pass_rate,
+       tasks.score                                                   AS task_score,
+       GROUP_CONCAT(tasks.status)                                    AS task_status,
+       inst.name                                                     AS instance_name,
+       inst.deleted_at                                               AS instance_deleted_at,
+       inst.maintenance_period                                       AS instance_maintenance_period,
+       wir.scheduled_at                                              AS instance_scheduled_at,
+       exec_user.deleted_at                                          AS execution_user_deleted_at,
+       COALESCE(exec_user.login_name, '')                            AS execution_user_name,
+       GROUP_CONCAT(DISTINCT COALESCE(curr_ass_user.login_name, '')) AS current_step_assignee_users
+
+{{- template "body" . -}}
+GROUP BY tasks.id, wir.id
+`
+
+var workflowTasksSummaryQueryBodyTpl = `
+{{ define "body" }}
+FROM workflow_instance_records AS wir
+LEFT JOIN workflow_records AS wr ON wir.workflow_record_id = wr.id
+LEFT JOIN workflows AS w ON w.workflow_record_id = wr.id
+LEFT JOIN users AS exec_user ON wir.execution_user_id = exec_user.id
+LEFT JOIN tasks ON wir.task_id = tasks.id
+LEFT JOIN instances AS inst ON tasks.instance_id = inst.id
+LEFT JOIN workflow_steps AS curr_ws ON wr.current_workflow_step_id = curr_ws.id
+LEFT JOIN workflow_step_user AS curr_ws_user ON curr_ws.id = curr_ws_user.workflow_step_id
+LEFT JOIN users AS curr_ass_user ON curr_ws_user.user_id = curr_ass_user.id
+
+WHERE
+w.deleted_at IS NULL
+AND w.id = :workflow_id
+
+{{ end }}
+`
+
+func (s *Storage) GetWorkflowTasksSummaryByReq(data map[string]interface{}) (
+	result []*WorkflowTasksSummaryDetail, err error) {
+
+	err = s.getListResult(workflowTasksSummaryQueryBodyTpl, workflowTasksSummaryQueryTpl, data, &result)
+	if err != nil {
+		return result, errors.New(errors.ConnectStorageError, err)
+	}
+
+	return result, nil
+}
