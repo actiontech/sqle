2451cfbc062c9cf5fd96b4ca9007114ac1cb064d
HuangWeiCen <1724659546@qq.com>
2022-09-22 23:06:51 +0800

    issue_891 optimization error

--- sqle/api/cloudbeaver_wrapper/service/audit.go
+++ sqle/api/cloudbeaver_wrapper/service/audit.go
@@ -10,44 +10,54 @@ import (
 	"github.com/actiontech/sqle/sqle/server"
 )
 
-func AuditSQL(sql string, connectionID string) (auditSuccess bool, result string, err error) {
+type AuditResult struct {
+	Result     string
+	LimitLevel string
+	AuditLevel string
+}
+
+func AuditSQL(sql string, connectionID string) (auditSuccess bool, result *AuditResult, err error) {
 	if sql == "" || connectionID == "" {
-		return true, "", nil
+		return true, nil, nil
 	}
 
 	// 获取SQLE实例
 	s := sqleModel.GetStorage()
 	cache, err := s.GetCloudBeaverInstanceCacheByCBInstIDs([]string{connectionID})
 	if err != nil {
-		return false, "", err
+		return false, nil, err
 	}
 
 	// 找不到这个实例的缓存表示这个实例不在SQLE管理范围内
 	if len(cache) == 0 {
-		return true, "", nil
+		return true, nil, nil
 	}
 
 	// 找不到sqle实例可能是实例被删除后没更新的脏数据
 	inst, exist, err := s.GetInstanceById(fmt.Sprintf("%v", cache[0].SQLEInstanceID))
 	if err != nil || !exist {
-		return false, "", err
+		return false, nil, err
 	}
 
 	// 不用审核的实例跳过审核
 	if !inst.SqlQueryConfig.AuditEnabled {
-		return true, "", nil
+		return true, nil, nil
 	}
 
 	task, err := server.AuditSQLByDBType(log.NewEntry(), sql, inst.DbType, "")
 	if err != nil {
-		return false, "", err
+		return false, nil, err
 	}
 
 	if driver.RuleLevel(task.AuditLevel).MoreOrEqual(driver.RuleLevel(inst.SqlQueryConfig.AllowQueryWhenLessThanAuditLevel)) {
-		return false, generateAuditResult(task), nil
+		return false, &AuditResult{
+			Result:     generateAuditResult(task),
+			LimitLevel: inst.SqlQueryConfig.AllowQueryWhenLessThanAuditLevel,
+			AuditLevel: task.AuditLevel,
+		}, nil
 	}
 
-	return true, "", nil
+	return true, nil, nil
 }
 
 func generateAuditResult(task *sqleModel.Task) string {
