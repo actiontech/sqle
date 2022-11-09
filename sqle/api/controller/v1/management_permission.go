4453218038abaff715f443cd15526926dc0b1345
HuangWeiCen <1724659546@qq.com>
2022-11-09 16:50:38 +0800

    issue 1004 update api realization

--- sqle/api/controller/v1/management_permission.go
+++ sqle/api/controller/v1/management_permission.go
@@ -1,7 +1,11 @@
 package v1
 
 import (
+	"net/http"
+
 	"github.com/actiontech/sqle/sqle/api/controller"
+	"github.com/actiontech/sqle/sqle/model"
+
 	"github.com/labstack/echo/v4"
 )
 
@@ -15,6 +19,22 @@ type ManagementPermissionResV1 struct {
 	Desc string `json:"desc"`
 }
 
+func generateManagementPermissionResV1(code uint) ManagementPermissionResV1 {
+	return ManagementPermissionResV1{
+		Code: code,
+		Desc: model.GetManagementPermissionDesc(code),
+	}
+}
+
+func generateManagementPermissionResV1s(code []uint) []*ManagementPermissionResV1 {
+	m := []*ManagementPermissionResV1{}
+	for _, u := range code {
+		p := generateManagementPermissionResV1(u)
+		m = append(m, &p)
+	}
+	return m
+}
+
 // GetManagementPermissions
 // @Summary 获取平台管理权限列表
 // @Description get platform management permissions
@@ -24,5 +44,17 @@ type ManagementPermissionResV1 struct {
 // @Success 200 {object} v1.GetManagementPermissionsResV1
 // @Router /v1/management_permissions [get]
 func GetManagementPermissions(c echo.Context) error {
-	return nil
+	p := model.GetManagementPermission()
+	data := []*ManagementPermissionResV1{}
+	for u, s := range p {
+		data = append(data, &ManagementPermissionResV1{
+			Code: u,
+			Desc: s,
+		})
+	}
+
+	return c.JSON(http.StatusOK, GetManagementPermissionsResV1{
+		BaseRes: controller.NewBaseReq(nil),
+		Data:    data,
+	})
 }
