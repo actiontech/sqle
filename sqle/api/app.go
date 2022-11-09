65ccb90748900d585a9d8ba5014545531738e384
Sun Jian <921465802@qq.com>
2022-11-09 17:50:17 +0800

    update router for get sql analysis

--- sqle/api/app.go
+++ sqle/api/app.go
@@ -235,7 +235,6 @@ func StartApi(net *gracenet.Net, exitChan chan struct{}, config config.SqleConfi
 	// audit plan
 	v1Router.GET("/audit_plan_metas", v1.GetAuditPlanMetas)
 	v1Router.GET("/audit_plan_types", v1.GetAuditPlanTypes)
-	v1Router.GET("/audit_plans/reports/:audit_plan_report_id/sqls/:number/analysis", v1.GetAuditPlanAnalysisData)
 
 	// project - audit plan
 	v1Router.POST("/projects/:project_name/audit_plans", v1.CreateAuditPlan)
@@ -252,6 +251,7 @@ func StartApi(net *gracenet.Net, exitChan chan struct{}, config config.SqleConfi
 	v1Router.PATCH("/projects/:project_name/audit_plans/:audit_plan_name/notify_config", v1.UpdateAuditPlanNotifyConfig)
 	v1Router.GET("/projects/:project_name/audit_plans/:audit_plan_name/notify_config", v1.GetAuditPlanNotifyConfig)
 	v1Router.GET("/projects/:project_name/audit_plans/:audit_plan_name/notify_config/test", v1.TestAuditPlanNotifyConfig)
+	v1Router.GET("/projects/:project_name/audit_plans/reports/:audit_plan_report_id/sqls/:number/analysis", v1.GetAuditPlanAnalysisData)
 
 	// sql query
 	cloudbeaver_wrapper.StartApp(e)
