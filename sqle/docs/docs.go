dfe14699cff1bccd8853c134d5894f015e00fcd7
luowei <ranshaodelengshui@gmail.com>
2022-11-11 11:26:30 +0800

    update swagger files

--- sqle/docs/docs.go
+++ sqle/docs/docs.go
@@ -10119,11 +10119,8 @@ var doc = `{
                         "$ref": "#/definitions/v1.MaintenanceTimeReqV1"
                     }
                 },
-                "rule_template_name_list": {
-                    "type": "array",
-                    "items": {
-                        "type": "string"
-                    }
+                "rule_template_name": {
+                    "type": "string"
                 },
                 "sql_query_config": {
                     "type": "object",
