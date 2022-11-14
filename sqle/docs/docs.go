991f3fcd12517025fa25d1e50c4b31b3d994c1d7
HuangWeiCen <1724659546@qq.com>
2022-11-14 14:42:12 +0800

    issue 998 user: fix bug

--- sqle/docs/docs.go
+++ sqle/docs/docs.go
@@ -10365,7 +10365,7 @@ var doc = `{
         "v1.UserDetailResV1": {
             "type": "object",
             "properties": {
-                "bind_project": {
+                "bind_projects": {
                     "type": "array",
                     "items": {
                         "$ref": "#/definitions/v1.UserBindProjectResV1"
