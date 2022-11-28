3b51fe60af30392cb852efe7dc227a0140960c40
HuangWeiCen <1724659546@qq.com>
2022-11-28 11:19:08 +0800

    ee_514 create project

--- sqle/api/controller/v1/project_ee.go
+++ sqle/api/controller/v1/project_ee.go
@@ -3,12 +3,49 @@
 
 package v1
 
-import "github.com/labstack/echo/v4"
+import (
+	"fmt"
+
+	"github.com/actiontech/sqle/sqle/api/controller"
+	"github.com/actiontech/sqle/sqle/errors"
+	"github.com/actiontech/sqle/sqle/log"
+	"github.com/actiontech/sqle/sqle/model"
+	"github.com/actiontech/sqle/sqle/server/auditplan"
+
+	"github.com/labstack/echo/v4"
+)
 
 func createProjectV1(c echo.Context) error {
-	return nil
-}
+	req := new(CreateProjectReqV1)
+	if err := controller.BindAndValidateReq(c, req); err != nil {
+		return err
+	}
 
-func deleteProjectV1(c echo.Context) error {
-	return nil
+	user, err := controller.GetCurrentUser(c)
+	if err != nil {
+		return controller.JSONBaseErrorReq(c, err)
+	}
+
+	// check
+	s := model.GetStorage()
+	have, err := s.CheckUserHaveManagementPermission(user.ID, []uint{model.ManagementPermissionCreateProject})
+	if err != nil {
+		return controller.JSONBaseErrorReq(c, err)
+	}
+	if !have {
+		return controller.JSONBaseErrorReq(c, errors.New(errors.UserNotPermission, fmt.Errorf("current user do not have permission to create project")))
+	}
+
+	_, exist, err := s.GetProjectByName(req.Name)
+	if err != nil {
+		return controller.JSONBaseErrorReq(c, err)
+	}
+	if exist {
+		return controller.JSONBaseErrorReq(c, errors.New(errors.DataExist, fmt.Errorf("project exist")))
+	}
+
+	// create
+	err = s.CreateProject(req.Name, req.Desc, user.ID)
+
+	return controller.JSONBaseErrorReq(c, err)
 }
