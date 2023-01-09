16ae161a55f17010609f3b096bda1442420f7331
taolx0 <taolx25@foxmail.com>
2023-01-09 16:10:38 +0800

    create sync instance task api define

---
+++ sqle/api/controller/v1/sync_instance_ce.go
@@ -0,0 +1,17 @@
+//go:build !enterprise
+// +build !enterprise
+
+package v1
+
+import (
+	e "errors"
+	"github.com/actiontech/sqle/sqle/api/controller"
+	"github.com/actiontech/sqle/sqle/errors"
+	"github.com/labstack/echo/v4"
+)
+
+var errCommunityEditionDoesNotSupportSyncInstance = errors.New(errors.EnterpriseEditionFeatures, e.New("community edition does not support sync instance"))
+
+func createSyncInstanceTask(c echo.Context) error {
+	return controller.JSONBaseErrorReq(c, errCommunityEditionDoesNotSupportSyncInstance)
+}
