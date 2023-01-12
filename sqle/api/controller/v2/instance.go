74ea38aded3feb9673f11ecc8759c9c48221e096
luowei <ranshaodelengshui@gmail.com>
2023-01-12 14:28:13 +0800

    return source of instance when get instance list and detail

--- sqle/api/controller/v2/instance.go
+++ sqle/api/controller/v2/instance.go
@@ -41,6 +41,7 @@ type InstanceResV2 struct {
 	RuleTemplate     *RuleTemplateV2                    `json:"rule_template,omitempty"`
 	AdditionalParams []*v1.InstanceAdditionalParamResV1 `json:"additional_params"`
 	SQLQueryConfig   *v1.SQLQueryConfigResV1            `json:"sql_query_config"`
+	Source           string                             `json:"source" example:"SQLE"`
 }
 
 type GetInstancesResV2 struct {
@@ -138,6 +139,7 @@ func GetInstances(c echo.Context) error {
 				AuditEnabled:                     instance.SqlQueryConfig.AuditEnabled,
 				AllowQueryWhenLessThanAuditLevel: instance.SqlQueryConfig.AllowQueryWhenLessThanAuditLevel,
 			},
+			Source: instance.Source,
 		}
 		instancesRes = append(instancesRes, instanceReq)
 	}
