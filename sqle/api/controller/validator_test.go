fa05878983325569a5abefd69a79a4bdfeddb596
taolx0 <taolx25@foxmail.com>
2023-02-03 16:44:44 +0800

    chore:update unit test

--- sqle/api/controller/validator_test.go
+++ sqle/api/controller/validator_test.go
@@ -6,23 +6,23 @@ import (
 )
 
 func TestValidateName(t *testing.T) {
-	assert.Equal(t, true, validateName("name"))
-	assert.Equal(t, true, validateName("name_"))
-	assert.Equal(t, true, validateName("name-"))
-	assert.Equal(t, true, validateName("name-1"))
-	assert.Equal(t, true, validateName("name_1"))
+	assert.Equal(t, true, validateName("姓名name"))
+	assert.Equal(t, true, validateName("姓名name_"))
+	assert.Equal(t, true, validateName("姓名name-"))
+	assert.Equal(t, true, validateName("姓名name-1"))
+	assert.Equal(t, true, validateName("姓名name_1"))
 
 	len60 := `test_name_length_60_0000000000000000000000000000000000000000`
 	assert.Equal(t, 60, len(len60))
 	assert.Equal(t, true, validateName(len60))
 
 	assert.Equal(t, false, validateName(""))
-	assert.Equal(t, false, validateName("1name"))
-	assert.Equal(t, false, validateName("_name"))
-	assert.Equal(t, false, validateName("-name"))
-	assert.Equal(t, false, validateName("name*"))
-	assert.Equal(t, false, validateName("name*name"))
-	assert.Equal(t, false, validateName("*name"))
+	assert.Equal(t, false, validateName("1name姓名"))
+	assert.Equal(t, false, validateName("_姓名name"))
+	assert.Equal(t, false, validateName("-姓名name"))
+	assert.Equal(t, false, validateName("姓名name*"))
+	assert.Equal(t, false, validateName("name姓名*name"))
+	assert.Equal(t, false, validateName("*name姓名"))
 
 	len61 := `test_name_length_61_00000000000000000000000000000000000000000`
 	assert.Equal(t, 61, len(len61))
