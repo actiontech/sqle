ef2626670e225b333f8c238b295d938640d3fdbd
taolx0 <taolx25@foxmail.com>
2023-02-08 15:49:03 +0800

    chore:add ip field

--- sqle/model/operation_record.go
+++ sqle/model/operation_record.go
@@ -20,6 +20,7 @@ type OperationRecord struct {
 	Model
 	OperationTime time.Time `gorm:"column:operation_time;type:datetime;not null" json:"operation_time"`
 	UserName      string    `gorm:"column:user_name;type:varchar(255);not null" json:"user_name"`
+	IP            string    `gorm:"column:ip;type:varchar(255);not null" json:"ip"`
 	TypeName      string    `gorm:"column:type_name;type:varchar(255);not null" json:"type_name"`
 	Content       string    `gorm:"column:content;type:varchar(255);not null" json:"content"`
 	ObjectName    string    `gorm:"column:object_name;type:varchar(255);not null" json:"object_name"`
