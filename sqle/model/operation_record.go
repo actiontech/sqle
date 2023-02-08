98965780f843f30f499128ab4cde2caea25293de
taolx0 <taolx25@foxmail.com>
2023-02-08 14:37:14 +0800

    chore:operationTime is required

--- sqle/model/operation_record.go
+++ sqle/model/operation_record.go
@@ -14,11 +14,11 @@ const (
 
 type OperationRecord struct {
 	Model
-	OperationTime *time.Time `gorm:"column:operation_time;type:datetime;not null" json:"operation_time"`
-	UserName      string     `gorm:"column:user_name;type:varchar(255);not null" json:"user_name"`
-	TypeName      string     `gorm:"column:type_name;type:varchar(255);not null" json:"type_name"`
-	Content       string     `gorm:"column:content;type:varchar(255);not null" json:"content"`
-	ObjectName    string     `gorm:"column:object_name;type:varchar(255);not null" json:"object_name"`
-	ProjectName   string     `gorm:"column:project_name;type:varchar(255);not null" json:"project_name"`
-	Status        string     `gorm:"column:status;type:varchar(255);not null" json:"status"`
+	OperationTime time.Time `gorm:"column:operation_time;type:datetime;not null" json:"operation_time"`
+	UserName      string    `gorm:"column:user_name;type:varchar(255);not null" json:"user_name"`
+	TypeName      string    `gorm:"column:type_name;type:varchar(255);not null" json:"type_name"`
+	Content       string    `gorm:"column:content;type:varchar(255);not null" json:"content"`
+	ObjectName    string    `gorm:"column:object_name;type:varchar(255);not null" json:"object_name"`
+	ProjectName   string    `gorm:"column:project_name;type:varchar(255);not null" json:"project_name"`
+	Status        string    `gorm:"column:status;type:varchar(255);not null" json:"status"`
 }
