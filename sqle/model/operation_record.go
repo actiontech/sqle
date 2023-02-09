fb17e03daca02278e55f056968e2f5d61269492a
taolx0 <taolx25@foxmail.com>
2023-02-09 10:52:40 +0800

    chore:rename operation record field

--- sqle/model/operation_record.go
+++ sqle/model/operation_record.go
@@ -18,12 +18,12 @@ const (
 
 type OperationRecord struct {
 	Model
-	OperationTime time.Time `gorm:"column:operation_time;type:datetime;not null" json:"operation_time"`
-	UserName      string    `gorm:"column:user_name;type:varchar(255);not null" json:"user_name"`
-	IP            string    `gorm:"column:ip;type:varchar(255);not null" json:"ip"`
-	TypeName      string    `gorm:"column:type_name;type:varchar(255);not null" json:"type_name"`
-	Content       string    `gorm:"column:content;type:varchar(255);not null" json:"content"`
-	ObjectName    string    `gorm:"column:object_name;type:varchar(255);not null" json:"object_name"`
-	ProjectName   string    `gorm:"column:project_name;type:varchar(255);not null" json:"project_name"`
-	Status        string    `gorm:"column:status;type:varchar(255);not null" json:"status"`
+	OperationTime        time.Time `gorm:"column:operation_time;type:datetime;" json:"operation_time"`
+	OperationUserName    string    `gorm:"column:operation_user_name;type:varchar(255);not null" json:"operation_user_name"`
+	OperationReqIP       string    `gorm:"column:operation_req_ip" json:"operation_req_ip"`
+	OperationTypeName    string    `gorm:"column:operation_type_name" json:"operation_type_name"`
+	OperationContent     string    `gorm:"column:operation_content" json:"operation_content"`
+	OperationObjectName  string    `gorm:"column:operation_object_name" json:"operation_object_name"`
+	OperationProjectName string    `gorm:"column:operation_project_name" json:"operation_project_name"`
+	OperationStatus      string    `gorm:"column:operation_status" json:"operation_status"`
 }
