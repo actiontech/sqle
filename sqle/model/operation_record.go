adabd932a1831bfc33600fc3a25ee755b7f5b41a
taolx0 <taolx25@foxmail.com>
2023-02-09 10:59:19 +0800

    chore:rename field name

--- sqle/model/operation_record.go
+++ sqle/model/operation_record.go
@@ -22,7 +22,7 @@ type OperationRecord struct {
 	OperationUserName    string    `gorm:"column:operation_user_name;type:varchar(255);not null" json:"operation_user_name"`
 	OperationReqIP       string    `gorm:"column:operation_req_ip" json:"operation_req_ip"`
 	OperationTypeName    string    `gorm:"column:operation_type_name" json:"operation_type_name"`
-	OperationContent     string    `gorm:"column:operation_content" json:"operation_content"`
+	OperationAction      string    `gorm:"column:operation_action" json:"operation_action"`
 	OperationObjectName  string    `gorm:"column:operation_object_name" json:"operation_object_name"`
 	OperationProjectName string    `gorm:"column:operation_project_name" json:"operation_project_name"`
 	OperationStatus      string    `gorm:"column:operation_status" json:"operation_status"`
