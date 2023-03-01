c8757b3b7d897a06e3e5f190914ddd8d40f4ccaa
luowei <ranshaodelengshui@gmail.com>
2023-03-01 13:20:36 +0800

    make code easier to be read

--- sqle/pkg/im/feishu/feishu.go
+++ sqle/pkg/im/feishu/feishu.go
@@ -30,29 +30,22 @@ func (f *FeishuClient) GetUsersByEmailOrMobile(emails, mobiles []string) (map[st
 	//查询限制每次最多50条emails和mobiles，https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/contact-v3/user/batch_get_id
 	limitation := 50
 	users := make(map[string] /*user_id*/ *UserContactInfo)
-	for head, end := 0, limitation-1; ; {
-		if head > len(mobiles)-1 && head > len(emails)-1 {
-			break
+	tempMobiles, tempEmails := mobiles, emails
+	for len(tempMobiles) > 0 || len(tempEmails) > 0 {
+		endEmails := limitation
+		endMobiles := limitation
+		if len(tempEmails) < limitation {
+			endEmails = len(tempEmails)
 		}
-
-		queryBuilder := func(queryIds []string) []string {
-			length := len(queryIds)
-			ret := []string{}
-			if end <= length-1 {
-				ret = queryIds[head : end+1]
-			} else if head <= length-1 {
-				ret = queryIds[head:length]
-			}
-			return ret
+		if len(tempMobiles) < limitation {
+			endMobiles = len(tempMobiles)
 		}
-		queryEmails := queryBuilder(emails)
-		queryMobiles := queryBuilder(mobiles)
 
 		req := larkcontact.NewBatchGetIdUserReqBuilder().
 			UserIdType(`user_id`).
 			Body(larkcontact.NewBatchGetIdUserReqBodyBuilder().
-				Emails(queryEmails).
-				Mobiles(queryMobiles).
+				Emails(tempEmails[:endEmails]).
+				Mobiles(tempMobiles[:endMobiles]).
 				Build()).
 			Build()
 
@@ -65,8 +58,8 @@ func (f *FeishuClient) GetUsersByEmailOrMobile(emails, mobiles []string) (map[st
 		}
 
 		f.convertUsersResp(resp.Data.UserList, users)
-		head = end + 1
-		end += limitation
+		tempMobiles = tempMobiles[endMobiles:]
+		tempEmails = tempEmails[endEmails:]
 	}
 	return users, nil
 }
