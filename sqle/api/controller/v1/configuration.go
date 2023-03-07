dde7284ffe543499dcb0f612f1fe87ea3befb943
luowei <ranshaodelengshui@gmail.com>
2023-03-07 14:31:29 +0800

    move wechat function to ce edition

--- sqle/api/controller/v1/configuration.go
+++ sqle/api/controller/v1/configuration.go
@@ -209,6 +209,37 @@ func TestWeChatConfigurationV1(c echo.Context) error {
 	return testWeChatConfigurationV1(c)
 }
 
+func testWeChatConfigurationV1(c echo.Context) error {
+	req := new(TestWeChatConfigurationReqV1)
+	if err := controller.BindAndValidateReq(c, req); err != nil {
+		return err
+	}
+	testID := req.RecipientID
+	notifier := &notification.WeChatNotifier{}
+	err := notifier.Notify(&notification.TestNotify{}, []*model.User{
+		{
+			Name:     testID,
+			WeChatID: testID,
+		},
+	})
+	if err != nil {
+		return c.JSON(http.StatusOK, &TestWeChatConfigurationResV1{
+			BaseRes: controller.NewBaseReq(nil),
+			Data: TestWeChatConfigurationResDataV1{
+				IsWeChatSendNormal: false,
+				SendErrorMessage:   err.Error(),
+			},
+		})
+	}
+	return c.JSON(http.StatusOK, &TestWeChatConfigurationResV1{
+		BaseRes: controller.NewBaseReq(nil),
+		Data: TestWeChatConfigurationResDataV1{
+			IsWeChatSendNormal: true,
+			SendErrorMessage:   "ok",
+		},
+	})
+}
+
 type GetDingTalkConfigurationResV1 struct {
 	controller.BaseRes
 	Data DingTalkConfigurationV1 `json:"data"`
@@ -580,6 +611,42 @@ func UpdateWeChatConfigurationV1(c echo.Context) error {
 	return updateWeChatConfigurationV1(c)
 }
 
+func updateWeChatConfigurationV1(c echo.Context) error {
+	req := new(UpdateWeChatConfigurationReqV1)
+	if err := controller.BindAndValidateReq(c, req); err != nil {
+		return err
+	}
+	s := model.GetStorage()
+	wechatC, _, err := s.GetWeChatConfiguration()
+	if err != nil {
+		return controller.JSONBaseErrorReq(c, err)
+	}
+
+	if req.CorpID != nil {
+		wechatC.CorpID = *req.CorpID
+	}
+	if req.CorpSecret != nil {
+		wechatC.CorpSecret = *req.CorpSecret
+	}
+	if req.AgentID != nil {
+		wechatC.AgentID = *req.AgentID
+	}
+	if req.ProxyIP != nil {
+		wechatC.ProxyIP = *req.ProxyIP
+	}
+	if req.EnableWeChatNotify != nil {
+		wechatC.EnableWeChatNotify = *req.EnableWeChatNotify
+	}
+	if req.SafeEnabled != nil {
+		wechatC.SafeEnabled = *req.SafeEnabled
+	}
+
+	if err := s.Save(wechatC); err != nil {
+		return controller.JSONBaseErrorReq(c, err)
+	}
+	return controller.JSONBaseErrorReq(c, nil)
+}
+
 type GetWeChatConfigurationResV1 struct {
 	controller.BaseRes
 	Data WeChatConfigurationResV1 `json:"data"`
@@ -603,6 +670,24 @@ type WeChatConfigurationResV1 struct {
 // @router /v1/configurations/wechat [get]
 func GetWeChatConfiguration(c echo.Context) error {
 	return getWeChatConfiguration(c)
+}
+
+func getWeChatConfiguration(c echo.Context) error {
+	s := model.GetStorage()
+	wechatC, _, err := s.GetWeChatConfiguration()
+	if err != nil {
+		return controller.JSONBaseErrorReq(c, err)
+	}
+	return c.JSON(http.StatusOK, &GetWeChatConfigurationResV1{
+		BaseRes: controller.NewBaseReq(nil),
+		Data: WeChatConfigurationResV1{
+			EnableWeChatNotify: wechatC.EnableWeChatNotify,
+			CorpID:             wechatC.CorpID,
+			AgentID:            wechatC.AgentID,
+			SafeEnabled:        wechatC.SafeEnabled,
+			ProxyIP:            wechatC.ProxyIP,
+		},
+	})
 }
 
 type GetLDAPConfigurationResV1 struct {
