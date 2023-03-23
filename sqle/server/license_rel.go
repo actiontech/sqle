1b3997b807e9a69e7ef623786349ed0026b5829c
sjjian <921465802@qq.com>
2023-03-23 10:02:09 +0000

    code refactor for license, update license stauts

---
+++ sqle/server/license_rel.go
@@ -0,0 +1,40 @@
+//go:build release
+// +build release
+
+package server
+
+import (
+	"time"
+
+	"github.com/actiontech/sqle/sqle/model"
+	"github.com/sirupsen/logrus"
+)
+
+type LicenseJob struct {
+	BaseJob
+}
+
+func NewLicenseJob(entry *logrus.Entry) ServerJob {
+	entry = entry.WithField("job", "ding_talk")
+	j := &LicenseJob{}
+	j.BaseJob = *NewBaseJob(entry, 1*time.Hour, j.UpdateLicense)
+	return j
+}
+
+func (j *LicenseJob) UpdateLicense(entry *logrus.Entry) {
+	s := model.GetStorage()
+	l, exist, err := s.GetLicense()
+	if err != nil {
+		entry.Errorf("fail to get license, error: %v", err)
+		return
+	}
+	if !exist || l.Content == nil {
+		return
+	}
+	l.Content.WorkDurationHour += 1
+	l.WorkDurationHour = l.WorkDurationHour + 1
+	err = s.Save(l)
+	if err != nil {
+		entry.Errorf("fail to update license, error: %v", err)
+	}
+}
