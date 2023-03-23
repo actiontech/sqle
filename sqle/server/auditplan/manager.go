cd07c78f88d9bff22bf2457d0fbbc32aa0c014ff
luowei <ranshaodelengshui@gmail.com>
2023-03-23 13:22:30 +0800

    get active audit plan when sync audit plan task

--- sqle/server/auditplan/manager.go
+++ sqle/server/auditplan/manager.go
@@ -105,7 +105,7 @@ func (mgr *Manager) SyncTask(auditPlanId uint) error {
 	mgr.mu.Lock()
 	defer mgr.mu.Unlock()
 
-	ap, exist, err := mgr.persist.GetAuditPlanById(auditPlanId)
+	ap, exist, err := mgr.persist.GetActiveAuditPlanById(auditPlanId)
 	if err != nil {
 		return err
 	}
