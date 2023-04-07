efa0ad11c712c0afa7d5bb6ba8a048adf45ca716
jessun <jessun.zhang@outlook.com>
2023-04-07 13:44:17 +0800

    fix: fix rule name

--- sqle/driver/mysql/mysql.go
+++ sqle/driver/mysql/mysql.go
@@ -362,9 +362,9 @@ func (i *MysqlDriverImpl) audit(ctx context.Context, sql string) (*driverV2.Audi
 	}
 	if useGhost {
 		if _, err := i.executeByGhost(ctx, sql, true); err != nil {
-			i.result.Add(driverV2.RuleLevelError, "", fmt.Sprintf("表空间大小超过%vMB, 将使用gh-ost进行上线, 但是dry-run抛出如下错误: %v", i.cnf.DDLGhostMinSize, err))
+			i.result.Add(driverV2.RuleLevelError, ghostRule.Name, fmt.Sprintf("表空间大小超过%vMB, 将使用gh-ost进行上线, 但是dry-run抛出如下错误: %v", i.cnf.DDLGhostMinSize, err))
 		} else {
-			i.result.Add(ghostRule.Level, "", fmt.Sprintf("表空间大小超过%vMB, 将使用gh-ost进行上线", i.cnf.DDLGhostMinSize))
+			i.result.Add(ghostRule.Level, ghostRule.Name, fmt.Sprintf("表空间大小超过%vMB, 将使用gh-ost进行上线", i.cnf.DDLGhostMinSize))
 		}
 	}
 
