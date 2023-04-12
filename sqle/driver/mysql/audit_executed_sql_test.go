21639d8c5fc3237a9a578f252387104217c84025
luowei <ranshaodelengshui@gmail.com>
2023-04-12 14:52:29 +0800

    t.Run() supports debugging a single case

--- sqle/driver/mysql/audit_executed_sql_test.go
+++ sqle/driver/mysql/audit_executed_sql_test.go
@@ -38,35 +38,54 @@ func TestAuditExecutedSQL(t *testing.T) {
 	{ // 完全屏蔽的规则
 
 		// DDLCheckAlterTableNeedMerge
-		runSingleRuleInspectCase(rulepkg.RuleHandlerMap[rulepkg.DDLCheckAlterTableNeedMerge].Rule,
-			t,
-			"DDLCheckAlterTableNeedMerge",
-			NewSQLExecutedInspect(nil),
-			`
+		t.Run("DDLCheckAlterTableNeedMerge", func(t *testing.T) {
+			runSingleRuleInspectCase(rulepkg.RuleHandlerMap[rulepkg.DDLCheckAlterTableNeedMerge].Rule,
+				t,
+				"DDLCheckAlterTableNeedMerge",
+				NewSQLExecutedInspect(nil),
+				`
 ALTER TABLE exist_db.exist_tb_1 Add column v5 varchar(255) NOT NULL DEFAULT "unit test" COMMENT "unit test";
 ALTER TABLE exist_db.exist_tb_1 Add column v6 varchar(255) NOT NULL DEFAULT "unit test" COMMENT "unit test";
 `,
-			newTestResult(),
-			newTestResult(),
-		)
+				newTestResult(),
+				newTestResult(),
+			)
+		})
+		t.Run("DDLCheckAlterTableNeedMerge", func(t *testing.T) {
+			runSingleRuleInspectCase(rulepkg.RuleHandlerMap[rulepkg.DDLCheckAlterTableNeedMerge].Rule,
+				t,
+				"DDLCheckAlterTableNeedMerge",
+				NewSQLExecutedInspect(nil),
+				`
+ALTER TABLE exist_db.exist_tb_1 Add column v5 varchar(255) NOT NULL DEFAULT "unit test" COMMENT "unit test";
+ALTER TABLE exist_db.exist_tb_1 Add column v6 varchar(255) NOT NULL DEFAULT "unit test" COMMENT "unit test";
+`,
+				newTestResult(),
+				newTestResult(),
+			)
+		})
 
 		// DDLCheckTableSize
-		runSingleRuleInspectCase(rulepkg.RuleHandlerMap[rulepkg.DDLCheckTableSize].Rule,
-			t,
-			"DDLCheckTableSize",
-			NewSQLExecutedInspect(nil),
-			`drop table exist_db.exist_tb_4;`,
-			newTestResult(),
-		)
+		t.Run("DDLCheckTableSize", func(t *testing.T) {
+			runSingleRuleInspectCase(rulepkg.RuleHandlerMap[rulepkg.DDLCheckTableSize].Rule,
+				t,
+				"DDLCheckTableSize",
+				NewSQLExecutedInspect(nil),
+				`drop table exist_db.exist_tb_4;`,
+				newTestResult(),
+			)
+		})
 
 		// DDLCheckIndexesExistBeforeCreateConstraints
-		runSingleRuleInspectCase(rulepkg.RuleHandlerMap[rulepkg.DDLCheckIndexesExistBeforeCreateConstraints].Rule,
-			t,
-			"DDLCheckIndexesExistBeforeCreateConstraints",
-			NewSQLExecutedInspect(nil),
-			`alter table exist_db.exist_tb_3 Add unique uniq_test(v2);`,
-			newTestResult(),
-		)
+		t.Run("DDLCheckIndexesExistBeforeCreateConstraints", func(t *testing.T) {
+			runSingleRuleInspectCase(rulepkg.RuleHandlerMap[rulepkg.DDLCheckIndexesExistBeforeCreateConstraints].Rule,
+				t,
+				"DDLCheckIndexesExistBeforeCreateConstraints",
+				NewSQLExecutedInspect(nil),
+				`alter table exist_db.exist_tb_3 Add unique uniq_test(v2);`,
+				newTestResult(),
+			)
+		})
 
 	}
 
@@ -75,11 +94,12 @@ ALTER TABLE exist_db.exist_tb_1 Add column v6 varchar(255) NOT NULL DEFAULT "uni
 		{ // 只检查建表语句
 
 			// DDLCheckIndexedColumnWithBlob
-			runDefaultRulesInspectCase(
-				t,
-				"DDLCheckIndexedColumnWithBlob",
-				NewSQLExecutedInspect(nil),
-				`
+			t.Run("DDLCheckIndexedColumnWithBlob", func(t *testing.T) {
+				runDefaultRulesInspectCase(
+					t,
+					"DDLCheckIndexedColumnWithBlob",
+					NewSQLExecutedInspect(nil),
+					`
 CREATE TABLE  if not exists exist_db.not_exist_tb_1 (
 id bigint unsigned NOT NULL AUTO_INCREMENT COMMENT "unit test",
 v1 varchar(255) NOT NULL DEFAULT "unit test" COMMENT "unit test",
@@ -92,21 +112,23 @@ ALTER TABLE exist_db.not_exist_tb_1 ADD INDEX idx_2(b1);
 ALTER TABLE exist_db.not_exist_tb_1 ADD COLUMN b2 blob UNIQUE KEY COMMENT "unit test";
 ALTER TABLE exist_db.not_exist_tb_1 MODIFY COLUMN b1 blob UNIQUE KEY COMMENT "unit test";
 `,
-				newTestResult().addResult(rulepkg.DDLCheckIndexedColumnWithBlob).
-					add(driverV2.RuleLevelWarn, "", "建表DDL必须包含CREATE_TIME字段且默认值为CURRENT_TIMESTAMP").
-					add(driverV2.RuleLevelWarn, "", "建表DDL必须包含UPDATE_TIME字段且默认值为CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"),
-				newTestResult(),
-				newTestResult(),
-				newTestResult(),
-				newTestResult(),
-			)
+					newTestResult().addResult(rulepkg.DDLCheckIndexedColumnWithBlob).
+						add(driverV2.RuleLevelWarn, "", "建表DDL必须包含CREATE_TIME字段且默认值为CURRENT_TIMESTAMP").
+						add(driverV2.RuleLevelWarn, "", "建表DDL必须包含UPDATE_TIME字段且默认值为CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"),
+					newTestResult(),
+					newTestResult(),
+					newTestResult(),
+					newTestResult(),
+				)
+			})
 
 			// DDLCheckIndexTooMany
-			runSingleRuleInspectCase(rulepkg.RuleHandlerMap[rulepkg.DDLCheckIndexTooMany].Rule,
-				t,
-				"DDLCheckIndexTooMany",
-				NewSQLExecutedInspect(nil),
-				`
+			t.Run("DDLCheckIndexTooMany", func(t *testing.T) {
+				runSingleRuleInspectCase(rulepkg.RuleHandlerMap[rulepkg.DDLCheckIndexTooMany].Rule,
+					t,
+					"DDLCheckIndexTooMany",
+					NewSQLExecutedInspect(nil),
+					`
 CREATE TABLE if not exists exist_db.not_exist_tb_1 (
 id bigint unsigned NOT NULL AUTO_INCREMENT COMMENT "unit test",
 v1 varchar(255) NOT NULL DEFAULT "unit test" COMMENT "unit test",
@@ -117,16 +139,18 @@ INDEX idx_2 (id)
 )ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COMMENT="unit test";
 AlTER TABLE exist_db.not_exist_tb_1 ADD INDEX idx_1(id), ADD INDEX idx_2(id), ADD INDEX idx_3(id);
 `,
-				newTestResult().addResult(rulepkg.DDLCheckIndexTooMany, "id", 2),
-				newTestResult(),
-			)
+					newTestResult().addResult(rulepkg.DDLCheckIndexTooMany, "id", 2),
+					newTestResult(),
+				)
+			})
 
 			// DDLCheckIndexCount
-			runSingleRuleInspectCase(rulepkg.RuleHandlerMap[rulepkg.DDLCheckIndexCount].Rule,
-				t,
-				"DDLCheckIndexCount",
-				NewSQLExecutedInspect(nil),
-				`
+			t.Run("DDLCheckIndexCount", func(t *testing.T) {
+				runSingleRuleInspectCase(rulepkg.RuleHandlerMap[rulepkg.DDLCheckIndexCount].Rule,
+					t,
+					"DDLCheckIndexCount",
+					NewSQLExecutedInspect(nil),
+					`
 CREATE TABLE  if not exists exist_db.not_exist_tb_1 (
 id bigint unsigned NOT NULL AUTO_INCREMENT COMMENT "unit test",
 v1 varchar(255) NOT NULL DEFAULT "unit test" COMMENT "unit test",
@@ -141,16 +165,18 @@ INDEX idx_6 (id)
 )ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COMMENT="unit test";
 AlTER TABLE exist_db.not_exist_tb_1 ADD INDEX idx_1(id), ADD INDEX idx_2(id), ADD INDEX idx_3(id), ADD INDEX idx_4(id), ADD INDEX idx_5(id), ADD INDEX idx_6 (id);
 `,
-				newTestResult().addResult(rulepkg.DDLCheckIndexCount, 5),
-				newTestResult(),
-			)
+					newTestResult().addResult(rulepkg.DDLCheckIndexCount, 5),
+					newTestResult(),
+				)
+			})
 
 			// DDLCheckCompositeIndexMax
-			runSingleRuleInspectCase(rulepkg.RuleHandlerMap[rulepkg.DDLCheckCompositeIndexMax].Rule,
-				t,
-				"DDLCheckCompositeIndexMax",
-				NewSQLExecutedInspect(nil),
-				`
+			t.Run("DDLCheckCompositeIndexMax", func(t *testing.T) {
+				runSingleRuleInspectCase(rulepkg.RuleHandlerMap[rulepkg.DDLCheckCompositeIndexMax].Rule,
+					t,
+					"DDLCheckCompositeIndexMax",
+					NewSQLExecutedInspect(nil),
+					`
 CREATE TABLE  if not exists exist_db.not_exist_tb_1 (
 id bigint unsigned NOT NULL AUTO_INCREMENT COMMENT "unit test",
 v1 varchar(255) NOT NULL DEFAULT "unit test" COMMENT "unit test",
@@ -163,16 +189,18 @@ INDEX idx_1 (id,v1,v2,v3,v4,v5)
 )ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COMMENT="unit test";
 ALTER TABLE exist_db.not_exist_tb_1 ADD INDEX idx_1 (id,v1,v2,v3,v4,v5);
 			`,
-				newTestResult().addResult(rulepkg.DDLCheckCompositeIndexMax, 3),
-				newTestResult(),
-			)
+					newTestResult().addResult(rulepkg.DDLCheckCompositeIndexMax, 3),
+					newTestResult(),
+				)
+			})
 
 			// DDLCheckPKProhibitAutoIncrement
-			runSingleRuleInspectCase(rulepkg.RuleHandlerMap[rulepkg.DDLCheckPKProhibitAutoIncrement].Rule,
-				t,
-				"DDLCheckPKProhibitAutoIncrement",
-				NewSQLExecutedInspect(nil),
-				`
+			t.Run("DDLCheckPKProhibitAutoIncrement", func(t *testing.T) {
+				runSingleRuleInspectCase(rulepkg.RuleHandlerMap[rulepkg.DDLCheckPKProhibitAutoIncrement].Rule,
+					t,
+					"DDLCheckPKProhibitAutoIncrement",
+					NewSQLExecutedInspect(nil),
+					`
 CREATE TABLE  if not exists exist_db.not_exist_tb_1 (
 id bigint unsigned NOT NULL AUTO_INCREMENT DEFAULT "unit test" COMMENT "unit test" ,
 v1 varchar(255) NOT NULL DEFAULT "unit test" COMMENT "unit test",
@@ -181,15 +209,17 @@ PRIMARY KEY (id)
 )ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COMMENT="unit test";
 ALTER TABLE exist_db.not_exist_tb_1 modify COLUMN id BIGINT auto_increment;
 				`,
-				newTestResult().addResult(rulepkg.DDLCheckPKProhibitAutoIncrement),
-				newTestResult(),
-			)
+					newTestResult().addResult(rulepkg.DDLCheckPKProhibitAutoIncrement),
+					newTestResult(),
+				)
+			})
 
 			// DDLCheckPKWithoutAutoIncrement
-			runDefaultRulesInspectCase(t,
-				"DDLCheckPKWithoutAutoIncrement",
-				NewSQLExecutedInspect(nil),
-				`
+			t.Run("DDLCheckPKWithoutAutoIncrement", func(t *testing.T) {
+				runDefaultRulesInspectCase(t,
+					"DDLCheckPKWithoutAutoIncrement",
+					NewSQLExecutedInspect(nil),
+					`
 CREATE TABLE  if not exists exist_db.not_exist_tb_1 (
 id bigint unsigned NOT NULL KEY DEFAULT "unit test" COMMENT "unit test",
 v1 varchar(255) NOT NULL DEFAULT "unit test" COMMENT "unit test",
@@ -197,17 +227,19 @@ v2 varchar(255) NOT NULL DEFAULT "unit test" COMMENT "unit test"
 )ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COMMENT="unit test";
 ALTER TABLE exist_db.exist_tb_1 Add primary key(v1);
 			`,
-				newTestResult().addResult(rulepkg.DDLCheckPKWithoutAutoIncrement).
-					add(driverV2.RuleLevelWarn, "", "建表DDL必须包含CREATE_TIME字段且默认值为CURRENT_TIMESTAMP").
-					add(driverV2.RuleLevelWarn, "", "建表DDL必须包含UPDATE_TIME字段且默认值为CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"),
-				newTestResult(),
-			)
+					newTestResult().addResult(rulepkg.DDLCheckPKWithoutAutoIncrement).
+						add(driverV2.RuleLevelWarn, "", "建表DDL必须包含CREATE_TIME字段且默认值为CURRENT_TIMESTAMP").
+						add(driverV2.RuleLevelWarn, "", "建表DDL必须包含UPDATE_TIME字段且默认值为CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"),
+					newTestResult(),
+				)
+			})
 
 			// DDLCheckPKWithoutBigintUnsigned
-			runDefaultRulesInspectCase(t,
-				"DDLCheckPKWithoutBigintUnsigned",
-				NewSQLExecutedInspect(nil),
-				`
+			t.Run("DDLCheckPKWithoutBigintUnsigned", func(t *testing.T) {
+				runDefaultRulesInspectCase(t,
+					"DDLCheckPKWithoutBigintUnsigned",
+					NewSQLExecutedInspect(nil),
+					`
 CREATE TABLE  if not exists exist_db.not_exist_tb_1 (
 id bigint NOT NULL AUTO_INCREMENT COMMENT "unit test",
 v1 varchar(255) NOT NULL DEFAULT "unit test" COMMENT "unit test",
@@ -216,19 +248,21 @@ PRIMARY KEY (id)
 )ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COMMENT="unit test";
 ALTER TABLE exist_db.exist_tb_1 Add primary key(v1);
 			`,
-				newTestResult().addResult(rulepkg.DDLCheckPKWithoutBigintUnsigned).
-					add(driverV2.RuleLevelWarn, "", "建表DDL必须包含CREATE_TIME字段且默认值为CURRENT_TIMESTAMP").
-					add(driverV2.RuleLevelWarn, "", "建表DDL必须包含UPDATE_TIME字段且默认值为CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"),
-				newTestResult(),
-			)
+					newTestResult().addResult(rulepkg.DDLCheckPKWithoutBigintUnsigned).
+						add(driverV2.RuleLevelWarn, "", "建表DDL必须包含CREATE_TIME字段且默认值为CURRENT_TIMESTAMP").
+						add(driverV2.RuleLevelWarn, "", "建表DDL必须包含UPDATE_TIME字段且默认值为CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"),
+					newTestResult(),
+				)
+			})
 
 			// TODO 这个规则不允许离线运行, 手动测试保证
 			// DDLCheckRedundantIndex
-			runSingleRuleInspectCase(rulepkg.RuleHandlerMap[rulepkg.DDLCheckRedundantIndex].Rule,
-				t,
-				"DDLCheckRedundantIndex",
-				NewSQLExecutedInspect(nil),
-				`
+			t.Run("DDLCheckRedundantIndex", func(t *testing.T) {
+				runSingleRuleInspectCase(rulepkg.RuleHandlerMap[rulepkg.DDLCheckRedundantIndex].Rule,
+					t,
+					"DDLCheckRedundantIndex",
+					NewSQLExecutedInspect(nil),
+					`
 			CREATE TABLE  if not exists exist_db.not_exist_tb_1 (
 			id bigint unsigned NOT NULL AUTO_INCREMENT COMMENT "unit test",
 			v1 varchar(255) NOT NULL DEFAULT "unit test" COMMENT "unit test",
@@ -239,9 +273,10 @@ ALTER TABLE exist_db.exist_tb_1 Add primary key(v1);
 			)ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COMMENT="unit test";
 			alter table exist_db.exist_tb_1 add index idx_t (v1);
 						`,
-				newTestResult().addResult(rulepkg.DDLCheckRedundantIndex, "存在重复索引:(id); "),
-				newTestResult(),
-			)
+					newTestResult().addResult(rulepkg.DDLCheckRedundantIndex, "存在重复索引:(id); "),
+					newTestResult(),
+				)
+			})
 
 		}
 	}
