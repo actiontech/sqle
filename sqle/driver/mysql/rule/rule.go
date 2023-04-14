9088f17f9371cb96c59eca24f8cd60df0a054261
luowei <ranshaodelengshui@gmail.com>
2023-04-14 10:12:27 +0800

    fix unit test and go lint error

--- sqle/driver/mysql/rule/rule.go
+++ sqle/driver/mysql/rule/rule.go
@@ -2166,7 +2166,7 @@ func checkJoinFieldType(input *RuleHandlerInput) error {
 }
 
 func checkJoinHasOn(input *RuleHandlerInput) error {
-	tableRefs := &ast.Join{}
+	var tableRefs *ast.Join
 	switch stmt := input.Node.(type) {
 	case *ast.SelectStmt:
 		if stmt.From == nil {
@@ -5461,7 +5461,6 @@ func checkSortColumnLength(input *RuleHandlerInput) error {
 		var table *ast.TableName
 		if singleTableSource == nil { // 这种情况是查询多表
 			if colName.Name.Table.O == "" { // 查询多表的情况下order by的字段没有指定表名，简单处理，暂不对这个字段做校验。但会通过审核结果给出提示
-				table = &ast.TableName{}
 				notCheckCols = append(notCheckCols, colName.Name.Name.O)
 				return
 			}
@@ -5545,7 +5544,10 @@ func checkSortColumnLength(input *RuleHandlerInput) error {
 		// join子查询里的order by不做处理
 		t, ok := stmt.From.TableRefs.Left.(*ast.TableSource)
 		if ok && t != nil && stmt.From.TableRefs.Right == nil {
-			singleTable = t.Source.(*ast.TableName)
+			temp, ok := t.Source.(*ast.TableName)
+			if ok {
+				singleTable = temp
+			}
 		}
 		gatherColFromSelectStmt(stmt, singleTable)
 	case *ast.UnionStmt:
@@ -5556,7 +5558,10 @@ func checkSortColumnLength(input *RuleHandlerInput) error {
 		for _, s := range stmt.SelectList.Selects {
 			t, ok := s.From.TableRefs.Left.(*ast.TableSource)
 			if ok && t != nil && s.From.TableRefs.Right == nil {
-				singleTable = t.Source.(*ast.TableName)
+				temp, ok := t.Source.(*ast.TableName)
+				if ok {
+					singleTable = temp
+				}
 			}
 			gatherColFromSelectStmt(s, singleTable)
 		}
@@ -5564,13 +5569,19 @@ func checkSortColumnLength(input *RuleHandlerInput) error {
 	case *ast.DeleteStmt:
 		t, ok := stmt.TableRefs.TableRefs.Left.(*ast.TableSource)
 		if ok && t != nil && stmt.TableRefs.TableRefs.Right == nil {
-			singleTable = t.Source.(*ast.TableName)
+			temp, ok := t.Source.(*ast.TableName)
+			if ok {
+				singleTable = temp
+			}
 		}
 		gatherColFromOrderByClause(stmt.Order, singleTable)
 	case *ast.UpdateStmt:
 		t, ok := stmt.TableRefs.TableRefs.Left.(*ast.TableSource)
 		if ok && t != nil && stmt.TableRefs.TableRefs.Right == nil {
-			singleTable = t.Source.(*ast.TableName)
+			temp, ok := t.Source.(*ast.TableName)
+			if ok {
+				singleTable = temp
+			}
 		}
 		gatherColFromOrderByClause(stmt.Order, singleTable)
 	default:
