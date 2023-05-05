848d76860049d280980ba09932d759a07129232c
jessun <jessun.zhang@outlook.com>
2023-05-05 10:34:41 +0800

    feat(config): 新增API以获取支持的数据库类型及其默认端口

---
+++ sqle/api/controller/v2/configuration.go
@@ -0,0 +1,47 @@
+package v2
+
+import (
+	"net/http"
+
+	"github.com/actiontech/sqle/sqle/api/controller"
+	"github.com/actiontech/sqle/sqle/driver"
+
+	"github.com/labstack/echo/v4"
+)
+
+type GetDriversRes struct {
+	controller.BaseRes
+	Data []*DriverMeta `json:"data"`
+}
+
+type DriverMeta struct {
+	Name        string `json:"driver_name"`
+	DefaultPort uint   `json:"default_port"`
+}
+
+// GetDrivers get support Driver list.
+// @Summary 获取当前 server 支持的审核类型
+// @Description get drivers
+// @Id getDriversV2
+// @Tags configuration
+// @Security ApiKeyAuth
+// @Success 200 {object} v2.GetDriversRes
+// @router /v2/configurations/drivers [get]
+func GetDrivers(c echo.Context) error {
+
+	metas := driver.GetPluginManager().AllDriverMetas()
+	data := make([]*DriverMeta, len(metas))
+
+	for i := range metas {
+		meta := metas[i]
+		data[i] = &DriverMeta{
+			Name:        meta.PluginName,
+			DefaultPort: uint(meta.DatabaseDefaultPort),
+		}
+	}
+
+	return c.JSON(http.StatusOK, &GetDriversRes{
+		BaseRes: controller.NewBaseReq(nil),
+		Data:    data,
+	})
+}
