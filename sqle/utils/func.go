87f512084c1a5faf0988d5c5ac8ddeb03a08460f
jessun <jessunz@outlook.com>
2023-05-24 15:59:32 +0800

    refactor(mysql_driver):  refactor KillProcess()

---
+++ sqle/utils/func.go
@@ -0,0 +1,26 @@
+package utils
+
+import "context"
+
+type asyncFunc func() error
+
+func AsyncCallTimeout(ctx context.Context, fn asyncFunc) error {
+	if fn == nil {
+		return nil
+	}
+
+	errChan := make(chan error)
+	go func() {
+		errChan <- fn()
+	}()
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case err, ok := <-errChan:
+		if ok {
+			close(errChan)
+		}
+		return err
+	}
+}
