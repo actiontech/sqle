967b4317a11702ba2b2c36017e2144dfa94644f5
bianyucheng <bianyucheng0@gmail.com>
2023-07-06 15:40:34 +0000

    instance interface

--- sqle/model/audit_plan_report.go
+++ sqle/model/audit_plan_report.go
@@ -51,30 +51,26 @@ func (s *Storage) GetAuditPlanReportSQLV2ByReportIDAndNumber(reportId, number ui
 type LatestAuditPlanReportScore struct {
 	DbType       string `json:"db_type"`
 	InstanceName string `json:"instance_name"`
-	Score     bool   `json:"score"`
+	Score        uint   `json:"score"`
 }
 
 func (s *Storage) GetLatestAuditPlanReportScoreFromInstanceByproject(projectName string) ([]*LatestAuditPlanReportScore, error) {
 	var latestAuditPlanReportScore []*LatestAuditPlanReportScore
 	subQuery := s.db.Model(&AuditPlanReportV2{}).
-		Select("audit_plans.db_type").
-		Select("audit_plans.instance_name").
-		Select("MAX(audit_plan_reports_v2.created_at) as latest_created_at").
+		Select("audit_plans.db_type, audit_plans.instance_name, MAX(audit_plan_reports_v2.created_at) as latest_created_at").
 		Joins("left join audit_plans on audit_plan_reports_v2.audit_plan_id=audit_plans.id").
 		Joins("left join projects on audit_plans.project_id=projects.id").
 		Where("projects.name=?", projectName).
-		Group("audit_plans.DBType, audit_plans.instance_name").
+		Group("audit_plans.db_type, audit_plans.instance_name").
 		SubQuery()
 
 	err := s.db.Model(&AuditPlanReportV2{}).
-		Select("audit_plans.db_type").
-		Select("audit_plans.instance_name").
-		Select("audit_plan_reports_v2.score").
+		Select("audit_plans.db_type, audit_plans.instance_name, audit_plan_reports_v2.score").
 		Joins("left join audit_plans on audit_plan_reports_v2.audit_plan_id=audit_plans.id").
 		Joins("left join projects on audit_plans.project_id=projects.id").
 		Joins("join (?) as sq on audit_plans.db_type=sq.db_type and audit_plans.instance_name=sq.instance_name and audit_plan_reports_v2.created_at=sq.latest_created_at", subQuery).
 		Where("projects.name=?", projectName).
 		Scan(&latestAuditPlanReportScore).Error
-	
+
 	return latestAuditPlanReportScore, errors.ConnectStorageErrWrapper(err)
 }
