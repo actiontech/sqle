6cccb3a8770e61a8995b4a4cfaeb2155dc857490
bianyucheng <bianyucheng@action.com>
2023-07-20 08:11:56 +0000

    use custom rules of template to audit

--- sqle/server/custom_rule_audit_ee.go
+++ sqle/server/custom_rule_audit_ee.go
@@ -4,40 +4,48 @@
 package server
 
 import (
+	"fmt"
 	"regexp"
-	"strconv"
 
 	driverV2 "github.com/actiontech/sqle/sqle/driver/v2"
 	"github.com/actiontech/sqle/sqle/model"
+	xerrors "github.com/pkg/errors"
 
 	"github.com/sirupsen/logrus"
 )
 
-func CustomRuleAudit(l *logrus.Entry, DbType string, sqls []string, results []*driverV2.AuditResults) {
+func CustomRuleAudit(l *logrus.Entry, task *model.Task, sqls []string, results []*driverV2.AuditResults, projectId *uint, ruleTemplateName string) {
 	st := model.GetStorage()
-	customRules, exist, err := st.GetCustomRuleByDBTypeAndScriptType(DbType, "regular")
-	if err != nil {
-		l.Errorf("query custom rules failed:%v", err)
+
+	var customRules []*model.CustomRule
+	var err error
+	if ruleTemplateName != "" {
+		if projectId == nil {
+			err = xerrors.New("project id is needed when rule template name is given")
+		} else {
+			customRules, err = st.GetCustomRulesFromRuleTemplateByName([]uint{*projectId, model.ProjectIdForGlobalRuleTemplate}, ruleTemplateName)
+		}
+	} else {
+		if task.Instance != nil {
+			customRules, err = st.GetCustomRulesByInstanceId(fmt.Sprintf("%v", task.Instance.ID))
+		} else {
+			templateName := st.GetDefaultRuleTemplateName(task.DBType)
+			// 默认规则模板从全局模板里拿
+			customRules, err = st.GetCustomRulesFromRuleTemplateByName([]uint{model.ProjectIdForGlobalRuleTemplate}, templateName)
+		}
 	}
-	if !exist {
+	if err != nil {
+		l.Errorf("get rules error: %v", err)
 		return
 	}
+
 	for i, sql := range sqls {
 		for _, customRule := range customRules {
 			ruleScript := customRule.RuleScript
 			regex, err := regexp.Compile(ruleScript)
 			if err != nil {
-				// 一些特殊字符会报错，需要将他们转为原始字符。例如:[\u4e00-\u9fa5]
-				ruleScript, err = strconv.Unquote(`"` + ruleScript + `"`)
-				if err != nil {
-					l.Errorf("str unquote failed:%v", err)
-					continue
-				}
-				regex, err = regexp.Compile(ruleScript)
-				if err != nil {
-					l.Errorf("regexp compile failed:%v", err)
-					continue
-				}
+				l.Errorf("regexp compile failed:%v", err)
+				continue
 			}
 			if regex.MatchString(sql) {
 				res := driverV2.AuditResult{
@@ -45,7 +53,7 @@ func CustomRuleAudit(l *logrus.Entry, DbType string, sqls []string, results []*d
 					RuleName: customRule.RuleId,
 					Level:    driverV2.RuleLevel(customRule.Level),
 				}
-				result := results[i]  // TODO: 判断越界
+				result := results[i] // TODO: 判断越界
 				result.Results = append(result.Results, &res)
 			}
 		}
