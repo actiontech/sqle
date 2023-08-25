4c11a0ccbeedccfddb858a9a45d7f7e496d8be87
yangzhongjiao <yangzhongjiao@actionsky.com>
2023-08-25 05:59:00 +0000

    use common operation handler

--- sqle/api/controller/v1/dms_handle.go
+++ sqle/api/controller/v1/dms_handle.go
@@ -1,31 +1,18 @@
 package v1
 
 import (
-	"context"
 	"fmt"
 	"net/http"
-	"reflect"
 
 	baseV1 "github.com/actiontech/dms/pkg/dms-common/api/base/v1"
 	dmsV1 "github.com/actiontech/dms/pkg/dms-common/api/dms/v1"
+	"github.com/actiontech/dms/pkg/dms-common/dmsobject"
 	"github.com/actiontech/sqle/sqle/api/controller"
-	"github.com/iancoleman/strcase"
 	"github.com/labstack/echo/v4"
 )
 
-var operateHandlers map[string]OperationHanler = make(map[string]OperationHanler)
-
-// NOTE:
-// The implemented structure must be named[CamelCase] by the combination of DataResourceType, OperationType, and OperationTimingType
-type OperationHanler interface {
-	Hanle(ctx context.Context, currentUserId string, dataResourceId string) error
-}
-
 func init() {
-	for _, v := range []OperationHanler{AfterDeleteNamespace{}, BeforeDeleteNamespace{}, BeforeArvhiveNamespace{}, AfterCreateNamespace{}} {
-		structName := strcase.ToSnake(reflect.TypeOf(v).Name())
-		operateHandlers[structName] = v
-	}
+	dmsobject.InitOperateHandlers([]dmsobject.OperationHanler{AfterDeleteNamespace{}, BeforeDeleteNamespace{}, BeforeArvhiveNamespace{}, AfterCreateNamespace{}})
 }
 
 // 内部接口
@@ -34,7 +21,7 @@ func OperateDataResourceHandle(c echo.Context) error {
 	if err := controller.BindAndValidateReq(c, req); err != nil {
 		return controller.JSONBaseErrorReq(c, err)
 	}
-	h, ok := operateHandlers[fmt.Sprintf("%s_%s_%s", req.OperationTiming, req.OperationType, req.DataResourceType)]
+	h, ok := dmsobject.OperateHandlers[fmt.Sprintf("%s_%s_%s", req.OperationTiming, req.OperationType, req.DataResourceType)]
 	if ok {
 		err := h.Hanle(c.Request().Context(), "", req.DataResourceUid)
 		if err != nil {
