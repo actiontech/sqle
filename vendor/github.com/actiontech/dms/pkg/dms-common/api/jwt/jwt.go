8a854b5e2fed8d6a255971c9a54c8d91861e9c7c
rocky114 <934313141@qq.com>
2023-09-22 11:52:22 +0800

    jwt, aes using functions provided by dms-common

--- vendor/github.com/actiontech/dms/pkg/dms-common/api/jwt/jwt.go
+++ vendor/github.com/actiontech/dms/pkg/dms-common/api/jwt/jwt.go
@@ -16,23 +16,26 @@ type EchoContextGetter interface {
 	Get(key string) interface{}
 }
 
-type myClaims struct {
-	Uid string `json:"uid"`
-	jwt.RegisteredClaims
-}
+type CustomClaimFunc func(claims jwt.MapClaims)
 
-func GenJwtToken(userUid string) (tokenStr string, err error) {
+const (
+	JWTUserId        = "uid"
+	JWTUsername      = "name"
+	JWTExpiredTime   = "exp"
+	JWTAuditPlanName = "apn"
+)
 
-	claims := myClaims{
-		Uid: userUid,
-		RegisteredClaims: jwt.RegisteredClaims{
-			Issuer:    "actiontech dms",
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
-		},
+func GenJwtToken(customClaims ...CustomClaimFunc) (tokenStr string, err error) {
+	var mapClaims = jwt.MapClaims{
+		"iss":          "actiontech dms",
+		JWTExpiredTime: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
 	}
 
+	for _, claimFunc := range customClaims {
+		claimFunc(mapClaims)
+	}
 	// Create token with claims
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, mapClaims)
 
 	// Generate encoded token and send it as response.
 	tokenStr, err = token.SignedString(dmsCommonV1.JwtSigningKey)
@@ -42,19 +45,34 @@ func GenJwtToken(userUid string) (tokenStr string, err error) {
 	return tokenStr, nil
 }
 
+func WithUserId(userId string) CustomClaimFunc {
+	return func(claims jwt.MapClaims) {
+		claims[JWTUserId] = userId
+	}
+}
+
+func WithUserName(name string) CustomClaimFunc {
+	return func(claims jwt.MapClaims) {
+		claims[JWTUsername] = name
+	}
+}
+
+func WithAuditPlanName(name string) CustomClaimFunc {
+	return func(claims jwt.MapClaims) {
+		claims[JWTAuditPlanName] = name
+	}
+}
+
+func WithExpiredTime(duration time.Duration) CustomClaimFunc {
+	return func(claims jwt.MapClaims) {
+		claims[JWTExpiredTime] = jwt.NewNumericDate(time.Now().Add(duration))
+	}
+}
+
 func ParseUidFromJwtTokenStr(tokenStr string) (uid string, err error) {
-	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
-		if signMethod256, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-		} else if signMethod256 != jwt.SigningMethodHS256 {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-		}
-
-		return dmsCommonV1.JwtSigningKey, nil
-	})
-
+	token, err := parseJwtTokenStr(tokenStr)
 	if err != nil {
-		return "", fmt.Errorf("parse token failed: %v", err)
+		return "", err
 	}
 
 	userId, err := ParseUserUidStrFromToken(token)
@@ -63,6 +81,44 @@ func ParseUidFromJwtTokenStr(tokenStr string) (uid string, err error) {
 	}
 
 	return userId, nil
+}
+
+func parseJwtTokenStr(tokenStr string) (*jwt.Token, error) {
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		if signMethod256, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, jwt.ErrSignatureInvalid
+		} else if signMethod256 != jwt.SigningMethodHS256 {
+			return nil, jwt.ErrSignatureInvalid
+		}
+
+		return dmsCommonV1.JwtSigningKey, nil
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("parse token failed: %v", err)
+	}
+
+	return token, nil
+}
+
+// ParseAuditPlanName used by echo middleware which only verify api request to audit plan related.
+func ParseAuditPlanName(tokenStr string) (string, error) {
+	token, err := parseJwtTokenStr(tokenStr)
+	if err != nil {
+		return "", err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return "", fmt.Errorf("failed to convert token claims to jwt")
+	}
+
+	auditPlanName, ok := claims[JWTAuditPlanName]
+	if !ok {
+		return "", jwt.NewValidationError("unknown token", jwt.ValidationErrorClaimsInvalid)
+	}
+
+	return fmt.Sprintf("%v", auditPlanName), nil
 }
 
 func GetUserFromContext(c EchoContextGetter) (uid int64, err error) {
@@ -122,7 +178,8 @@ func ParseUserUidStrFromToken(token *jwt.Token) (uid string, err error) {
 	if !ok {
 		return "", fmt.Errorf("failed to convert token claims to jwt")
 	}
-	uidStr := fmt.Sprintf("%v", claims["uid"])
+
+	uidStr := fmt.Sprintf("%v", claims[JWTUserId])
 	if uidStr == "" {
 		return "", fmt.Errorf("failed to parse user id: empty uid")
 	}
@@ -134,7 +191,7 @@ func ParseUserUidStrFromTokenWithOldJwt(token *jwtOld.Token) (uid string, err er
 	if !ok {
 		return "", fmt.Errorf("failed to convert token claims to jwt")
 	}
-	uidStr := fmt.Sprintf("%v", claims["uid"])
+	uidStr := fmt.Sprintf("%v", claims[JWTUserId])
 	if uidStr == "" {
 		return "", fmt.Errorf("failed to parse user id: empty uid")
 	}
