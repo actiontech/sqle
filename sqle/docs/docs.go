7b0f93e06ba93750b8cf22d90d9aa47f013ed921
yangzhongjiao <yangzhongjiao@actionsky.com>
2023-10-13 10:19:27 +0000

    gen swag

--- sqle/docs/docs.go
+++ sqle/docs/docs.go
@@ -5535,6 +5535,12 @@ var doc = `{
                         "name": "project_name",
                         "in": "path",
                         "required": true
+                    },
+                    {
+                        "type": "string",
+                        "description": "fuzzy matching subject/workflow_id",
+                        "name": "fuzzy_keyword",
+                        "in": "query"
                     }
                 ],
                 "responses": {
@@ -5766,6 +5772,12 @@ var doc = `{
                         "name": "project_name",
                         "in": "path",
                         "required": true
+                    },
+                    {
+                        "type": "string",
+                        "description": "fuzzy matching subject/workflow_id/desc",
+                        "name": "fuzzy_keyword",
+                        "in": "query"
                     }
                 ],
                 "responses": {
