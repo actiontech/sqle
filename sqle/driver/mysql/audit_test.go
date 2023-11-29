64d66d93b41120489b0ecd98bd30366183ca729c
winfredLIN <linxiaotao1998@live.com>
2023-11-29 14:41:12 +0800

    ci: change name of variables

--- sqle/driver/mysql/audit_test.go
+++ sqle/driver/mysql/audit_test.go
@@ -6469,7 +6469,7 @@ func TestDMLCheckJoinFieldUseIndex(t *testing.T) {
 	runSingleRuleInspectCase(
 		rule,
 		t,
-		"without join conodition",
+		"without join condition",
 		DefaultMysqlInspect(),
 		`select * from exist_tb_2`,
 		newTestResult(),
