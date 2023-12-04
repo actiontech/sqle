a40fe4f4feeb5910dd72a35db17d7ab21482ca62
taolx0 <taolx25@foxmail.com>
2023-12-04 10:27:18 +0800

    Revert "Revert "chore:temp remove endpoint""
    This reverts commit 9fcbd537f8453529da5ed58e2c2eb313b6ea8f06.

--- sqle/api/controller/v1/sql_manager_ee.go
+++ sqle/api/controller/v1/sql_manager_ee.go
@@ -115,8 +115,7 @@ func convertToGetSqlManageListResp(sqlManageList []*model.SqlManageDetail) []*Sq
 
 		sqlMgr.Status = sqlManage.Status
 		sqlMgr.Remark = sqlManage.Remark
-		sqlMgr.Endpoint = sqlManage.Endpoint
-
+		// todo: 端点信息数组
 		sqlManageRespList = append(sqlManageRespList, sqlMgr)
 	}
 
@@ -260,7 +259,7 @@ func exportSqlManagesV1(c echo.Context) error {
 			sqlManage.FirstAppearTime(),
 			sqlManage.LastReceiveTime(),
 			strconv.FormatUint(sqlManage.FpCount, 10),
-			sqlManage.Endpoint,
+			// todo: 端点信息数组
 			strings.Join(assignees, ","),
 			model.SqlManageStatusMap[sqlManage.Status],
 			sqlManage.Remark,
