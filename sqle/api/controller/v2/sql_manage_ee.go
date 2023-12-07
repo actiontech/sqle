def10ee096ce4efa8b22d5b520150d7671fe67e2
taolx0 <taolx25@foxmail.com>
2023-12-07 15:16:38 +0800

    Add 'endpoints' field and grouping to SqlManageDetail
    Implemented changes to SqlManageDetail model, including addition of 'endpoints' field. Adjustments have been made for grouping matching endpoints together, utilizing the GROUP_CONCAT function. A new filter has also been introduced to enable fuzzy search on the basis of endpoint.

---
+++ sqle/api/controller/v2/sql_manage_ee.go
@@ -0,0 +1,116 @@
+//go:build enterprise
+// +build enterprise
+
+package v2
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/actiontech/sqle/sqle/api/controller"
+	v1 "github.com/actiontech/sqle/sqle/api/controller/v1"
+	"github.com/actiontech/sqle/sqle/model"
+	"github.com/labstack/echo/v4"
+)
+
+func getSqlManageList(c echo.Context) error {
+	req := new(v1.GetSqlManageListReq)
+	if err := controller.BindAndValidateReq(c, req); err != nil {
+		return controller.JSONBaseErrorReq(c, err)
+	}
+
+	projectName := c.Param("project_name")
+
+	var offset uint32
+	if req.PageIndex > 0 {
+		offset = (req.PageIndex - 1) * req.PageSize
+	}
+
+	searchSqlFingerprint := ""
+	if req.FuzzySearchSqlFingerprint != nil {
+		searchSqlFingerprint = strings.Replace(*req.FuzzySearchSqlFingerprint, "'", "\\'", -1)
+	}
+
+	data := map[string]interface{}{
+		"fuzzy_search_sql_fingerprint":      searchSqlFingerprint,
+		"filter_assignee":                   req.FilterAssignee,
+		"filter_instance_name":              req.FilterInstanceName,
+		"filter_source":                     req.FilterSource,
+		"filter_audit_level":                req.FilterAuditLevel,
+		"filter_last_audit_start_time_from": req.FilterLastAuditStartTimeFrom,
+		"filter_last_audit_start_time_to":   req.FilterLastAuditStartTimeTo,
+		"filter_status":                     req.FilterStatus,
+		"project_name":                      projectName,
+		"filter_db_type":                    req.FilterDbType,
+		"filter_rule_name":                  req.FilterRuleName,
+		"fuzzy_search_endpoint":             req.FuzzySearchEndpoint,
+		"sort_field":                        req.SortField,
+		"sort_order":                        req.SortOrder,
+		"limit":                             req.PageSize,
+		"offset":                            offset,
+	}
+
+	s := model.GetStorage()
+	sqlManage, err := s.GetSqlManageListByReq(data)
+	if err != nil {
+		return controller.JSONBaseErrorReq(c, err)
+	}
+
+	return c.JSON(http.StatusOK, &GetSqlManageListResp{
+		BaseRes:               controller.NewBaseReq(nil),
+		Data:                  convertToGetSqlManageListResp(sqlManage.SqlManageList),
+		SqlManageTotalNum:     sqlManage.SqlManageTotalNum,
+		SqlManageBadNum:       sqlManage.SqlManageBadNum,
+		SqlManageOptimizedNum: sqlManage.SqlManageOptimizedNum,
+	})
+}
+
+func convertToGetSqlManageListResp(sqlManageList []*model.SqlManageDetail) []*SqlManage {
+	sqlManageRespList := make([]*SqlManage, 0, len(sqlManageList))
+	for _, sqlManage := range sqlManageList {
+		sqlMgr := new(SqlManage)
+		sqlMgr.Id = uint64(sqlManage.ID)
+		sqlMgr.SqlFingerprint = sqlManage.SqlFingerprint
+		sqlMgr.Sql = sqlManage.SqlText
+		sqlMgr.InstanceName = sqlManage.InstanceName
+		sqlMgr.SchemaName = sqlManage.SchemaName
+
+		for i := range sqlManage.AuditResults {
+			ar := sqlManage.AuditResults[i]
+			sqlMgr.AuditResult = append(sqlMgr.AuditResult, &v1.AuditResult{
+				Level:    ar.Level,
+				Message:  ar.Message,
+				RuleName: ar.RuleName,
+			})
+		}
+
+		source := &v1.Source{Type: sqlManage.Source}
+		if sqlManage.ApName != nil {
+			source.AuditPlanName = *sqlManage.ApName
+		}
+		if len(sqlManage.SqlAuditRecordIDs) > 0 {
+			source.SqlAuditRecordIds = sqlManage.SqlAuditRecordIDs
+		}
+		sqlMgr.Source = source
+
+		if sqlManage.AppearTimestamp != nil {
+			sqlMgr.FirstAppearTimeStamp = sqlManage.AppearTimestamp.Format("2006-01-02 15:04:05")
+		}
+		if sqlManage.LastReceiveTimestamp != nil {
+			sqlMgr.LastReceiveTimeStamp = sqlManage.LastReceiveTimestamp.Format("2006-01-02 15:04:05")
+		}
+		sqlMgr.FpCount = sqlManage.FpCount
+
+		for _, assignee := range sqlManage.Assignees {
+			sqlMgr.Assignees = append(sqlMgr.Assignees, assignee)
+		}
+
+		sqlMgr.Status = sqlManage.Status
+		sqlMgr.Remark = sqlManage.Remark
+		sqlMgr.Endpoints = sqlManage.Endpoints
+
+		sqlManageRespList = append(sqlManageRespList, sqlMgr)
+	}
+
+	return sqlManageRespList
+}
