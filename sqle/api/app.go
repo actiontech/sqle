5cf0e331f5bdce3bf1580facf4131f0c5bd7b44b
yangzhongjiao <yangzhongjiao@actionsky.com>
2023-12-13 11:09:39 +0000

    revert interface  v2/sql_audit

--- sqle/api/app.go
+++ sqle/api/app.go
@@ -342,6 +342,7 @@ func StartApi(net *gracenet.Net, exitChan chan struct{}, config *config.SqleOpti
 
 		// sql audit
 		v1Router.POST("/sql_audit", v1.DirectAudit)
+		v2Router.POST("/sql_audit", v2.DirectAudit)
 		v1Router.POST("/audit_files", v1.DirectAuditFiles)
 		v2Router.POST("/audit_files", v2.DirectAuditFiles)
 		v1Router.GET("/sql_analysis", v1.DirectGetSQLAnalysis)
