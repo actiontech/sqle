ea1690d33a5a303ab5211ae0b7b592d3af61e045
taolx0 <taolx25@foxmail.com>
2023-12-20 15:53:27 +0800

    refactor:Move the GetSQLAnalysisResult function from package v2 to package V1

--- sqle/api/controller/v1/sql_manager_ee.go
+++ sqle/api/controller/v1/sql_manager_ee.go
@@ -5,6 +5,7 @@ package v1
 
 import (
 	"bytes"
+	"context"
 	"encoding/csv"
 	e "errors"
 	"fmt"
@@ -15,10 +16,14 @@ import (
 	"time"
 
 	"github.com/actiontech/sqle/sqle/api/controller"
+	"github.com/actiontech/sqle/sqle/common"
+	"github.com/actiontech/sqle/sqle/driver"
+	"github.com/actiontech/sqle/sqle/driver/v2"
 	"github.com/actiontech/sqle/sqle/errors"
 	"github.com/actiontech/sqle/sqle/model"
 	"github.com/actiontech/sqle/sqle/utils"
 	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
 )
 
 func getSqlManageList(c echo.Context) error {
@@ -344,4 +349,63 @@ func convertRuleTipsToResp(tips []*model.SqlManageRuleTips) []RuleTips {
 	}
 
 	return ruleResp
+}
+
+type AnalysisResult struct {
+	ExplainResult    *driverV2.ExplainResult
+	ExplainResultErr error
+
+	TableMetaResult    *driver.GetTableMetaBySQLResult
+	TableMetaResultErr error
+
+	AffectRowsResult    *driverV2.EstimatedAffectRows
+	AffectRowsResultErr error
+}
+
+func GetSQLAnalysisResult(l *logrus.Entry, instance *model.Instance, schema, sql string) (res *AnalysisResult, err error) {
+	dsn, err := common.NewDSN(instance, schema)
+	if err != nil {
+		return nil, err
+	}
+
+	plugin, err := driver.GetPluginManager().
+		OpenPlugin(l, instance.DbType, &driverV2.Config{DSN: dsn})
+	if err != nil {
+		return nil, err
+	}
+	defer plugin.Close(context.TODO())
+
+	res = &AnalysisResult{}
+	res.ExplainResult, res.ExplainResultErr = Explain(instance.DbType, plugin, sql)
+	res.TableMetaResult, res.TableMetaResultErr = GetTableMetas(instance.DbType, plugin, sql)
+	res.AffectRowsResult, res.AffectRowsResultErr = GetRowsAffected(instance.DbType, plugin, sql)
+
+	return res, nil
+}
+
+func Explain(dbType string, plugin driver.Plugin, sql string) (res *driverV2.ExplainResult, err error) {
+	if !driver.GetPluginManager().
+		IsOptionalModuleEnabled(dbType, driverV2.OptionalModuleExplain) {
+		return nil, driver.NewErrPluginAPINotImplement(driverV2.OptionalModuleExplain)
+	}
+
+	return plugin.Explain(context.TODO(), &driverV2.ExplainConf{Sql: sql})
+}
+
+func GetTableMetas(dbType string, plugin driver.Plugin, sql string) (res *driver.GetTableMetaBySQLResult, err error) {
+	if !driver.GetPluginManager().
+		IsOptionalModuleEnabled(dbType, driverV2.OptionalModuleGetTableMeta) {
+		return nil, driver.NewErrPluginAPINotImplement(driverV2.OptionalModuleGetTableMeta)
+	}
+
+	return plugin.GetTableMetaBySQL(context.TODO(), &driver.GetTableMetaBySQLConf{Sql: sql})
+}
+
+func GetRowsAffected(dbType string, plugin driver.Plugin, sql string) (res *driverV2.EstimatedAffectRows, err error) {
+	if !driver.GetPluginManager().
+		IsOptionalModuleEnabled(dbType, driverV2.OptionalModuleEstimateSQLAffectRows) {
+		return nil, driver.NewErrPluginAPINotImplement(driverV2.OptionalModuleEstimateSQLAffectRows)
+	}
+
+	return plugin.EstimateSQLAffectRows(context.TODO(), sql)
 }
