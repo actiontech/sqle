d233c5450c299700b93be5d5b5ed400435b7ec15
yangzhongjiao <yangzhongjiao@actionsky.com>
2024-03-20 11:10:43 +0000

    feat: record development SQL && implement development SQL record list

---
+++ sqle/model/sql_dev_record_ee.go
@@ -0,0 +1,149 @@
+//go:build enterprise
+// +build enterprise
+
+package model
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/jinzhu/gorm"
+)
+
+const (
+	SQLDevRecordSourceIDEPlugin = "ide_plugin"
+)
+
+var SqlDEVManageSourceMap = map[string]string{
+	SQLDevRecordSourceIDEPlugin: "IDE插件",
+}
+
+func (s *Storage) GetSqlDEVRecordListByReq(data map[string]interface{}) (list []*SQLDevRecord, total uint64, err error) {
+	sqlDEVRecords := make([]*SQLDevRecord, 0)
+
+	err = s.getListResult(sqlDEVRecordQueryTpl, sqlDEVRecordBodyTpl, data, &sqlDEVRecords)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	total, err = s.getCountResult(sqlDEVRecordBodyTpl, sqlDEVRecordTotalCount, data)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return sqlDEVRecords, total, nil
+}
+
+var sqlDEVRecordTotalCount = `
+SELECT COUNT(DISTINCT sdr.id)
+
+{{- template "body" . -}}
+`
+
+var sqlDEVRecordQueryTpl = `
+SELECT 
+	sdr.id,
+	sdr.sql_fingerprint,
+	sdr.sql_text,
+	sdr.source,
+	sdr.audit_level,
+	sdr.audit_results,
+	sdr.fp_count,
+    sdr.first_appear_timestamp,
+	sdr.last_receive_timestamp,
+	sdr.instance_name,
+	sdr.schema_name,
+	sdr.creator
+
+{{- template "body" . -}} 
+
+ORDER BY 
+{{- if and .sort_field .sort_order }}
+	{{ .sort_field }} {{ .sort_order }}
+{{- else }}
+	sdr.id desc
+{{- end }}
+
+{{- if .limit }}
+LIMIT :limit OFFSET :offset
+{{- end -}}
+`
+
+var sqlDEVRecordBodyTpl = `
+{{ define "body" }}
+
+FROM sql_dev_records sdr
+
+WHERE sdr.project_id = :project_id
+  AND sdr.deleted_at IS NULL
+
+{{- if .fuzzy_search_sql_fingerprint }}
+AND sdr.sql_fingerprint LIKE '%{{ .fuzzy_search_sql_fingerprint }}%'
+{{- end }}
+
+{{- if .filter_creator }}
+AND sdr.creator = :filter_creator
+{{- end }}
+
+{{- if .filter_instance_name }}
+AND sdr.instance_name = :filter_instance_name
+{{- end }}
+
+{{- if .filter_source }}
+AND sdr.source = :filter_source
+{{- end }}
+
+{{- if .filter_last_receive_time_from }}
+AND sdr.last_receive_timestamp >= :filter_last_receive_time_from
+{{- end }}
+
+{{- if .filter_last_receive_time_to }}
+AND sdr.last_receive_timestamp <= :filter_last_receive_time_to
+{{- end }}
+
+
+{{ end }}
+`
+
+func (s *Storage) InsertOrUpdateSqlDevRecord(SqlDevRecordList []*SQLDevRecord) error {
+	return s.Tx(func(tx *gorm.DB) error {
+		for batchSize, start := 50, 0; start < len(SqlDevRecordList); start += batchSize {
+			end := start + batchSize
+			if end > len(SqlDevRecordList) {
+				end = len(SqlDevRecordList)
+			}
+			batchSqlDevRecordList := SqlDevRecordList[start:end]
+
+			args := make([]interface{}, 0)
+			pattern := make([]string, 0)
+			for _, SqlDevRecord := range batchSqlDevRecordList {
+				pattern = append(pattern, "( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+				args = append(args, SqlDevRecord.SqlFingerprint, SqlDevRecord.ProjFpSourceInstSchemaMd5, SqlDevRecord.SqlText,
+					SqlDevRecord.Source, SqlDevRecord.AuditLevel, SqlDevRecord.AuditResults, SqlDevRecord.FpCount, SqlDevRecord.FirstAppearTimestamp,
+					SqlDevRecord.LastReceiveTimestamp, SqlDevRecord.InstanceName, SqlDevRecord.SchemaName, SqlDevRecord.Creator,
+					SqlDevRecord.ProjectId)
+			}
+
+			raw := fmt.Sprintf(`
+			INSERT INTO sql_dev_records (sql_fingerprint, proj_fp_source_inst_schema_md5, sql_text, source, audit_level, audit_results,
+			                        fp_count, first_appear_timestamp, last_receive_timestamp, instance_name, schema_name,
+			                        creator, project_id)
+					VALUES %s
+					ON DUPLICATE KEY UPDATE sql_text       = VALUES(sql_text),
+			                       audit_level            = VALUES(audit_level),
+			                       audit_results          = VALUES(audit_results),
+			                       fp_count 			   = fp_count + VALUES(fp_count),
+			                       first_appear_timestamp = VALUES(first_appear_timestamp),
+			                       last_receive_timestamp = VALUES(last_receive_timestamp);`,
+				strings.Join(pattern, ", "))
+
+			err := tx.Exec(raw, args...).Error
+			if err != nil {
+				return err
+			}
+
+		}
+
+		return nil
+	})
+}
