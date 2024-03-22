6fdae93fe6dc5af53289d58ec5e29a887d0d9c4e
WinfredLIN <linxiaotao1998@live.com>
2024-03-22 06:46:58 +0000

    model: add table related to task n:1, name is files

---
+++ sqle/model/task_files.go
@@ -0,0 +1,9 @@
+package model
+
+type File struct {
+	Model
+	TaskId     uint   `json:"-" gorm:"index"`
+	UniqueName string `json:"unique_name" gorm:"type:varchar(255)"`
+	FileHost   string `json:"file_host" gorm:"type:varchar(255)"`
+	NickName   string `json:"nick_name" gorm:"type:varchar(255)"`
+}
