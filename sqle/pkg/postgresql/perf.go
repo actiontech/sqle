ac38ce4c291ff76daec610c347bd990d1cc95f13
zhangsuyun2022 <107415572+zhangsuyun2022@users.noreply.github.com>
2024-03-22 16:41:52 +0800

    Update perf.go
    更新物理读和逻辑读的单位

--- sqle/pkg/postgresql/perf.go
+++ sqle/pkg/postgresql/perf.go
@@ -16,8 +16,8 @@ SELECT query as sql_fulltext,
 sum(calls) as executions,
 sum(total_exec_time) AS elapsed_time,
 sum(cpu_user_time) as cpu_time,
-sum(shared_blks_read) AS disk_reads,
-sum(shared_blks_hit) AS buffer_gets,
+sum(shared_blks_read) AS disk_reads, // 表示从共享缓冲区中读取的块数。这个值表示数据库系统从磁盘或其他存储介质中读取的数据块数量，而不是从内存中读取的数据。
+sum(shared_blks_hit) AS buffer_gets, // 表示从共享缓冲区中命中的块数。这个值表示数据库系统从内存中读取的数据块数量，而不是从磁盘或其他存储介质中读取的数据。
 sum(blk_read_time) as user_io_wait_time
 FROM pg_stat_monitor
 WHERE calls > 0
