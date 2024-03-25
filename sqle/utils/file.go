7c7780aa4fcdd0980fb53ec9d8cfede2c798e72a
WinfredLIN <linxiaotao1998@live.com>
2024-03-25 08:22:22 +0000

    feat: tool functions related to files

---
+++ sqle/utils/file.go
@@ -0,0 +1,72 @@
+package utils
+
+import (
+	"io"
+	"io/fs"
+	"os"
+)
+
+const FileModeReadWriteAll fs.FileMode = 0666
+
+func EnsureFilePathWithPermission(filePath string, perm os.FileMode) error {
+	_, err := os.Stat(filePath)
+	if os.IsNotExist(err) {
+		err = MkdirWithPermission(filePath, perm)
+		if err != nil {
+			return err
+		}
+	} else {
+		err = EnsureFilePermission(filePath, perm)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func MkdirWithPermission(filePath string, perm os.FileMode) error {
+	err := os.Mkdir(filePath, os.ModeDir)
+	if err != nil {
+		return err
+	}
+	err = os.Chmod(filePath, perm)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func EnsureFilePermission(filePath string, perm os.FileMode) error {
+	info, err := os.Stat(filePath)
+	if err != nil {
+		return err
+	}
+	currentPerm := info.Mode().Perm()
+	// 使用按位与来检查每一个权限位是否满足要求
+	if currentPerm&perm != perm {
+		return os.Chmod(filePath, perm)
+	}
+	return nil
+}
+
+// SaveFile 从 io.ReadSeeker 读取内容并保存到指定路径的文件中。
+func SaveFile(file io.ReadSeeker, targetPath string) (err error) {
+	// 从文件头开始读取
+	_, err = file.Seek(0, io.SeekStart)
+	if err != nil {
+		return err
+	}
+
+	// 创建目标文件
+	target, err := os.Create(targetPath)
+	if err != nil {
+		return err
+	}
+
+	// 保存文件
+	_, err = io.Copy(target, file)
+	if err != nil {
+		return err
+	}
+	return nil
+}
