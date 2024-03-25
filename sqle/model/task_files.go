f8fae57bdf68529d26d1a499f05214194cd866a8
WinfredLIN <linxiaotao1998@live.com>
2024-03-25 09:08:54 +0000

    feat: clean job for all sqle node to clean expired files

--- sqle/model/task_files.go
+++ sqle/model/task_files.go
@@ -43,3 +43,31 @@ func (s *Storage) GetFileByTaskId(taskId string) ([]*File, error) {
 	}
 	return files, errors.New(errors.ConnectStorageError, err)
 }
+
+// expired time 24hour
+func (s *Storage) GetExpiredFileWithNoWorkflow() ([]File, error) {
+	files := []File{}
+	err := s.db.Model(&File{}).
+		Joins("LEFT JOIN workflow_instance_records wir ON files.task_id = wir.task_id").
+		Where("wir.task_id IS NULL AND files.deleted_at IS NULL"). // 删除没有提交为工单的SQL文件
+		Where("files.file_host = ?", config.GetOptions().SqleOptions.ReportHost).
+		Where("files.created_at < ?", time.Now().Add(-24*time.Hour)). // 减少提交前文件就被删除的几率
+		Find(&files).Error
+	if err == gorm.ErrRecordNotFound {
+		return nil, nil
+	}
+	return files, errors.New(errors.ConnectStorageError, err)
+}
+
+// expired time 7*24hour
+func (s *Storage) GetExpiredFile() ([]File, error) {
+	files := []File{}
+	err := s.db.Model(&File{}).
+		Where("files.file_host = ?", config.GetOptions().SqleOptions.ReportHost).
+		Where("files.created_at < ?", time.Now().Add(-7*24*time.Hour)). // 减少提交前文件就被删除的几率
+		Find(&files).Error
+	if err == gorm.ErrRecordNotFound {
+		return nil, nil
+	}
+	return files, errors.New(errors.ConnectStorageError, err)
+}
