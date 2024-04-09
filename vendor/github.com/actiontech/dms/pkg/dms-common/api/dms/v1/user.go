b93a5cba26e3a1764a0eb20217ae97e71f161221
huangdongcheng <huangdongcheng@actionsky.com>
2024-04-09 07:57:21 +0000

    update vendor

--- vendor/github.com/actiontech/dms/pkg/dms-common/api/dms/v1/user.go
+++ vendor/github.com/actiontech/dms/pkg/dms-common/api/dms/v1/user.go
@@ -38,6 +38,14 @@ type GetUser struct {
 	// user bind name space
 	UserBindProjects   []UserBindProject `json:"user_bind_projects"`
 	ThirdPartyUserInfo string            `json:"third_party_user_info"`
+	// access token
+	AccessTokenInfo AccessTokenInfo `json:"access_token_info"`
+}
+
+type AccessTokenInfo struct {
+	AccessToken string `json:"access_token"`
+	ExpiredTime string `json:"token_expired_timestamp" example:"RFC3339"`
+	IsExpired   bool   `json:"is_expired"`
 }
 
 type UserBindProject struct {
@@ -229,7 +237,7 @@ func GetOperationTypeDesc(opType OpPermissionType) string {
 	case OpPermissionTypeSQLQuery:
 		return "SQL查询"
 	default:
-		return "不支持的操作类型"
+		return "未知操作类型"
 	}
 }
 
@@ -285,6 +293,8 @@ type ListUser struct {
 	OpPermissions []UidWithName `json:"op_permissions"`
 	// user is deleted
 	IsDeleted bool `json:"is_deleted"`
+	// third party user info
+	ThirdPartyUserInfo string `json:"third_party_user_info"`
 }
 
 // swagger:model ListUserReply
@@ -292,6 +302,20 @@ type ListUserReply struct {
 	// List user reply
 	Data  []*ListUser `json:"data"`
 	Total int64       `json:"total_nums"`
+
+	// Generic reply
+	base.GenericResp
+}
+
+// swagger:parameters GenAccessToken
+type GenAccessToken struct {
+	ExpirationDays string `param:"expiration_days" json:"expiration_days" validate:"required"`
+}
+
+// swagger:model GenAccessTokenReply
+type GenAccessTokenReply struct {
+	// Get user reply
+	Data *AccessTokenInfo `json:"data"`
 
 	// Generic reply
 	base.GenericResp
