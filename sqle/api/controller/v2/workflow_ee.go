84fbd4ea96d51ecce6df1b76de35e78fc27972af
bianyucheng <bianyucheng0@gmail.com>
2024-04-12 16:55:20 +0800

    workflow scheduled use notify

---
+++ sqle/api/controller/v2/workflow_ee.go
@@ -0,0 +1,28 @@
+//go:build enterprise
+// +build enterprise
+
+package v2
+
+import "github.com/actiontech/sqle/sqle/model"
+
+const (
+	NotifyTypeWechat = "Wechat"
+)
+
+func createNotifyRecord(notifyType string, curTaskRecord *model.WorkflowInstanceRecord) error {
+	s := model.GetStorage()
+	switch notifyType {
+	case NotifyTypeWechat:
+		record := model.WechatRecord{
+			TaskId: curTaskRecord.TaskId,
+		}
+		if err := s.Save(&record); err != nil {
+			return nil
+		}
+		err := s.UpdateWorkflowInstanceRecordById(curTaskRecord.ID, map[string]interface{}{"need_scheduled_task_notify": true})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
