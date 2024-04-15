836f93c80b51fd69d3ab017c3a2b4e63a3b5aa96
yangzhongjiao <yangzhongjiao@actionsky.com>
2024-04-15 09:52:29 +0000

    feat: ce func implement

---
+++ sqle/api/controller/v1/optimization_ce.go
@@ -0,0 +1,31 @@
+//go:build !enterprise
+// +build !enterprise
+
+package v1
+
+import (
+	e "errors"
+
+	"github.com/actiontech/sqle/sqle/errors"
+	"github.com/labstack/echo/v4"
+)
+
+func sqlOptimizate(c echo.Context) error {
+	return errors.New(errors.SQLOptimizationCommunityNotSupported, e.New("sql optimization community not supported"))
+}
+
+func getOptimizationRecord(c echo.Context) error {
+	return errors.New(errors.SQLOptimizationCommunityNotSupported, e.New("sql optimization community not supported"))
+}
+
+func getOptimizationRecords(c echo.Context) error {
+	return errors.New(errors.SQLOptimizationCommunityNotSupported, e.New("sql optimization community not supported"))
+}
+
+func getOptimizationSQL(c echo.Context) error {
+	return errors.New(errors.SQLOptimizationCommunityNotSupported, e.New("sql optimization community not supported"))
+}
+
+func getOptimizationSQLs(c echo.Context) error {
+	return errors.New(errors.SQLOptimizationCommunityNotSupported, e.New("sql optimization community not supported"))
+}
