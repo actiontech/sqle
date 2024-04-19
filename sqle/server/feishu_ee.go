8e72894919f805a740ec2a48dd140db25a38baae
bianyucheng <bianyucheng0@gmail.com>
2024-04-19 13:52:55 +0800

    add send oa and get scheduled oa

--- sqle/server/feishu_ee.go
+++ sqle/server/feishu_ee.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/actiontech/sqle/sqle/dms"
 	"github.com/actiontech/sqle/sqle/model"
+	"github.com/actiontech/sqle/sqle/pkg/im"
 	imPkg "github.com/actiontech/sqle/sqle/pkg/im"
 	"github.com/actiontech/sqle/sqle/pkg/im/feishu"
 	larkContact "github.com/larksuite/oapi-sdk-go/v3/service/contact/v3"
@@ -42,6 +43,16 @@ func (j *FeishuJob) feishuRotation(entry *logrus.Entry) {
 	if !im.IsEnable {
 		entry.Infof("im config is disabled")
 		return
+	}
+
+	err = sendFeishuScheduledApprove(entry)
+	if err != nil {
+		entry.Errorf("send feishu scheduled approve error: %v", err)
+	}
+
+	err = updateFeishuScheduledTask(entry, im)
+	if err != nil {
+		entry.Errorf("update feishu scheduled approve error: %v", err)
 	}
 
 	instList, err := s.GetFeishuInstByStatus(model.FeishuAuditStatusInitialized)
@@ -161,4 +172,71 @@ func getSqleUserByFeishuUserID(client *feishu.FeishuClient, userId string, assig
 	}
 
 	return nil, fmt.Errorf("user not found, userId: %s", userId)
+}
+
+func sendFeishuScheduledApprove(entry *logrus.Entry) error {
+	st := model.GetStorage()
+	workflows, err := st.GetNeedScheduledWorkflows()
+	if err != nil {
+		return fmt.Errorf("get need scheduled workflows from storage error: %v", err)
+	}
+
+	for _, workflow := range workflows {
+		w, err := dms.GetWorkflowDetailByWorkflowId(string(workflow.ProjectId), workflow.WorkflowId, st.GetWorkflowDetailWithoutInstancesByWorkflowID)
+		if err != nil {
+			return fmt.Errorf("get workflow from storage error: %v", err)
+		}
+		taskIds, err := w.GetNeedSendOATaskIds(entry)
+		if err != nil {
+			return err
+		}
+		records, err := st.GetFeishuRecordsByTaskIds(taskIds)
+		if err != nil {
+			return fmt.Errorf("get feishu record failed, taskIDs:%v, err:%v", taskIds, err)
+		}
+
+		needSendOATaskIds := []uint{}
+		for _, r := range records {
+			if r.ApproveInstanceCode == "" {
+				needSendOATaskIds = append(needSendOATaskIds, r.TaskId)
+			}
+		}
+
+		for _, taskId := range needSendOATaskIds {
+			im.CreateScheduledApprove(taskId, string(w.ProjectId), w.WorkflowId, model.ImTypeFeishuAudit)
+		}
+	}
+
+	return nil
+}
+
+func updateFeishuScheduledTask(entry *logrus.Entry, im *model.IM) error {
+	s := model.GetStorage()
+
+	records, err := s.GetFeishuScheduledByStatus(model.FeishuAuditStatusInitialized)
+	if err != nil {
+		return fmt.Errorf("get feishu record by status error: %v", err)
+	}
+	client := feishu.NewFeishuClient(im.AppKey, im.AppSecret)
+	for _, record := range records {
+		instDetail, err := client.GetApprovalInstDetail(context.TODO(), record.ApproveInstanceCode)
+		if err != nil {
+			entry.Errorf("get feishu approval record detail error: %v", err)
+			continue
+		}
+
+		switch *instDetail.Status {
+		case model.FeishuAuditStatusApprove:
+			if err := s.FeishuAgreeScheduledTask(record); err != nil {
+				entry.Errorf("save feishu record error: %v", err)
+				continue
+			}
+		case model.FeishuAuditStatusRejected:
+			if err := s.FeishuCancelScheduledTask(record); err != nil {
+				entry.Errorf("save feishu record error: %v", err)
+				continue
+			}
+		}
+	}
+	return nil
 }
