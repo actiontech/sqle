d379d34a6ccf33ecb279fe0e02d1babc3ecaceb0
bianyucheng <bianyucheng0@gmail.com>
2024-04-19 13:51:58 +0800

    add feishu

--- sqle/api/controller/v2/workflow_ee.go
+++ sqle/api/controller/v2/workflow_ee.go
@@ -7,6 +7,7 @@ import "github.com/actiontech/sqle/sqle/model"
 
 const (
 	NotifyTypeWechat = "Wechat"
+	NotifyTypeFeishu = "feishu"
 )
 
 func createNotifyRecord(notifyType string, curTaskRecord *model.WorkflowInstanceRecord) error {
@@ -14,6 +15,17 @@ func createNotifyRecord(notifyType string, curTaskRecord *model.WorkflowInstance
 	switch notifyType {
 	case NotifyTypeWechat:
 		record := model.WechatRecord{
+			TaskId: curTaskRecord.TaskId,
+		}
+		if err := s.Save(&record); err != nil {
+			return nil
+		}
+		err := s.UpdateWorkflowInstanceRecordById(curTaskRecord.ID, map[string]interface{}{"need_scheduled_task_notify": true})
+		if err != nil {
+			return err
+		}
+	case NotifyTypeFeishu:
+		record := model.FeishuScheduledRecord{
 			TaskId: curTaskRecord.TaskId,
 		}
 		if err := s.Save(&record); err != nil {
