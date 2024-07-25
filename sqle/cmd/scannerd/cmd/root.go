c990d0ed4c02596a04b756b1725cee8747ab39e8
taolx0 <taolx25@foxmail.com>
2024-07-25 18:00:13 +0800

    chore:The function of scanner for reviewing and removing SQL files and MyBatis files uploaded to the SQLE scanning task has been adjusted to directly upload SQL files or MyBatis files to the SQLE quick review module.

--- sqle/cmd/scannerd/cmd/root.go
+++ sqle/cmd/scannerd/cmd/root.go
@@ -1,8 +1,8 @@
 package cmd
 
 import (
-	"fmt"
 	"context"
+	"fmt"
 
 	pkgScanner "github.com/actiontech/sqle/sqle/pkg/scanner"
 
@@ -33,7 +33,6 @@ func init() {
 	rootCmd.PersistentFlags().StringVarP(&rootCmdFlags.token, "token", "A", "", "sqle token")
 	rootCmd.PersistentFlags().IntVarP(&rootCmdFlags.timeout, "timeout", "T", pkgScanner.DefaultTimeoutNum, "request sqle timeout in seconds")
 	rootCmd.PersistentFlags().StringVarP(&rootCmdFlags.project, "project", "J", "default", "project name")
-	_ = rootCmd.MarkPersistentFlagRequired("name")
 	_ = rootCmd.MarkPersistentFlagRequired("token")
 }
 
--- sqle/cmd/scannerd/scanners/sql_file/sqlfile.go
+++ sqle/cmd/scannerd/scanners/sql_file/sqlfile.go
@@ -2,11 +2,12 @@ package sqlFile
 
 import (
 	"context"
+	"fmt"
+
 	"github.com/actiontech/sqle/sqle/utils"
 
 	"github.com/actiontech/sqle/sqle/cmd/scannerd/scanners"
 	"github.com/actiontech/sqle/sqle/cmd/scannerd/scanners/common"
-	driverV2 "github.com/actiontech/sqle/sqle/driver/v2"
 	"github.com/actiontech/sqle/sqle/pkg/scanner"
 
 	"github.com/sirupsen/logrus"
@@ -16,77 +17,54 @@ type SQLFile struct {
 	l *logrus.Entry
 	c *scanner.Client
 
-	sqls []scanners.SQL
-
-	allSQL []driverV2.Node
-	getAll chan struct{}
-
-	apName           string
 	sqlDir           string
 	skipErrorSqlFile bool
 	skipAudit        bool
+	dbType           string
+	instName         string
+	schemaName       string
 }
 
 type Params struct {
 	SQLDir           string
-	APName           string
 	SkipErrorQuery   bool
 	SkipErrorSqlFile bool
 	SkipAudit        bool
+	DbType           string
+	InstName         string
+	SchemaName       string
 }
 
 func New(params *Params, l *logrus.Entry, c *scanner.Client) (*SQLFile, error) {
 	return &SQLFile{
 		sqlDir:           params.SQLDir,
-		apName:           params.APName,
 		skipErrorSqlFile: params.SkipErrorSqlFile,
 		skipAudit:        params.SkipAudit,
+		dbType:           params.DbType,
+		instName:         params.InstName,
+		schemaName:       params.SchemaName,
 		l:                l,
 		c:                c,
-		getAll:           make(chan struct{}),
 	}, nil
 }
 
 func (sf *SQLFile) Run(ctx context.Context) error {
 	sqls, err := common.GetSQLFromPath(sf.sqlDir, false, sf.skipErrorSqlFile, utils.SQLFileSuffix)
 	if err != nil {
-		return err
-	}
-
-	sf.allSQL = sqls
-	close(sf.getAll)
-
-	<-ctx.Done()
-	return nil
-}
-
-func (sf *SQLFile) SQLs() <-chan scanners.SQL {
-	// todo: channel size configurable
-	sqlCh := make(chan scanners.SQL, 10240)
-
-	go func() {
-		<-sf.getAll
-		for _, sql := range sf.allSQL {
-			sqlCh <- scanners.SQL{
-				Fingerprint: sql.Fingerprint,
-				RawText:     sql.Text,
-			}
-		}
-		close(sqlCh)
-	}()
-	return sqlCh
-}
-
-func (sf *SQLFile) Upload(ctx context.Context, sqls []scanners.SQL) error {
-	sf.sqls = append(sf.sqls, sqls...)
-	err := common.Upload(ctx, sf.sqls, sf.c, sf.apName)
-	if err != nil {
-		return err
+		return fmt.Errorf("failed to get sql from path: %v", err)
 	}
 
 	if sf.skipAudit {
 		return nil
 	}
 
-	return common.Audit(sf.c, sf.apName)
+	return common.DirectAudit(ctx, sf.c, sqls, sf.dbType, sf.instName, sf.schemaName)
+}
+
+func (sf *SQLFile) SQLs() <-chan scanners.SQL {
+	return nil
+}
+
+func (sf *SQLFile) Upload(ctx context.Context, sqls []scanners.SQL) error {
+	return nil
 }
