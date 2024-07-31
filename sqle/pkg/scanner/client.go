2c15385fbf636e625bf6287dab8ea52c85ac8c8b
LordofAvernus <yangzhongjiao@actionsky.com>
2024-07-31 06:52:55 +0000

    chore:The function of scanner for reviewing and removing SQL files and MyBatis files uploaded to the SQLE scanning task has been adjusted to directly upload SQL files or MyBatis files to the SQLE quick review module.

--- sqle/pkg/scanner/client.go
+++ sqle/pkg/scanner/client.go
@@ -31,6 +31,14 @@ const (
 	PartialUpload = "/sqle/v2/projects/%v/audit_plans/%s/sqls/partial"
 	// Get										                  %v=report_id
 	GetAuditReport = "/sqle/v1/projects/%v/audit_plans/%s/reports/%v/sqls?page_index=%d&page_size=%d"
+	// 创建sql审核记录
+	CreateSqlAudit = "/sqle/v1/projects/%v/sql_audit_records"
+	// 获取sql审核记录
+	GetSqlAudit = "/sqle/v1/projects/%v/sql_audit_records/%v/"
+	// 获取task sqls
+	GetTaskSQLs = "/sqle/v2/tasks/audits/%v/sqls?page_index=%d&page_size=%d"
+	// 获取所有项目
+	GetAllProjects = "/v1/dms/projects?page_index=%d&page_size=%d"
 )
 
 // %s = project name
@@ -48,6 +56,10 @@ type (
 	FullSyncAuditPlanSQLsReq    = v2.FullSyncAuditPlanSQLsReqV2
 	PartialSyncAuditPlanSQLsReq = v1.PartialSyncAuditPlanSQLsReqV1
 	TriggerAuditPlanRes         = v1.TriggerAuditPlanResV1
+	CreateSqlAuditReq           = v1.CreateSQLAuditRecordReqV1
+	CreateSqlAuditResp          = v1.CreateSQLAuditRecordResV1
+	GetSqlAuditResp             = v1.GetSQLAuditRecordResV1
+	GetAuditTaskSqls            = v2.GetAuditTaskSQLsResV2
 )
 
 type Client struct {
@@ -130,6 +142,151 @@ func (sc *Client) TriggerAuditReq(auditPlanName string) (string, error) {
 	return triggerRes.Data.Id, nil
 }
 
+func (sc *Client) DirectAudit(ctx context.Context, sqlAuditReq *CreateSqlAuditReq) error {
+	createSqlAuditResp, err := sc.CreateSqlAudit(ctx, sqlAuditReq)
+	if err != nil {
+		return fmt.Errorf("failed to create sql audit record, error: %s", err)
+	}
+
+	sqlAudit, err := sc.GetSqlAudit(ctx, createSqlAuditResp.Data.Id)
+	if err != nil {
+		return fmt.Errorf("failed to get sql audit record, error: %s", err)
+	}
+
+	projectID, err := sc.GetProjectUidByName(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get project uid by name, error: %s", err)
+	}
+
+	err = sc.GetTaskSQLs(ctx, sqlAudit.Data.Task.Id, sqlAudit.Data.SQLAuditRecordId, projectID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (sc *Client) GetProjectUidByName(ctx context.Context) (string, error) {
+	projectList, err := sc.GetProjectList(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	var projectID string
+	for _, project := range projectList.Data {
+		if project.Name == sc.project {
+			projectID = project.ProjectUid
+			break
+		}
+	}
+
+	return projectID, nil
+}
+
+func (sc *Client) CreateSqlAudit(ctx context.Context, sqlAuditReq *CreateSqlAuditReq) (*CreateSqlAuditResp, error) {
+	url := sc.baseURL + fmt.Sprintf(CreateSqlAudit, sc.project)
+	bodyBuf := &bytes.Buffer{}
+	encoder := json.NewEncoder(bodyBuf)
+	encoder.SetEscapeHTML(false)
+	err := encoder.Encode(&sqlAuditReq)
+	if err != nil {
+		return nil, err
+	}
+
+	resBody, err := sc.httpClient.sendRequest(ctx, url, http.MethodPost, sc.token, bytes.NewBuffer(bodyBuf.Bytes()))
+	if err != nil {
+		return nil, err
+	}
+
+	resp := new(CreateSqlAuditResp)
+	err = json.Unmarshal(resBody, &resp)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.Code != 0 {
+		return nil, fmt.Errorf("failed to request %s, error:%s", url, resp.Message)
+	}
+
+	return resp, nil
+}
+
+func (sc *Client) GetSqlAudit(ctx context.Context, sqlAuditRecordID string) (*GetSqlAuditResp, error) {
+	url := sc.baseURL + fmt.Sprintf(GetSqlAudit, sc.project, sqlAuditRecordID)
+	resBody, err := sc.httpClient.sendRequest(ctx, url, http.MethodGet, sc.token, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp := new(GetSqlAuditResp)
+	err = json.Unmarshal(resBody, &resp)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.Code != 0 {
+		return nil, fmt.Errorf("failed to request %s message: %s", url, resp.Message)
+	}
+
+	return resp, nil
+}
+
+func (sc *Client) GetTaskSQLs(ctx context.Context, taskID uint, sqlAuditRecordID, projectID string) error {
+	var pageIndex, pageSize, cursor uint64
+	pageIndex, pageSize = 1, 10
+	cursor = pageIndex * pageSize
+	var finalErr error
+	auditError := errors.New("audit result error")
+	var totalCount, errorCount, warningCount int
+
+	for {
+		url := sc.baseURL + fmt.Sprintf(GetTaskSQLs, taskID, pageIndex, pageSize)
+		resp, err := sc.httpClient.sendRequest(ctx, url, http.MethodGet, sc.token, nil)
+		if err != nil {
+			return err
+		}
+
+		taskSqlList := new(GetAuditTaskSqls)
+		err = json.Unmarshal(resp, taskSqlList)
+		if err != nil {
+			return err
+		}
+		if taskSqlList.Code != 0 {
+			return fmt.Errorf("failed to request %s,message: %s", url, taskSqlList.Message)
+		}
+		fmt.Println("---------------------------------------------------------")
+		for _, sql := range taskSqlList.Data {
+			totalCount++
+			fmt.Println(sql.ExecSQL)
+			for _, result := range sql.AuditResult {
+				fmt.Printf("[%s]%s\n", result.Level, result.Message)
+			}
+			fmt.Println("---------------------------------------------------------")
+
+			if sql.AuditLevel == "error" {
+				errorCount++
+				finalErr = auditError
+			}
+
+			if sql.AuditLevel == "warn" {
+				warningCount++
+			}
+		}
+
+		if cursor < taskSqlList.TotalNums {
+			pageIndex++
+			cursor = pageIndex * pageSize
+		} else {
+			break
+		}
+	}
+
+	fmt.Printf("total sqls: %d, error sqls: %d, warning sqls: %d, if you want to view the details, visit the link %s\n",
+		totalCount, errorCount, warningCount, sc.baseURL+fmt.Sprintf("/sqle/project/%s/sql-audit/detail/%s", projectID, sqlAuditRecordID))
+
+	return finalErr
+}
+
 func (sc *Client) GetAuditReportReq(auditPlanName string, reportID string) error {
 	var pageIndex, pageSize, cursor uint64
 	pageIndex, pageSize = 1, 10
@@ -169,6 +326,41 @@ func (sc *Client) GetAuditReportReq(auditPlanName string, reportID string) error
 	}
 
 	return finalErr
+}
+
+type GetProjectListResp struct {
+	controller.BaseRes
+	Data  []ListProject `json:"data"`
+	Total int64         `json:"total_nums"`
+}
+
+// A dms Project
+type ListProject struct {
+	// Project uid
+	ProjectUid string `json:"uid"`
+	// Project name
+	Name string `json:"name"`
+}
+
+func (sc *Client) GetProjectList(ctx context.Context) (*GetProjectListResp, error) {
+	url := sc.baseURL + GetAllProjects
+
+	resBody, err := sc.httpClient.sendRequest(ctx, fmt.Sprintf(url, 1, 999999), http.MethodGet, sc.token, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to request %s, error: %s", url, err)
+	}
+
+	resp := new(GetProjectListResp)
+	err = json.Unmarshal(resBody, &resp)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal response body, error: %s", err)
+	}
+
+	if resp.Code != 0 {
+		return nil, fmt.Errorf("failed to request %s message: %s", url, resp.Message)
+	}
+
+	return resp, nil
 }
 
 const (
