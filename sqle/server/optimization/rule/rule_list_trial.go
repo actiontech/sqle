1436c212c91ce238b91585d5119ae2a025ddbcf1
luowei <ranshaodelengshui@gmail.com>
2024-08-01 14:06:29 +0800

    Professional version should not show SQL optimization rules

---
+++ sqle/server/optimization/rule/rule_list_trial.go
@@ -0,0 +1,7 @@
+//go:build trial
+// +build trial
+
+package optimization
+
+var MySQLOptimizationRuleHandler = make([]OptimizationRuleHandler, 0)
+var OracleOptimizationRuleHandler = make([]OptimizationRuleHandler, 0)
