cdc33cf03dcc807bc840f02f781f21a031bbf36e
LordofAvernus <yangzhongjiao@actionsky.com>
2024-08-07 10:38:48 +0000

    update route url

--- sqle/api/app.go
+++ sqle/api/app.go
@@ -309,7 +309,7 @@ func StartApi(net *gracenet.Net, exitChan chan struct{}, config *config.SqleOpti
 		v1ProjectRouter.POST("/:project_name/tasks/audits", v1.CreateAndAuditTask)
 
 		// report push
-		v1ProjectAdminRouter.PUT("/:project_name/report_push_configs/", v1.UpdateReportPushConfig)
+		v1ProjectAdminRouter.PUT("/:project_name/report_push_configs/:report_push_config_id/", v1.UpdateReportPushConfig)
 		v1ProjectAdminRouter.GET("/:project_name/report_push_configs", v1.GetReportPushConfigList)
 	}
 
