a985afb424bdb1f2dc238a25c69d1b0850794f43
LordofAvernus <yangzhongjiao@actionsky.com>
2024-08-08 05:07:00 +0000

    init default push config

--- sqle/model/base.go
+++ sqle/model/base.go
@@ -3,6 +3,11 @@
 // - public const variables.
 package model
 
+import (
+	"database/sql/driver"
+	"encoding/json"
+)
+
 const (
 
 	// used by Model:
@@ -10,3 +15,14 @@ const (
 	Enabled  = 0
 	Disabled = 1
 )
+
+type Strings []string
+
+func (t *Strings) Scan(value interface{}) error {
+	bytesValue, _ := value.([]byte)
+	return json.Unmarshal(bytesValue, t)
+}
+
+func (t Strings) Value() (driver.Value, error) {
+	return json.Marshal(t)
+}
