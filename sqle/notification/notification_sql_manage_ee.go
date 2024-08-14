5a41afa6e008e50b98f24e3587b0003a69794507
LordofAvernus <yangzhongjiao@actionsky.com>
2024-08-14 09:30:23 +0000

    redirect route removal conditions

--- sqle/notification/notification_sql_manage_ee.go
+++ sqle/notification/notification_sql_manage_ee.go
@@ -1,6 +1,7 @@
 package notification
 
 import (
+	"context"
 	"fmt"
 	"strings"
 
@@ -35,8 +36,15 @@ func (a *SQLmanageRecordNotification) NotificationBody() string {
 	builder := strings.Builder{}
 	builder.WriteString(fmt.Sprintf("记录时间周期：%v - %v\n", a.Config.StartTime, a.Config.EndTime))
 	builder.WriteString(fmt.Sprintf(`所属项目  %v`, a.Config.ProjectName))
+
 	if a.Config.SQLEUrl != "" && a.Config.ProjectName != "" {
-		builder.WriteString(fmt.Sprintf("\n- SQL管控记录链接: %v/v2/projects/%v/sql_manages?filter_priority=hight&&filter_last_audit_start_time_from>%v&&filter_last_audit_start_time_to<%v", strings.TrimRight(a.Config.SQLEUrl, "/"), a.Config.ProjectName, a.Config.StartTime, a.Config.EndTime))
+		// TODO 当前只跳转到管控页面，无筛选条件
+		// builder.WriteString(fmt.Sprintf("\n- SQL管控记录链接: %v/v2/projects/%v/sql_manages?filter_priority=hight&filter_last_audit_start_time_from=%v&filter_last_audit_start_time_to=%v&page_index=1&page_size=20&filter_status=unhandled", strings.TrimRight(a.Config.SQLEUrl, "/"), a.Config.ProjectName, a.Config.StartTime, a.Config.EndTime))
+		projectId, err := dms.GetPorjectUIDByName(context.TODO(), a.Config.ProjectName)
+		if err == nil {
+			builder.WriteString(fmt.Sprintf("\n- SQL管控记录链接: %v/project/%v/sql-management", strings.TrimRight(a.Config.SQLEUrl, "/"), projectId))
+		}
+
 	}
 	for _, sqlManagerRecord := range a.SQLmanageRecords {
 		db := dms.GetInstancesByIdWithoutError(sqlManagerRecord.InstanceID)
