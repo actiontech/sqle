48f77b0dc0b889917ef5710e5449eb057d9857d5
WinfredLIN <linxiaotao1998@live.com>
2024-09-26 16:16:47 +0800

    add restriction: a workflow can only be bound to one stage.

--- sqle/model/workflow_ee.go
+++ sqle/model/workflow_ee.go
@@ -17,16 +17,13 @@ type WorkflowWithInstanceID struct {
 	InstanceIDs RowList `json:"instance_ids"`
 }
 
-func (s *Storage) GetWorkflowsThatCanBeAssociatedToStage(instanceIdRange []uint64, excludeWorkflowIds []string) ([]*WorkflowWithInstanceID, error) {
+func (s *Storage) GetWorkflowsThatCanBeAssociatedToStage(instanceIdRange []uint64) ([]*WorkflowWithInstanceID, error) {
 	if len(instanceIdRange) == 0 {
 		return nil, fmt.Errorf("can not get workflow that need to associate to stage without instance id")
 	}
 	workflows := []*WorkflowWithInstanceID{}
 	data := map[string]interface{}{
 		"filter_instance_id": instanceIdRange,
-	}
-	if len(excludeWorkflowIds) > 0 {
-		data["filter_workflow_id"] = excludeWorkflowIds
 	}
 	err := s.getListResult(getWorkflowByInstanceIDsBodyTpl, getWorkflowByInstanceIDsQueryBodyTpl, data, &workflows)
 	if err != nil {
@@ -47,14 +44,14 @@ LEFT JOIN workflow_records
 ON workflows.workflow_record_id = workflow_records.id
 LEFT JOIN workflow_instance_records 
 ON workflow_instance_records.workflow_record_id = workflow_records.id
+LEFT JOIN workflow_version_stages 
+ON workflows.workflow_id = workflow_version_stages.workflow_id
 {{- template "body" . -}}
 `
 
 var getWorkflowByInstanceIDsQueryBodyTpl = `
 {{ define "body" }}
-{{- if .filter_workflow_id }}
-WHERE workflows.workflow_id NOT IN ( {{range $index, $element := .filter_workflow_id}}{{if $index}},{{end}}{{$element}}{{end}} )
-{{- end }}
+WHERE workflow_version_stages.workflow_id IS NULL
 GROUP BY workflows.id
 HAVING COUNT(CASE WHEN workflow_instance_records.instance_id NOT IN ( {{range $index, $element := .filter_instance_id}}{{if $index}},{{end}}{{$element}}{{end}} ) THEN 1 END) = 0
 ORDER BY workflows.created_at DESC
