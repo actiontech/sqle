54eadec434f0fef3a0646be0c8007903ab37429e
iwanghc <eternity5701@163.com>
2024-09-27 15:59:13 +0800

    add annotation：sql version add annotation

--- sqle/model/sql_version.go
+++ sqle/model/sql_version.go
@@ -22,9 +22,10 @@ const (
 
 type SqlVersionStage struct {
 	Model
-	SqlVersionID  uint   `json:"sql_version_id" gorm:"not null"`
-	Name          string `json:"name" gorm:"type:varchar(255) not null"`
-	StageSequence int    `json:"stage_sequence" gorm:"type:int not null"`
+	SqlVersionID uint   `json:"sql_version_id" gorm:"not null"`
+	Name         string `json:"name" gorm:"type:varchar(255) not null"`
+	// stage_sequence标识版本阶段的顺序，是一段从1开始连续的int值
+	StageSequence int `json:"stage_sequence" gorm:"type:int not null"`
 
 	SqlVersionStagesDependency []*SqlVersionStagesDependency
 	WorkflowVersionStage       []*WorkflowVersionStage
@@ -40,9 +41,11 @@ type SqlVersionStagesDependency struct {
 
 type WorkflowVersionStage struct {
 	Model
-	WorkflowID            string     `json:"workflow_id" gorm:"not null"`
-	SqlVersionID          uint       `json:"sql_version_id"`
-	SqlVersionStageID     uint       `json:"sql_version_stage_id"`
+	WorkflowID        string `json:"workflow_id" gorm:"not null"`
+	SqlVersionID      uint   `json:"sql_version_id"`
+	SqlVersionStageID uint   `json:"sql_version_stage_id"`
+	// workflow_sequence标识工单所处阶段的顺序及占位，当工单不需要发布到下一阶段时（如工单关闭），下一阶段的workflow_sequence可能是不连续的
+	// 同一版本每个阶段之间的工单占位和顺序都相互对应，如：开发阶段发布到测试阶段，workflow_sequence为1的工单发布成功后workflow_sequence也为1
 	WorkflowSequence      int        `json:"workflow_sequence" gorm:"type:int"`
 	WorkflowReleaseStatus string     `json:"workflow_release_status" gorm:"type:varchar(255) not null"`
 	WorkflowExecTime      *time.Time `json:"workflow_exec_time" gorm:"type:datetime(3)"`
