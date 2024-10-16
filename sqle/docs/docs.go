3bcfcb46921635995cacf3c82237dcd16ad15598
WinfredLIN <linxiaotao1998@live.com>
2024-10-16 16:23:16 +0800

    docs: make swagger

--- sqle/docs/docs.go
+++ sqle/docs/docs.go
@@ -9010,6 +9010,24 @@ var doc = `{
                         "in": "query"
                     },
                     {
+                        "enum": [
+                            "wait_for_audit",
+                            "wait_for_execution",
+                            "rejected",
+                            "executing",
+                            "canceled",
+                            "exec_failed",
+                            "finished"
+                        ],
+                        "type": "array",
+                        "items": {
+                            "type": "string"
+                        },
+                        "description": "filter by workflow status,, support using many status",
+                        "name": "filter_status_list",
+                        "in": "query"
+                    },
+                    {
                         "type": "string",
                         "description": "filter by project uid",
                         "name": "filter_project_uid",
