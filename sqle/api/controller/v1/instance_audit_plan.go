7fa0cff1f758d6d25a8c1430f43dc800dd538ed6
taolx0 <taolx25@foxmail.com>
2024-10-24 18:03:13 +0800

    chore:add app extract link tips

--- sqle/api/controller/v1/instance_audit_plan.go
+++ sqle/api/controller/v1/instance_audit_plan.go
@@ -834,22 +834,32 @@ func GetAuditPlanExecCmd(projectName string, iap *model.InstanceAuditPlan, ap *m
 		return ""
 	}
 
-	scannerd, err := scannerCmd.GetScannerdCmd(ap.Type)
-	if err != nil {
-		logger.Infof("get scannerd %s failed %s", ap.Type, err)
-		return ""
+	var cmd string
+	switch ap.Type {
+	case auditplan.TypeAllAppExtract:
+		cmd = fmt.Sprintf(`SQLE_PROJECT_NAME=%s \
+PROJECT_APP_NAME=<Please provide the business parameters here> \
+java -<Please provide the java agent name parameters here> \
+-jar <Please provide the java app name parameters here>`, projectName)
+	default:
+		scannerd, err := scannerCmd.GetScannerdCmd(ap.Type)
+		if err != nil {
+			logger.Infof("get scannerd %s failed %s", ap.Type, err)
+			return ""
+		}
+		cmd, err = scannerd.GenCommand("./scannerd", map[string]string{
+			scannerCmd.FlagHost:        ip,
+			scannerCmd.FlagPort:        port,
+			scannerCmd.FlagProject:     projectName,
+			scannerCmd.FlagToken:       iap.Token,
+			scannerCmd.FlagAuditPlanID: fmt.Sprint(ap.ID),
+		})
+		if err != nil {
+			logger.Infof("generate scannerd %s command failed %s", ap.Type, err)
+			return ""
+		}
 	}
-	cmd, err := scannerd.GenCommand("./scannerd", map[string]string{
-		scannerCmd.FlagHost:        ip,
-		scannerCmd.FlagPort:        port,
-		scannerCmd.FlagProject:     projectName,
-		scannerCmd.FlagToken:       iap.Token,
-		scannerCmd.FlagAuditPlanID: fmt.Sprint(ap.ID),
-	})
-	if err != nil {
-		logger.Infof("generate scannerd %s command failed %s", ap.Type, err)
-		return ""
-	}
+
 	return cmd
 }
 
