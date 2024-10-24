accd9f800f0eceeae43f0d4efc47e0e8a5386310
WinfredLIN <linxiaotao1998@live.com>
2024-10-24 16:48:15 +0800

    optimize: code structure

--- sqle/api/controller/v1/red_dot_notification_ee.go
+++ sqle/api/controller/v1/red_dot_notification_ee.go
@@ -4,6 +4,8 @@
 package v1
 
 import (
+	"context"
+
 	"github.com/actiontech/dms/pkg/dms-common/dmsobject"
 	"github.com/actiontech/sqle/sqle/api/controller"
 	dms "github.com/actiontech/sqle/sqle/dms"
@@ -21,13 +23,6 @@ func (m GlobalDashBoardModule) Name() string {
 	return "global_dashboard"
 }
 
-var statusOfGlobalWorkflowRequireAttention []string = []string{
-	model.WorkflowStatusWaitForExecution,
-	model.WorkflowStatusExecFailed,
-	model.WorkflowStatusWaitForAudit,
-	model.WorkflowStatusReject,
-}
-
 func (m GlobalDashBoardModule) HasRedDot(ctx echo.Context) (bool, error) {
 	user, err := controller.GetCurrentUser(ctx, dms.GetUser)
 	if err != nil {
@@ -37,44 +32,39 @@ func (m GlobalDashBoardModule) HasRedDot(ctx echo.Context) (bool, error) {
 	if err != nil {
 		return false, err
 	}
-	// 2. 将用户权限信息，转化为全局待处理清单统一的用户可视范围
+	// 将用户权限信息，转化为全局待处理清单统一的用户可视范围
 	userVisibility := getGlobalDashBoardVisibilityOfUser(isAdmin, permissions)
-	// 查询待关注SQL是否有未处理的SQL
-	filter, err := constructGlobalSqlManageBasicFilter(ctx.Request().Context(), user, userVisibility, &globalSqlManageBasicFilter{})
+
+	has, err := HasSqlManageToHandle(ctx.Request().Context(), user, userVisibility)
+	if err != nil {
+		return false, err
+	}
+	if has {
+		return true, nil
+	}
+	has, err = HasOthersWorkflowToHandle(ctx.Request().Context(), user, userVisibility)
+	if err != nil {
+		return false, err
+	}
+	if has {
+		return true, nil
+	}
+
+	has, err = HasMyWorkflowToHandle(ctx.Request().Context(), user, userVisibility)
+	if err != nil {
+		return false, err
+	}
+	return has, nil
+}
+
+// 查询待关注工单，是否有未处理的工单
+func HasSqlManageToHandle(ctx context.Context, user *model.User, userVisibility GlobalDashBoardVisibility) (has bool, err error) {
+	filter, err := constructGlobalSqlManageBasicFilter(ctx, user, userVisibility, &globalSqlManageBasicFilter{})
 	if err != nil {
 		return false, err
 	}
 	s := model.GetStorage()
 	total, err := s.GetGlobalSqlManageTotalNum(filter)
-	if err != nil {
-		return false, err
-	}
-	if total > 0 {
-		return true, nil
-	}
-	// 查询待关注工单，是否有未处理的工单
-	filter, err = constructGlobalWorkflowBasicFilter(ctx.Request().Context(), user, userVisibility, &globalWorkflowBasicFilter{
-		FilterStatusList: statusOfGlobalWorkflowRequireAttention,
-	})
-	if err != nil {
-		return false, err
-	}
-	total, err = s.GetGlobalWorkflowTotalNum(filter)
-	if err != nil {
-		return false, err
-	}
-	if total > 0 {
-		return true, nil
-	}
-	// 查询创建的工单，是否有未处理的工单
-	filter, err = constructGlobalWorkflowBasicFilter(ctx.Request().Context(), user, userVisibility,&globalWorkflowBasicFilter{
-		FilterStatusList:   statusOfGlobalWorkflowRequireAttention,
-		FilterCreateUserId: user.GetIDStr(),
-	})
-	if err != nil {
-		return false, err
-	}
-	total, err = s.GetGlobalWorkflowTotalNum(filter)
 	if err != nil {
 		return false, err
 	}
