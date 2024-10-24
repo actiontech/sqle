c8d4d373836124bb3a153646215de3b087cdbd95
WinfredLIN <linxiaotao1998@live.com>
2024-10-24 16:48:11 +0800

    optimize: code structure

--- sqle/api/controller/v1/red_dot_notification.go
+++ sqle/api/controller/v1/red_dot_notification.go
@@ -1,6 +1,9 @@
 package v1
 
 import (
+	"context"
+
+	"github.com/actiontech/sqle/sqle/model"
 	"github.com/labstack/echo/v4"
 )
 
@@ -33,4 +36,50 @@ func GetSystemModuleRedDotsList(ctx echo.Context) ([]*RedDot, error) {
 		}
 	}
 	return redDots, nil
+}
+
+var statusOfGlobalWorkflowToBeFiltered []string = []string{
+	model.WorkflowStatusWaitForExecution,
+	model.WorkflowStatusExecFailed,
+	model.WorkflowStatusWaitForAudit,
+	model.WorkflowStatusReject,
+}
+
+// 查询待关注工单，是否有未处理的工单
+func HasOthersWorkflowToHandle(ctx context.Context, user *model.User, userVisibility GlobalDashBoardVisibility) (has bool, err error) {
+	filter, err := constructGlobalWorkflowBasicFilter(ctx, user, userVisibility, &globalWorkflowBasicFilter{
+		FilterStatusList: statusOfGlobalWorkflowToBeFiltered,
+	})
+	if err != nil {
+		return false, err
+	}
+	s := model.GetStorage()
+	total, err := s.GetGlobalWorkflowTotalNum(filter)
+	if err != nil {
+		return false, err
+	}
+	if total > 0 {
+		return true, nil
+	}
+	return false, nil
+}
+
+// 查询创建的工单，是否有未处理的工单
+func HasMyWorkflowToHandle(ctx context.Context, user *model.User, userVisibility GlobalDashBoardVisibility) (has bool, err error) {
+	filter, err := constructGlobalWorkflowBasicFilter(ctx, user, userVisibility, &globalWorkflowBasicFilter{
+		FilterStatusList:   statusOfGlobalWorkflowToBeFiltered,
+		FilterCreateUserId: user.GetIDStr(),
+	})
+	if err != nil {
+		return false, err
+	}
+	s := model.GetStorage()
+	total, err := s.GetGlobalWorkflowTotalNum(filter)
+	if err != nil {
+		return false, err
+	}
+	if total > 0 {
+		return true, nil
+	}
+	return false, nil
 }
