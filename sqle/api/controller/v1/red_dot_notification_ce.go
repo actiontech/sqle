6240108f1de490a0aaa13242befa64694ce6deb9
WinfredLIN <linxiaotao1998@live.com>
2024-10-24 16:48:11 +0800

    optimize: code structure

--- sqle/api/controller/v1/red_dot_notification_ce.go
+++ sqle/api/controller/v1/red_dot_notification_ce.go
@@ -37,37 +37,19 @@ func (m GlobalDashBoardModule) HasRedDot(ctx echo.Context) (bool, error) {
 	if err != nil {
 		return false, err
 	}
-	// 2. 将用户权限信息，转化为全局待处理清单统一的用户可见性
+	// 将用户权限信息，转化为全局待处理清单统一的用户可见性
 	userVisibility := getGlobalDashBoardVisibilityOfUser(isAdmin, permissions)
-	// 查询待关注工单，是否有未处理的工单
-	filter, err := constructGlobalWorkflowBasicFilter(ctx.Request().Context(), user, userVisibility,&globalWorkflowBasicFilter{
-		FilterStatusList: statusOfGlobalWorkflowRequireAttention,
-	})
+	has, err := HasOthersWorkflowToHandle(ctx.Request().Context(), user, userVisibility)
 	if err != nil {
 		return false, err
 	}
-	s := model.GetStorage()
-	total, err := s.GetGlobalWorkflowTotalNum(filter)
-	if err != nil {
-		return false, err
-	}
-	if total > 0 {
+	if has {
 		return true, nil
 	}
-	// 查询创建的工单，是否有未处理的工单
-	filter, err = constructGlobalWorkflowBasicFilter(ctx.Request().Context(), user, userVisibility,  &globalWorkflowBasicFilter{
-		FilterStatusList:   statusOfGlobalWorkflowRequireAttention,
-		FilterCreateUserId: user.GetIDStr(),
-	})
+
+	has, err = HasMyWorkflowToHandle(ctx.Request().Context(), user, userVisibility)
 	if err != nil {
 		return false, err
 	}
-	total, err = s.GetGlobalWorkflowTotalNum(filter)
-	if err != nil {
-		return false, err
-	}
-	if total > 0 {
-		return true, nil
-	}
-	return false, nil
+	return has, nil
 }
