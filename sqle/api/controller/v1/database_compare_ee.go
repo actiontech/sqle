5119fb83ee8c85211c3d1fe9f9fc2eabbaacfcd0
iwanghc <eternity5701@163.com>
2024-10-31 16:29:13 +0800

    chore: execute database comparsion api impl

--- sqle/api/controller/v1/database_compare_ee.go
+++ sqle/api/controller/v1/database_compare_ee.go
@@ -4,18 +4,113 @@
 package v1
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/actiontech/sqle/sqle/api/controller"
+	dms "github.com/actiontech/sqle/sqle/dms"
+	driverV2 "github.com/actiontech/sqle/sqle/driver/v2"
+	"github.com/actiontech/sqle/sqle/errors"
+	"github.com/actiontech/sqle/sqle/locale"
+	"github.com/actiontech/sqle/sqle/model"
+	"github.com/actiontech/sqle/sqle/server/compare"
 	"github.com/labstack/echo/v4"
 )
 
 func getDatabaseComparison(c echo.Context) error {
+	req := new(GetDatabaseComparisonReqV1)
+	if err := controller.BindAndValidateReq(c, req); err != nil {
+		return controller.JSONBaseErrorReq(c, err)
+	}
+	if (req.BaseDBObject.SchemaName != nil && req.ComparisonDBObject.SchemaName == nil) ||
+		(req.BaseDBObject.SchemaName == nil && req.ComparisonDBObject.SchemaName != nil) {
+		return controller.JSONBaseErrorReq(c, errors.New(errors.DataConflict, fmt.Errorf("the base instance and comparison instance must be consistent")))
+	}
+	// 获取基准数据源和比对数据源实例信息
+	baseInst, err := getInstanceById(c, req.BaseDBObject.InstanceId)
+	if err != nil {
+		return controller.JSONBaseErrorReq(c, err)
+	}
+
+	comparedInst, err := getInstanceById(c, req.ComparisonDBObject.InstanceId)
+	if err != nil {
+		return controller.JSONBaseErrorReq(c, err)
+	}
+	schemaNames := make([]*driverV2.DatabasCompareSchemaInfo, 0, 1)
+	if req.BaseDBObject.SchemaName != nil && req.ComparisonDBObject.SchemaName != nil {
+		schemaNames = append(schemaNames, &driverV2.DatabasCompareSchemaInfo{
+			BaseSchemaName:     *req.BaseDBObject.SchemaName,
+			ComparedSchemaName: *req.ComparisonDBObject.SchemaName,
+		})
+	}
+	compared := &compare.Compared{
+		BaseInstance:     baseInst,
+		ComparedInstance: comparedInst,
+		ObjInfos:         schemaNames,
+	}
+	execCompareRes, err := compared.ExecDatabaseCompare()
+	if err != nil {
+		return controller.JSONBaseErrorReq(c, errors.New(errors.DataConflict, err))
+	}
+	comparedRes := make([]*SchemaObject, len(execCompareRes))
+	for i, compared := range execCompareRes {
+		comparedRes[i] = &SchemaObject{
+			BaseSchemaName:       compared.BaseSchemaName,
+			ComparisonSchemaName: compared.ComparisonSchemaName,
+			InconsistentNum:      compared.InconsistentNum,
+			ComparisonResult:     compared.ComparedResult,
+			DatabaseDiffObjects:  convertDatabaseDiffObject(compared.DatabaseDiffObjects),
+		}
+	}
 
 	return c.JSON(http.StatusOK, &DatabaseComparisonResV1{
 		BaseRes: controller.NewBaseReq(nil),
-		Data:    nil,
+		Data:    comparedRes,
 	})
+}
+
+func convertDatabaseDiffObject(comparedDiffs []*compare.DatabaseDiffObject) []*DatabaseDiffObject {
+	diffObjects := make([]*DatabaseDiffObject, len(comparedDiffs))
+	for j, diffObject := range comparedDiffs {
+		objects := make([]*ObjectDiffResult, len(diffObject.ObjectsDiffResults))
+		for m, object := range diffObject.ObjectsDiffResults {
+			objects[m] = &ObjectDiffResult{
+				ObjectName:       object.ObjectName,
+				ComparisonResult: object.ComparedResult,
+			}
+		}
+		diffObjects[j] = &DatabaseDiffObject{
+			ObjectType:         diffObject.ObjectType,
+			InconsistentNum:    diffObject.InconsistentNum,
+			ObjectsDiffResults: objects,
+		}
+	}
+	return diffObjects
+}
+
+func getInstanceById(c echo.Context, instanceID string) (*model.Instance, error) {
+	inst, exist, err := dms.GetInstancesById(c.Request().Context(), instanceID)
+	if err != nil {
+		return nil, errors.New(errors.DataConflict, err)
+	}
+	if !exist {
+		return nil, ErrInstanceNotExist
+	}
+	return inst, nil
+}
+
+func convertTaskAuditResults(c echo.Context, taskAuditResults model.AuditResults, dbType string) []*SQLAuditResult {
+	auditResults := make([]*SQLAuditResult, len(taskAuditResults))
+	for i, auditResult := range taskAuditResults {
+		auditResults[i] = &SQLAuditResult{
+			Level:               auditResult.Level,
+			Message:             auditResult.GetAuditMsgByLangTag(locale.Bundle.GetLangTagFromCtx(c.Request().Context())),
+			RuleName:            auditResult.RuleName,
+			DbType:              dbType,
+			I18nAuditResultInfo: auditResult.I18nAuditResultInfo,
+		}
+	}
+	return auditResults
 }
 
 func getComparisonStatement(c echo.Context) error {
---
+++ sqle/server/compare/database_compare_ee.go
@@ -0,0 +1,250 @@
+//go:build enterprise
+// +build enterprise
+
+package compare
+
+import (
+	"context"
+	"strings"
+
+	"github.com/actiontech/sqle/sqle/common"
+	driverV2 "github.com/actiontech/sqle/sqle/driver/v2"
+	"github.com/actiontech/sqle/sqle/log"
+	"github.com/actiontech/sqle/sqle/model"
+)
+
+type Compared struct {
+	BaseInstance     *model.Instance
+	ComparedInstance *model.Instance
+	ObjInfos         []*driverV2.DatabasCompareSchemaInfo
+}
+
+type SchemaObject struct {
+	BaseSchemaName       string
+	ComparisonSchemaName string
+	ComparedResult       string
+	DatabaseDiffObjects  []*DatabaseDiffObject
+	InconsistentNum      int
+}
+
+type DatabaseDiffObject struct {
+	InconsistentNum    int
+	ObjectType         string
+	ObjectsDiffResults []*ObjectDiffResult
+}
+
+type ObjectDiffResult struct {
+	ComparedResult string
+	ObjectName     string
+}
+
+const (
+	DatabaseStructSame               string = "same"                 // 对比一致
+	DatabaseStructInconsistent       string = "inconsistent"         // 对比有差异
+	DatabaseStructBaseNotExist       string = "base_not_exist"       // 基准对象不存在
+	DatabaseStructComparisonNotExist string = "comparison_not_exist" // 比对对象不存在
+)
+
+func (c *Compared) ExecDatabaseCompare() ([]*SchemaObject, error) {
+	baseInfos := make([]*driverV2.DatabasSchemaInfo, len(c.ObjInfos))
+	comparedInfos := make([]*driverV2.DatabasSchemaInfo, len(c.ObjInfos))
+	if len(c.ObjInfos) > 0 {
+		for i, objInfo := range c.ObjInfos {
+			baseInfos[i] = &driverV2.DatabasSchemaInfo{
+				SchemaName: objInfo.BaseSchemaName,
+			}
+			comparedInfos[i] = &driverV2.DatabasSchemaInfo{
+				SchemaName: objInfo.ComparedSchemaName,
+			}
+		}
+	}
+
+	basePlugin, err := common.NewDriverManagerWithoutAudit(log.NewEntry(), c.BaseInstance, "")
+	if err != nil {
+		return nil, err
+	}
+	defer basePlugin.Close(context.TODO())
+	baseRes, err := basePlugin.GetDatabaseObjectDDL(context.TODO(), baseInfos)
+	if err != nil {
+		return nil, err
+	}
+	comparedPlugin, err := common.NewDriverManagerWithoutAudit(log.NewEntry(), c.ComparedInstance, "")
+	if err != nil {
+		return nil, err
+	}
+	defer comparedPlugin.Close(context.TODO())
+	comparedRes, err := comparedPlugin.GetDatabaseObjectDDL(context.TODO(), comparedInfos)
+	if err != nil {
+		return nil, err
+	}
+	schemaObjects := CompareDDL(baseRes, comparedRes, c.ObjInfos)
+	return schemaObjects, err
+}
+
+// 根据对象名称对比基准数据源和对比数据源对象的差异
+// 1. 当比对数据源中对象在基准数据源对象中不存在时，比对结果为基准对象不存在
+// 2. 当基准数据源中对象在比对数据源对象中不存在时，比对结果为比对对象不存在
+// 3. 当基准数据源中对象和比对数据源中对象DDL比对一致时，比对结果为比对一致
+// 4. 当基准数据源中对象和比对数据源中对象DDL比对有差异时，比对结果为不一致
+func CompareDDL(baseRes, comparedRes []*driverV2.DatabaseSchemaObjectResult, originCompareObjects []*driverV2.DatabasCompareSchemaInfo) []*SchemaObject {
+	var schemaObjects []*SchemaObject
+
+	baseMap := make(map[string]*driverV2.DatabaseSchemaObjectResult)
+	comparedMap := make(map[string]*driverV2.DatabaseSchemaObjectResult)
+
+	for _, base := range baseRes {
+		baseMap[base.SchemaName] = base
+	}
+
+	for _, compared := range comparedRes {
+		comparedMap[compared.SchemaName] = compared
+	}
+
+	if len(originCompareObjects) > 0 {
+		// 对指定的schema进行对比
+		for _, obj := range originCompareObjects {
+			schemaObjects = append(schemaObjects, compareSchema(baseMap[obj.BaseSchemaName], comparedMap[obj.ComparedSchemaName]))
+		}
+	} else {
+		// 对指定的数据源进行对比
+		// 对基准数据源中的每个 schema 进行比较
+		for schemaName := range baseMap {
+			schemaObjects = append(schemaObjects, compareSchema(baseMap[schemaName], comparedMap[schemaName]))
+		}
+		// 对比对数据源中基准数据源没有的 schema 进行比较
+		for schemaName := range comparedMap {
+			if _, exists := baseMap[schemaName]; !exists {
+				schemaObjects = append(schemaObjects, compareSchema(nil, comparedMap[schemaName]))
+			}
+		}
+
+	}
+	return schemaObjects
+}
+
+func compareSchema(base, compared *driverV2.DatabaseSchemaObjectResult) *SchemaObject {
+	schemaObject := &SchemaObject{}
+	// 对基准和对比数据源进行nil判断，为了兼顾两个实例直接对比，可能会出现schema不存在的情况
+	if base != nil {
+		schemaObject.BaseSchemaName = base.SchemaName
+	} else {
+		schemaObject.ComparedResult = DatabaseStructBaseNotExist
+	}
+	if compared != nil {
+		schemaObject.ComparisonSchemaName = compared.SchemaName
+	} else if base != nil {
+		schemaObject.ComparedResult = DatabaseStructComparisonNotExist
+	}
+
+	diffObjectMap := make(map[string]*DatabaseDiffObject)
+
+	baseObjMap := make(map[struct{ Name, Type string }]*driverV2.DatabaseObjectDDL)
+	comparedObjMap := make(map[struct{ Name, Type string }]*driverV2.DatabaseObjectDDL)
+
+	if base != nil && base.DatabaseObjectDDLs != nil {
+		for _, obj := range base.DatabaseObjectDDLs {
+			baseObjMap[struct{ Name, Type string }{obj.DatabaseObject.ObjectName, obj.DatabaseObject.ObjectType}] = obj
+		}
+	}
+
+	if compared != nil && compared.DatabaseObjectDDLs != nil {
+		for _, obj := range compared.DatabaseObjectDDLs {
+			comparedObjMap[struct{ Name, Type string }{obj.DatabaseObject.ObjectName, obj.DatabaseObject.ObjectType}] = obj
+		}
+	}
+
+	// 对基准数据源中的每个对象进行比较
+	for objName, baseObj := range baseObjMap {
+		if comparedObj, exists := comparedObjMap[objName]; exists {
+			// 比对数据源中也存在相同的对象，则进行 DDL 对比
+			diffObj := compareObjects(baseObj, comparedObj)
+			updateDiffObjectMap(diffObjectMap, baseObj.DatabaseObject.ObjectType, diffObj)
+		} else {
+			// 比对数据源中不存在该对象
+			updateDiffObjectMap(diffObjectMap, baseObj.DatabaseObject.ObjectType, &DatabaseDiffObject{
+				InconsistentNum: 1,
+				ObjectType:      baseObj.DatabaseObject.ObjectType,
+				ObjectsDiffResults: []*ObjectDiffResult{
+					{
+						ComparedResult: DatabaseStructComparisonNotExist,
+						ObjectName:     strings.Trim(objName.Name, "`"),
+					},
+				},
+			})
+		}
+	}
+
+	// 检查比对数据源中有但基准数据源中不存在的对象
+	for objName, comparedObj := range comparedObjMap {
+		if _, exists := baseObjMap[objName]; !exists {
+			updateDiffObjectMap(diffObjectMap, comparedObj.DatabaseObject.ObjectType, &DatabaseDiffObject{
+				InconsistentNum: 1,
+				ObjectType:      comparedObj.DatabaseObject.ObjectType,
+				ObjectsDiffResults: []*ObjectDiffResult{
+					{
+						ComparedResult: DatabaseStructBaseNotExist,
+						ObjectName:     strings.Trim(objName.Name, "`"),
+					},
+				},
+			})
+		}
+	}
+
+	// 收集最终结果并计算不一致数量
+	var diffObjects []*DatabaseDiffObject
+	inconsistentCount := 0
+	for _, diffObj := range diffObjectMap {
+		diffObjects = append(diffObjects, diffObj)
+		inconsistentCount += diffObj.InconsistentNum
+	}
+
+	// 填充对比结果和不一致的数量
+	schemaObject.DatabaseDiffObjects = diffObjects
+	schemaObject.InconsistentNum = inconsistentCount
+	if schemaObject.ComparedResult == "" {
+		if inconsistentCount == 0 {
+			schemaObject.ComparedResult = DatabaseStructSame
+		} else {
+			schemaObject.ComparedResult = DatabaseStructInconsistent
+		}
+	}
+
+	return schemaObject
+}
+
+// 比较两个对象的 DDL，如果相同则返回对比一致，否则返回对比有差异
+func compareObjects(baseObj, comparedObj *driverV2.DatabaseObjectDDL) *DatabaseDiffObject {
+	diffObject := &DatabaseDiffObject{
+		ObjectType: baseObj.DatabaseObject.ObjectType,
+	}
+	var comparedResult string
+	if baseObj.ObjectDDL == comparedObj.ObjectDDL {
+		// DDL相同
+		comparedResult = DatabaseStructSame
+	} else {
+		// DDL不同，增加不一致计数
+		comparedResult = DatabaseStructInconsistent
+		diffObject.InconsistentNum++
+	}
+
+	diffObject.ObjectsDiffResults = []*ObjectDiffResult{
+		{
+			ComparedResult: comparedResult,
+			ObjectName:     strings.Trim(baseObj.DatabaseObject.ObjectName, "`"),
+		},
+	}
+
+	return diffObject
+}
+
+// 将比较结果更新到 diffObjectMap 中，并根据对象类型进行聚合
+func updateDiffObjectMap(diffObjectMap map[string]*DatabaseDiffObject, objectType string, diffObj *DatabaseDiffObject) {
+	if existingDiffObj, exists := diffObjectMap[objectType]; exists {
+		// 如果该对象类型已存在于 map 中，累加不一致数量，并追加对比结果
+		existingDiffObj.InconsistentNum += diffObj.InconsistentNum
+		existingDiffObj.ObjectsDiffResults = append(existingDiffObj.ObjectsDiffResults, diffObj.ObjectsDiffResults...)
+	} else {
+		// 如果不存在，则直接将当前对比结果加入 map
+		diffObjectMap[objectType] = diffObj
+	}
+}
