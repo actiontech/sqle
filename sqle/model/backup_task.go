4972a41f5fa44e04db265eccc3f1e1fecde9cc7e
WinfredLIN <linxiaotao1998@live.com>
2024-11-21 14:51:57 +0800

    ce model: update backup task table definition

--- sqle/model/backup_task.go
+++ sqle/model/backup_task.go
@@ -10,13 +10,13 @@ func init() {
 
 type BackupTask struct {
 	gorm.Model
-	TaskId            uint   `gorm:"column:task_id"`                                                                                 // 备份任务关联原始SQL的任务的id
-	InstanceId        uint64 `gorm:"column:instance_id"`                                                                             // 备份任务关联原始SQL对应的数据源
-	ExecuteSqlId      uint   `gorm:"index;column:execute_sql_id"`                                                                    // 备份任务关联原始SQL的id
-	BackupStrategy    string `gorm:"type:enum('none','reverse_sql','original_row','manual');column:backup_strategy;size:20"`         // 备份任务支持的备份策略
-	BackupStrategyTip string `gorm:"column:backup_strategy_tip;size:255"`                                                            // 推荐备份任务的原因
-	BackupStatus      string `gorm:"type:enum('waiting_for_execution','executing','failed','succeed');column:backup_status;size:20"` // 备份任务的执行状态
-	BackupExecInfo    string `gorm:"column:backup_exec_info;size:255"`                                                               // 备份任务的执行信息，如错误原因
-	SchemaName        string `gorm:"column:schema_name;size:50"`                                                                     // 备份的SQL对应的schema
-	TableName         string `gorm:"column:table_name;size:50"`                                                                      // 备份的SQL对应的table
+	TaskId            uint   `gorm:"column:task_id;not null"`                                                                         // 备份任务关联原始SQL的任务的id
+	InstanceId        uint64 `gorm:"column:instance_id;not null"`                                                                     // 备份任务关联原始SQL对应的数据源
+	ExecuteSqlId      uint   `gorm:"index;column:execute_sql_id;not null"`                                                            // 备份任务关联原始SQL的id
+	BackupStrategy    string `gorm:"type:enum('none','reverse_sql','original_row','manual');column:backup_strategy;size:20;not null"` // 备份任务支持的备份策略
+	BackupStrategyTip string `gorm:"column:backup_strategy_tip;size:255;not null;default:''"`                                         // 推荐备份任务的原因
+	BackupStatus      string `gorm:"type:enum('waiting_for_execution','executing','failed','succeed');column:backup_status;size:20"`  // 备份任务的执行状态
+	BackupExecResult  string `gorm:"column:backup_exec_result;size:255;not null;default:''"`                                          // 备份任务的执行结果
+	SchemaName        string `gorm:"column:schema_name;size:50;not null;default:''"`                                                  // 备份的SQL对应的schema
+	TableName         string `gorm:"column:table_name;size:50;not null;default:''"`                                                   // 备份的SQL对应的table
 }
