bd428eff7c763e6e8412b086086de7dd0b5d6e60
WinfredLIN <linxiaotao1998@live.com>
2024-11-21 10:20:25 +0800

    ce rename: origin to original

--- sqle/server/backup.go
+++ sqle/server/backup.go
@@ -7,11 +7,11 @@ type BackupTask interface {
 type BackupStrategy string
 
 const (
-	BackupStrategyNone       BackupStrategy = "none"        // 不备份(不支持备份、无需备份、选择不备份)
-	BackupStrategyReverseSql BackupStrategy = "reverse_sql" // 备份为反向SQL
-	BackupStrategyOriginRow  BackupStrategy = "origin_row"  // 备份为原始行
-	BackupStrategyManually   BackupStrategy = "manually"    // 标记为人工备份
-	BackupRowsAffectedLimit  int            = 1000          // SQL影响行数上限，超过该上限的SQL不进行备份
+	BackupStrategyNone        BackupStrategy = "none"         // 不备份(不支持备份、无需备份、选择不备份)
+	BackupStrategyReverseSql  BackupStrategy = "reverse_sql"  // 备份为反向SQL
+	BackupStrategyOriginalRow BackupStrategy = "original_row" // 备份为原始行
+	BackupStrategyManually    BackupStrategy = "manually"     // 标记为人工备份
+	BackupRowsAffectedLimit   int            = 1000           // SQL影响行数上限，超过该上限的SQL不进行备份
 )
 
 type BackupStatus string
