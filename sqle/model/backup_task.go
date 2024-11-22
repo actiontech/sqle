de885f241bfb691d21def0b3883769647b616ead
WinfredLIN <linxiaotao1998@live.com>
2024-11-22 17:37:25 +0800

    ce model: add index on task id

--- sqle/model/backup_task.go
+++ sqle/model/backup_task.go
@@ -10,7 +10,7 @@ func init() {
 
 type BackupTask struct {
 	gorm.Model
-	TaskId            uint   `gorm:"column:task_id;not null"`                                                                         // 备份任务关联原始SQL的任务的id
+	TaskId            uint   `gorm:"index;column:task_id;not null"`                                                                   // 备份任务关联原始SQL的任务的id
 	InstanceId        uint64 `gorm:"column:instance_id;not null"`                                                                     // 备份任务关联原始SQL对应的数据源
 	ExecuteSqlId      uint   `gorm:"index;column:execute_sql_id;not null"`                                                            // 备份任务关联原始SQL的id
 	BackupStrategy    string `gorm:"type:enum('none','reverse_sql','original_row','manual');column:backup_strategy;size:20;not null"` // 备份任务支持的备份策略
