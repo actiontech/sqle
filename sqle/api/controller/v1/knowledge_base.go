c0f72411932ba4f6e6b88656c1aed04ad565eb91
WinfredLIN <linxiaotao1998@live.com>
2025-02-14 10:35:48 +0800

    api definition:  GetKnowledgeGraph

--- sqle/api/controller/v1/knowledge_base.go
+++ sqle/api/controller/v1/knowledge_base.go
@@ -20,6 +20,7 @@ type GetKnowledgeBaseListRes struct {
 
 type KnowledgeBase struct {
 	ID          uint   `json:"id"`          // 知识库ID
+	RuleName    string `json:"rule_name"`   // 规则名称
 	Title       string `json:"title"`       // 标题
 	Description string `json:"description"` // 描述
 	Content     string `json:"content"`     // 内容
@@ -39,6 +40,8 @@ type Tag struct {
 // @Tags knowledge_base
 // @Param keywords query string false "keywords"
 // @Param tags query []string false "tags"
+// @Param page_index query uint32 true "page index"
+// @Param page_size query uint32 true "size of per page"
 // @Security ApiKeyAuth
 // @Success 200 {object} v1.GetKnowledgeBaseListRes
 // @router /v1/knowledge_bases [get]
@@ -62,4 +65,52 @@ type GetKnowledgeBaseTagListRes struct {
 // @router /v1/knowledge_bases/tags [get]
 func GetKnowledgeBaseTagList(c echo.Context) error {
 	return getKnowledgeBaseTagList(c)
+}
+
+// GetKnowledgeGraph
+// @Summary 获取知识库知识图谱
+// @Description get knowledge graph
+// @Id getKnowledgeGraph
+// @Tags knowledge
+// @Security ApiKeyAuth
+// @Success 200 {object} v1.GetKnowledgeGraphResp
+// @router /v1/knowledge_bases/graph [get]
+func GetKnowledgeGraph(c echo.Context) error {
+	return getKnowledgeGraph(c)
+}
+
+type GetKnowledgeGraphResp struct {
+	controller.BaseRes
+	Data *GraphResponse `json:"data"`
+}
+
+// NodeResponse represents a node in the API response
+type NodeResponse struct {
+	ID     string      `json:"id"`
+	Name   string      `json:"name"`
+	Weight uint64      `json:"weight"`
+	Data   interface{} `json:"data,omitempty"`
+}
+
+// EdgeResponse represents an edge in the API response
+type EdgeResponse struct {
+	FromID     string `json:"from_id"`     // 存储Node的ID而不是指针
+	FromName   string `json:"from_name"`   // 方便前端显示
+	ToID       string `json:"to_id"`       // 存储Node的ID而不是指针
+	ToName     string `json:"to_name"`     // 方便前端显示
+	Weight     uint64 `json:"weight"`      // 权重
+	IsDirected bool   `json:"is_directed"` // 是否有向
+}
+
+// GraphResponse represents the complete graph structure in the API response
+type GraphResponse struct {
+	Nodes []*NodeResponse `json:"nodes"` // 节点集合
+	Edges []*EdgeResponse `json:"edges"` // 边集合
+	Stats GraphStats      `json:"stats"` // 添加图的统计信息
+}
+
+// GraphStats 包含图的统计信息
+type GraphStats struct {
+	TotalNodes uint64 `json:"total_nodes"`
+	TotalEdges uint64 `json:"total_edges"`
 }
