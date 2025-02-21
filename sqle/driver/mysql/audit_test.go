541636b0c60d8acb47da03100ffb57e9c658f081
iwanghc <eternity5701@163.com>
2025-02-21 13:32:41 +0800

    fix: fix test dml check select rows

--- sqle/driver/mysql/audit_test.go
+++ sqle/driver/mysql/audit_test.go
@@ -6405,60 +6405,60 @@ func TestDMLCheckSelectRows(t *testing.T) {
 	runSingleRuleInspectCase(rule, t, "", inspect6, "select * from exist_tb_2 where user_id in (select v3 from exist_tb_3)", newTestResult().addResult(rulepkg.DMLCheckSelectRows))
 
 	inspect7 := NewMockInspect(e)
-	handler.ExpectQuery(regexp.QuoteMeta("select id, v1 as id from exist_tb_2 limit 10, 10")).
+	handler.ExpectQuery(regexp.QuoteMeta("select id, v1 from exist_tb_2 limit 10, 10")).
 		WillReturnRows(sqlmock.NewRows([]string{"type"}).AddRow(executor.ExplainRecordAccessTypeIndex).AddRow("range"))
-	handler.ExpectQuery(regexp.QuoteMeta("select count(*) from (SELECT 1 FROM `exist_tb_2` LIMIT 10,10) as t")).
+	handler.ExpectQuery(regexp.QuoteMeta("select count(*) from (select id, v1 from exist_tb_2 limit 10, 10) as t")).
 		WillReturnRows(sqlmock.NewRows([]string{"count(*)"}).AddRow("100000000"))
-	runSingleRuleInspectCase(rule, t, "", inspect7, "select id, v1 as id from exist_tb_2 limit 10, 10", newTestResult().addResult(rulepkg.DMLCheckSelectRows))
+	runSingleRuleInspectCase(rule, t, "", inspect7, "select id, v1 from exist_tb_2 limit 10, 10", newTestResult().addResult(rulepkg.DMLCheckSelectRows))
 
 	inspect8 := NewMockInspect(e)
-	handler.ExpectQuery(regexp.QuoteMeta("select id, v1 as id from exist_tb_2 group by id, v1")).
+	handler.ExpectQuery(regexp.QuoteMeta("select id, v1 from exist_tb_2 group by id, v1")).
 		WillReturnRows(sqlmock.NewRows([]string{"type"}).AddRow(executor.ExplainRecordAccessTypeIndex).AddRow("range"))
-	handler.ExpectQuery(regexp.QuoteMeta("select count(*) from (SELECT 1 FROM `exist_tb_2` GROUP BY `id`,`v1`) as t")).
+	handler.ExpectQuery(regexp.QuoteMeta("select count(*) from (select id, v1 from exist_tb_2 group by id, v1) as t")).
 		WillReturnRows(sqlmock.NewRows([]string{"count(*)"}).AddRow("100000000"))
-	runSingleRuleInspectCase(rule, t, "", inspect8, "select id, v1 as id from exist_tb_2 group by id, v1", newTestResult().addResult(rulepkg.DMLCheckSelectRows))
+	runSingleRuleInspectCase(rule, t, "", inspect8, "select id, v1 from exist_tb_2 group by id, v1", newTestResult().addResult(rulepkg.DMLCheckSelectRows))
 
 	inspect9 := NewMockInspect(e)
-	handler.ExpectQuery(regexp.QuoteMeta("select id, v1 as id from exist_tb_2 limit 10, 10")).
+	handler.ExpectQuery(regexp.QuoteMeta("select id, v1 from exist_tb_2 limit 10, 10")).
 		WillReturnRows(sqlmock.NewRows([]string{"type"}).AddRow(executor.ExplainRecordAccessTypeIndex).AddRow("range"))
-	handler.ExpectQuery(regexp.QuoteMeta("select count(*) from (SELECT 1 FROM `exist_tb_2` LIMIT 10,10) as t")).
+	handler.ExpectQuery(regexp.QuoteMeta("select count(*) from (select id, v1 from exist_tb_2 limit 10, 10) as t")).
 		WillReturnRows(sqlmock.NewRows([]string{"count(*)"}).AddRow("10"))
-	runSingleRuleInspectCase(rule, t, "", inspect9, "select id, v1 as id from exist_tb_2 limit 10, 10", newTestResult())
+	runSingleRuleInspectCase(rule, t, "", inspect9, "select id, v1 from exist_tb_2 limit 10, 10", newTestResult())
 
 	inspect10 := NewMockInspect(e)
-	handler.ExpectQuery(regexp.QuoteMeta("select id, v1 as id from exist_tb_2 group by id, v1")).
+	handler.ExpectQuery(regexp.QuoteMeta("select id, v1 from exist_tb_2 group by id, v1")).
 		WillReturnRows(sqlmock.NewRows([]string{"type"}).AddRow(executor.ExplainRecordAccessTypeIndex).AddRow("range"))
-	handler.ExpectQuery(regexp.QuoteMeta("select count(*) from (SELECT 1 FROM `exist_tb_2` GROUP BY `id`,`v1`) as t")).
+	handler.ExpectQuery(regexp.QuoteMeta("select count(*) from (select id, v1 from exist_tb_2 group by id, v1) as t")).
 		WillReturnRows(sqlmock.NewRows([]string{"count(*)"}).AddRow("10"))
-	runSingleRuleInspectCase(rule, t, "", inspect10, "select id, v1 as id from exist_tb_2 group by id, v1", newTestResult())
+	runSingleRuleInspectCase(rule, t, "", inspect10, "select id, v1 from exist_tb_2 group by id, v1", newTestResult())
 
 	inspect11 := NewMockInspect(e)
 	handler.ExpectQuery(regexp.QuoteMeta("select max(v1) from exist_tb_2 group by id")).
 		WillReturnRows(sqlmock.NewRows([]string{"type"}).AddRow(executor.ExplainRecordAccessTypeIndex).AddRow("range"))
-	handler.ExpectQuery(regexp.QuoteMeta("select count(*) from (SELECT 1 FROM `exist_tb_2` GROUP BY `id`) as t")).
+	handler.ExpectQuery(regexp.QuoteMeta("select count(*) from (select max(v1) from exist_tb_2 group by id) as t")).
 		WillReturnRows(sqlmock.NewRows([]string{"count(*)"}).AddRow("10"))
 	runSingleRuleInspectCase(rule, t, "", inspect11, "select max(v1) from exist_tb_2 group by id", newTestResult())
 
 	inspect12 := NewMockInspect(e)
 	handler.ExpectQuery(regexp.QuoteMeta("select max(v1) from exist_tb_2 group by id")).
 		WillReturnRows(sqlmock.NewRows([]string{"type"}).AddRow(executor.ExplainRecordAccessTypeIndex).AddRow("range"))
-	handler.ExpectQuery(regexp.QuoteMeta("select count(*) from (SELECT 1 FROM `exist_tb_2` GROUP BY `id`) as t")).
+	handler.ExpectQuery(regexp.QuoteMeta("select count(*) from (select max(v1) from exist_tb_2 group by id) as t")).
 		WillReturnRows(sqlmock.NewRows([]string{"count(*)"}).AddRow("10000000"))
 	runSingleRuleInspectCase(rule, t, "", inspect12, "select max(v1) from exist_tb_2 group by id", newTestResult().addResult(rulepkg.DMLCheckSelectRows))
 
 	inspect13 := NewMockInspect(e)
-	handler.ExpectQuery(regexp.QuoteMeta("select max(v1) as id, id from exist_tb_2 group by id")).
+	handler.ExpectQuery(regexp.QuoteMeta("select max(v1) as id from exist_tb_2 group by id")).
 		WillReturnRows(sqlmock.NewRows([]string{"type"}).AddRow(executor.ExplainRecordAccessTypeIndex).AddRow("range"))
-	handler.ExpectQuery(regexp.QuoteMeta("select count(*) from (SELECT 1 FROM `exist_tb_2` GROUP BY `id`) as t")).
+	handler.ExpectQuery(regexp.QuoteMeta("select count(*) from (select max(v1) as id from exist_tb_2 group by id) as t")).
 		WillReturnRows(sqlmock.NewRows([]string{"count(*)"}).AddRow("10"))
-	runSingleRuleInspectCase(rule, t, "", inspect13, "select max(v1) as id, id from exist_tb_2 group by id", newTestResult())
+	runSingleRuleInspectCase(rule, t, "", inspect13, "select max(v1) as id from exist_tb_2 group by id", newTestResult())
 
 	inspect14 := NewMockInspect(e)
-	handler.ExpectQuery(regexp.QuoteMeta("select max(v1) as id, id from exist_tb_2 group by id")).
+	handler.ExpectQuery(regexp.QuoteMeta("select max(v1) as id from exist_tb_2 group by id")).
 		WillReturnRows(sqlmock.NewRows([]string{"type"}).AddRow(executor.ExplainRecordAccessTypeIndex).AddRow("range"))
-	handler.ExpectQuery(regexp.QuoteMeta("select count(*) from (SELECT 1 FROM `exist_tb_2` GROUP BY `id`) as t")).
+	handler.ExpectQuery(regexp.QuoteMeta("select count(*) from (select max(v1) as id from exist_tb_2 group by id) as t")).
 		WillReturnRows(sqlmock.NewRows([]string{"count(*)"}).AddRow("10000000"))
-	runSingleRuleInspectCase(rule, t, "", inspect14, "select max(v1) as id, id from exist_tb_2 group by id", newTestResult().addResult(rulepkg.DMLCheckSelectRows))
+	runSingleRuleInspectCase(rule, t, "", inspect14, "select max(v1) as id from exist_tb_2 group by id", newTestResult().addResult(rulepkg.DMLCheckSelectRows))
 
 }
 
