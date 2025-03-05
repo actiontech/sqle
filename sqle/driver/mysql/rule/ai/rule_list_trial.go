9e9d650da92379ecd1576671d6d5b501d43b2d76
BugsGuru <lwqlwq888@gmail.com>
2025-03-05 19:06:19 +0800

    fix AI rules in trial

---
+++ sqle/driver/mysql/rule/ai/rule_list_trial.go
@@ -0,0 +1,12 @@
+//go:build trial
+// +build trial
+
+package ai
+
+import (
+	rulepkg "github.com/actiontech/sqle/sqle/driver/mysql/rule"
+)
+
+var sourceRuleHandlers []*rulepkg.SourceHandler
+
+func init() {}
