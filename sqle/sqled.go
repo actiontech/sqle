ba41d6abd76c548ba8d70179bdaa779f85d40626
yangzhongjiao <yangzhongjiao@gmail.com>
2025-03-20 03:32:46 +0000

    refactor(sqled): Adjust signal listening logic

--- sqle/sqled.go
+++ sqle/sqled.go
@@ -14,7 +14,6 @@ import (
 	knowledge_base "github.com/actiontech/sqle/sqle/server/knowledge_base"
 	optimizationRule "github.com/actiontech/sqle/sqle/server/optimization/rule"
 
-	// "github.com/actiontech/sqle/sqle/api/cloudbeaver_wrapper/service"
 	"github.com/actiontech/sqle/sqle/config"
 	"github.com/actiontech/sqle/sqle/driver"
 	"github.com/actiontech/sqle/sqle/log"
@@ -22,7 +21,6 @@ import (
 	"github.com/actiontech/sqle/sqle/server"
 	"github.com/actiontech/sqle/sqle/server/cluster"
 
-	opt "github.com/actiontech/sqle/sqle/server/optimization/rule"
 	"github.com/facebookgo/grace/gracenet"
 )
 
@@ -38,12 +36,10 @@ func Run(options *config.SqleOptions) error {
 	log.Logger().Infoln("starting sqled server")
 	defer log.Logger().Info("stop sqled server")
 
-	if sqleCnf.EnableClusterMode && options.ID == 0 {
-		return fmt.Errorf("server id is required on cluster mode")
-	}
-	if sqleCnf.EnableClusterMode && options.ReportHost == "" {
-		// TODO Check if the report host meets the format of ip:port
-		return fmt.Errorf("report host is required on cluster mode")
+	// validate config
+	err := validateConfig(options)
+	if err != nil {
+		return err
 	}
 
 	secretKey := options.SecretKey
@@ -59,33 +55,74 @@ func Run(options *config.SqleOptions) error {
 		}
 	}
 
-	defer driver.GetPluginManager().Stop()
-	if err := driver.GetPluginManager().Start(sqleCnf.PluginPath, options.Service.PluginConfig); err != nil {
-		return fmt.Errorf("init plugins error: %v", err)
-	}
+	// nofify singal
+	exitChan := make(chan struct{})
+	net := &gracenet.Net{}
+	go NotifySignal(exitChan, net)
 
-	// service.InitSQLQueryConfig(sqleCnf.SqleServerPort, sqleCnf.EnableHttps, config.Server.SQLQueryConfig)
-
-	dbConfig := options.Service.Database
-
-	dbPassword := dbConfig.Password
-	// Support using secret mysql password in sqled config, read secret_mysql_password first,
-	// but you can still use mysql_password to be compatible with older versions.
-	secretPassword := dbConfig.SecretPassword
-	if secretPassword != "" {
-		password, err := dmsCommonAes.AesDecrypt(secretPassword)
-		if err != nil {
-			return fmt.Errorf("read db info from config file error, %d", err)
+	// init plugins
+	{
+		defer driver.GetPluginManager().Stop()
+		if err := driver.GetPluginManager().Start(sqleCnf.PluginPath, options.Service.PluginConfig); err != nil {
+			return fmt.Errorf("init plugins error: %v", err)
 		}
-		dbPassword = password
 	}
 
-	s, err := model.NewStorage(dbConfig.User, dbPassword,
-		dbConfig.Host, dbConfig.Port, dbConfig.Schema, sqleCnf.DebugLog)
-	if err != nil {
-		return fmt.Errorf("get new storage failed: %v", err)
+	// init storage
+	{
+		dbConfig := options.Service.Database
+
+		dbPassword := dbConfig.Password
+		// Support using secret mysql password in sqled config, read secret_mysql_password first,
+		// but you can still use mysql_password to be compatible with older versions.
+		secretPassword := dbConfig.SecretPassword
+		if secretPassword != "" {
+			password, err := dmsCommonAes.AesDecrypt(secretPassword)
+			if err != nil {
+				return fmt.Errorf("read db info from config file error, %v", err)
+			}
+			dbPassword = password
+		}
+
+		s, err := model.NewStorage(dbConfig.User, dbPassword,
+			dbConfig.Host, dbConfig.Port, dbConfig.Schema, sqleCnf.DebugLog)
+		if err != nil {
+			return fmt.Errorf("get new storage failed: %v", err)
+		}
+		model.InitStorage(s)
+
+		if sqleCnf.AutoMigrateTable {
+			if err := s.AutoMigrate(); err != nil {
+				return fmt.Errorf("auto migrate table failed: %v", err)
+			}
+			err := s.CreateDefaultWorkflowTemplateIfNotExist()
+			if err != nil {
+				return fmt.Errorf("create workflow template failed: %v", err)
+			}
+			if err := s.CreateRuleCategoriesRelated(); err != nil {
+				return fmt.Errorf("create rule categories related failed while auto migrating table: %v", err)
+			}
+			rules := model.MergeOptimizationRules(driver.GetPluginManager().GetAllRules(), optimizationRule.OptimizationRuleMap)
+			if err := s.CreateRulesIfNotExist(rules); err != nil {
+				return fmt.Errorf("create rules failed while auto migrating table: %v", err)
+			}
+			if err := s.DeleteRulesIfNotExist(rules); err != nil {
+				return fmt.Errorf("delete rules failed while auto migrating table: %v", err)
+			}
+
+			if err := s.CreateDefaultTemplateIfNotExist(model.ProjectIdForGlobalRuleTemplate, driver.GetPluginManager().GetAllRules()); err != nil {
+				return fmt.Errorf("create default template failed while auto migrating table: %v", err)
+			}
+			if err := knowledge_base.LoadKnowledge(rules); err != nil {
+				return fmt.Errorf("LoadKnowledge failed : %v", err)
+			}
+		}
+		{
+			if err := s.CreateDefaultReportPushConfigIfNotExist(model.DefaultProjectUid); err != nil {
+				return fmt.Errorf("create default report push config failed: %v", err)
+			}
+		}
 	}
-	model.InitStorage(s)
 
 	err = dms.RegisterAsDMSTarget(options)
 	if err != nil {
@@ -93,41 +130,9 @@ func Run(options *config.SqleOptions) error {
 	}
 
 	if options.OptimizationConfig.OptimizationKey != "" && options.OptimizationConfig.OptimizationURL != "" {
-		// 配置了pawsql才会去初始化重写规则
 		optimizationRule.InitOptimizationRule()
 	}
-	if sqleCnf.AutoMigrateTable {
-		if err := s.AutoMigrate(); err != nil {
-			return fmt.Errorf("auto migrate table failed: %v", err)
-		}
-		err := s.CreateDefaultWorkflowTemplateIfNotExist()
-		if err != nil {
-			return fmt.Errorf("create workflow template failed: %v", err)
-		}
-		if err := s.CreateRuleCategoriesRelated(); err != nil {
-			return fmt.Errorf("create rule categories related failed while auto migrating table: %v", err)
-		}
-		rules := model.MergeOptimizationRules(driver.GetPluginManager().GetAllRules(), opt.OptimizationRuleMap)
-		if err := s.CreateRulesIfNotExist(rules); err != nil {
-			return fmt.Errorf("create rules failed while auto migrating table: %v", err)
-		}
-		if err := s.DeleteRulesIfNotExist(rules); err != nil {
-			return fmt.Errorf("delete rules failed while auto migrating table: %v", err)
-		}
 
-		if err := s.CreateDefaultTemplateIfNotExist(model.ProjectIdForGlobalRuleTemplate, driver.GetPluginManager().GetAllRules()); err != nil {
-			return fmt.Errorf("create default template failed while auto migrating table: %v", err)
-		}
-		if err := knowledge_base.LoadKnowledge(rules); err != nil {
-			return fmt.Errorf("LoadKnowledge failed : %v", err)
-		}
-	}
-	{
-		if err := s.CreateDefaultReportPushConfigIfNotExist(model.DefaultProjectUid); err != nil {
-			return fmt.Errorf("create default report push config failed: %v", err)
-		}
-	}
-	exitChan := make(chan struct{})
 	server.InitSqled(exitChan)
 
 	var node cluster.Node
@@ -145,28 +150,45 @@ func Run(options *config.SqleOptions) error {
 	jm.Start()
 	defer jm.Stop()
 
-	net := &gracenet.Net{}
-	go api.StartApi(net, exitChan, options, sqleSwaggerYaml)
+	api.StartApi(net, exitChan, options, sqleSwaggerYaml)
 
+	// Wait for exit signal from NotifySignal goroutine
+	<-exitChan
+	log.Logger().Infoln("sqled server will exit")
+	return nil
+}
+
+func validateConfig(options *config.SqleOptions) error {
+	sqleCnf := options.Service
+	if sqleCnf.EnableClusterMode {
+		if options.ID == 0 {
+			return fmt.Errorf("server id is required on cluster mode")
+		}
+		if options.ReportHost == "" {
+			return fmt.Errorf("report host is required on cluster mode")
+		}
+	}
+	return nil
+}
+
+func NotifySignal(exitChan chan struct{}, net *gracenet.Net) error {
 	killChan := make(chan os.Signal, 1)
 	// os.Kill is like kill -9 which kills a process immediately, can't be caught
 	signal.Notify(killChan, os.Interrupt, syscall.SIGTERM, syscall.SIGUSR2 /*graceful-shutdown*/)
-	select {
-	case <-exitChan:
-		log.Logger().Infoln("sqled server will exit")
-	case sig := <-killChan:
-		switch sig {
-		case syscall.SIGUSR2:
-			if pid, err := net.StartProcess(); nil != err {
-				log.Logger().Infof("Graceful restarted by signal SIGUSR2, but failed: %v", err)
-				return err
-			} else {
-				log.Logger().Infof("Graceful restarted, new pid is %v", pid)
-			}
-			log.Logger().Infof("old sqled exit")
-		default:
-			log.Logger().Infof("Exit by signal %v", sig)
+	sig := <-killChan
+	switch sig {
+	case syscall.SIGUSR2:
+		if pid, err := net.StartProcess(); nil != err {
+			log.Logger().Infof("Graceful restarted by signal SIGUSR2, but failed: %v", err)
+			return err
+		} else {
+			log.Logger().Infof("Graceful restarted, new pid is %v", pid)
 		}
+		log.Logger().Infof("old sqled exit")
+	default:
+		log.Logger().Infof("Exit by signal %v", sig)
 	}
+
+	close(exitChan)
 	return nil
 }
