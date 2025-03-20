4bb75c022e11b3ca248c417d860ccc086c8deff7
littleniannian <jarviscrick@outlook.com>
2025-03-20 17:53:36 +0800

    fix: implement td mysql distributed lock

--- sqle/server/auditplan/task_type_td_mysql_distributed_lock_ee.go
+++ sqle/server/auditplan/task_type_td_mysql_distributed_lock_ee.go
@@ -61,12 +61,25 @@ func (at *TDMySQLDistributedLock) HighPriorityParams() params.ParamsWithOperator
 // Metrics定义了采集任务的采集项
 func (at *TDMySQLDistributedLock) Metrics() []string {
 	return []string{
+		MetricNameGrantedLockTrxStarted,
+		MetricNameWaitingLockTrxWaitStarted,
+		MetricNameGrantedLockConnectionId,
+		MetricNameWaitingLockConnectionId,
+		MetricNameGrantedLockTrxId,
+		MetricNameWaitingLockTrxId,
+		MetricNameGrantedLockId,
+		MetricNameWaitingLockId,
+		MetricNameObjectName,
 		MetricNameEngine,
+		MetricNameDatabase,
 		MetricNameLockType,
 		MetricNameLockMode,
 		MetricNameLockStatus,
-		MetricNameTable,
 		MetricNameDBUser,
+		MetricNameHost,
+		MetricNameIndexType,
+		MetricNameGrantedLockSql,
+		MetricNameWaitingLockSql,
 	}
 }
 
@@ -127,10 +140,14 @@ func getInstanceOfAuditPlan(ctx context.Context, ap *AuditPlan) (*model.Instance
 
 func (at *TDMySQLDistributedLock) getLockSQL() string {
 	return `SELECT
+    granted_locks.ENGINE_LOCK_ID granted_lock_id,
+    waiting_locks.ENGINE_LOCK_ID waiting_lock_id,
     granted_locks.ENGINE AS engine,
-    granted_locks.OBJECT_SCHEMA AS schema_name,
+    granted_locks.OBJECT_SCHEMA AS database_name,
     granted_locks.OBJECT_NAME AS object_name,
-    granted_locks.INDEX_NAME AS index_name,
+    granted_lock_threads.PROCESSLIST_USER AS db_user,
+    granted_lock_threads.PROCESSLIST_HOST AS host,
+    granted_locks.INDEX_NAME AS index_type,
     granted_locks.LOCK_TYPE AS lock_type,
     granted_locks.LOCK_MODE AS lock_mode,
     granted_lock_threads.PROCESSLIST_INFO AS granted_lock_sql,
@@ -140,14 +157,15 @@ func (at *TDMySQLDistributedLock) getLockSQL() string {
     granted_locks.ENGINE_TRANSACTION_ID AS granted_lock_trx_id,
     waiting_locks.ENGINE_TRANSACTION_ID AS waiting_lock_trx_id,
     granted_trx.TRX_STARTED AS trx_started,
-    granted_trx.TRX_WAIT_STARTED AS trx_wait_started
-	FROM information_schema.innodb_trx granted_trx
-	JOIN performance_schema.data_locks granted_locks ON granted_trx.trx_id = granted_locks.ENGINE_TRANSACTION_ID
-	JOIN performance_schema.data_lock_waits lock_waits ON granted_locks.ENGINE_LOCK_ID = lock_waits.BLOCKING_ENGINE_LOCK_ID
-	JOIN performance_schema.data_locks waiting_locks ON lock_waits.REQUESTING_ENGINE_LOCK_ID = waiting_locks.ENGINE_LOCK_ID
-	JOIN performance_schema.threads granted_lock_threads ON granted_locks.THREAD_ID = granted_lock_threads.THREAD_ID
-	JOIN performance_schema.threads waiting_lock_threads ON waiting_locks.THREAD_ID = waiting_lock_threads.THREAD_ID
-	WHERE granted_locks.LOCK_STATUS = 'GRANTED';`
+    waiting_trx.TRX_WAIT_STARTED AS trx_wait_started
+FROM information_schema.innodb_trx granted_trx
+JOIN performance_schema.data_locks granted_locks ON granted_trx.trx_id = granted_locks.ENGINE_TRANSACTION_ID
+JOIN performance_schema.data_lock_waits lock_waits ON granted_locks.ENGINE_LOCK_ID = lock_waits.BLOCKING_ENGINE_LOCK_ID
+JOIN performance_schema.data_locks waiting_locks ON lock_waits.REQUESTING_ENGINE_LOCK_ID = waiting_locks.ENGINE_LOCK_ID
+JOIN information_schema.innodb_trx waiting_trx ON waiting_locks.ENGINE_TRANSACTION_ID = waiting_trx.trx_id
+JOIN performance_schema.threads granted_lock_threads ON granted_locks.THREAD_ID = granted_lock_threads.THREAD_ID
+JOIN performance_schema.threads waiting_lock_threads ON waiting_locks.THREAD_ID = waiting_lock_threads.THREAD_ID
+WHERE granted_locks.LOCK_STATUS = 'GRANTED';`
 }
 
 func (at *TDMySQLDistributedLock) convertToSQLV2(logger *logrus.Entry, ap *AuditPlan, resultRows []map[string]sql.NullString) []*SQLV2 {
@@ -159,20 +177,20 @@ func (at *TDMySQLDistributedLock) convertToSQLV2(logger *logrus.Entry, ap *Audit
 			ProjectId:   ap.ProjectId,
 			InstanceID:  ap.InstanceID,
 			AuditPlanId: strconv.FormatUint(uint64(ap.ID), 10),
-			SchemaName:  row["schema_name"].String,
+			SchemaName:  row[MetricNameDatabase].String,
 		}
 		layout := "2006-01-02T15:04:05Z"
-		metricNameTrxStartedStr := row[MetricNameTrxStarted].String
-		metricNameTrxWaitStartedStr := row[MetricNameTrxWaitStarted].String
+		metricNameTrxStartedStr := row[MetricNameGrantedLockTrxStarted].String
+		metricNameTrxWaitStartedStr := row[MetricNameWaitingLockTrxWaitStarted].String
 		metricNameTrxStarted, err := time.Parse(layout, metricNameTrxStartedStr)
 		// 这里暂时将锁的信息全部存储到info中
 		info := NewMetrics()
 		if err == nil {
-			info.SetTime(MetricNameTrxStarted, &metricNameTrxStarted)
+			info.SetTime(MetricNameGrantedLockTrxStarted, &metricNameTrxStarted)
 		}
 		metricNameTrxWaitStarted, err := time.Parse(layout, metricNameTrxWaitStartedStr)
 		if err == nil {
-			info.SetTime(MetricNameTrxWaitStarted, &metricNameTrxWaitStarted)
+			info.SetTime(MetricNameWaitingLockTrxWaitStarted, &metricNameTrxWaitStarted)
 		}
 		grantedLockConnectionId, err := strconv.ParseInt(row[MetricNameGrantedLockConnectionId].String, 10, 64)
 		if err == nil {
@@ -190,13 +208,17 @@ func (at *TDMySQLDistributedLock) convertToSQLV2(logger *logrus.Entry, ap *Audit
 		if err == nil {
 			info.SetInt(MetricNameWaitingLockTrxId, waitingLockTrxId)
 		}
+		info.SetString(MetricNameGrantedLockId, row[MetricNameGrantedLockId].String)
+		info.SetString(MetricNameWaitingLockId, row[MetricNameWaitingLockId].String)
 		info.SetString(MetricNameObjectName, row[MetricNameObjectName].String)
 		info.SetString(MetricNameEngine, row[MetricNameEngine].String)
+		info.SetString(MetricNameDatabase, row[MetricNameDatabase].String)
 		info.SetString(MetricNameLockType, row[MetricNameLockType].String)
 		info.SetString(MetricNameLockMode, row[MetricNameLockMode].String)
 		info.SetString(MetricNameLockStatus, row[MetricNameLockStatus].String)
 		info.SetString(MetricNameDBUser, row[MetricNameDBUser].String)
-		info.SetString(MetricNameIndexName, row[MetricNameIndexName].String)
+		info.SetString(MetricNameHost, row[MetricNameHost].String)
+		info.SetString(MetricNameIndexType, row[MetricNameIndexType].String)
 		info.SetString(MetricNameGrantedLockSql, row[MetricNameGrantedLockSql].String)
 		info.SetString(MetricNameWaitingLockSql, row[MetricNameWaitingLockSql].String)
 		sqlV2.Info = info
@@ -218,20 +240,44 @@ func (at *TDMySQLDistributedLock) Audit(sqls []*model.SQLManageRecord) (*AuditRe
 func (at *TDMySQLDistributedLock) Head(ap *AuditPlan) []Head {
 	return []Head{
 		{
-			Name: MetricNameEngine,
-			Desc: locale.ApMetricEngine,
+			Name: MetricNameGrantedLockId,
+			Desc: locale.ApMetricNameGrantedLockId,
 		},
 		{
-			Name: "schema",
-			Desc: locale.ApSchema,
+			Name: MetricNameGrantedLockTrxId,
+			Desc: locale.ApMetricNameGrantedLockTrxId,
 		},
 		{
-			Name: MetricNameObjectName,
-			Desc: locale.ApMetricNameTable,
+			Name: MetricNameGrantedLockTrxStarted,
+			Desc: locale.ApMetricNameTrxStarted,
 		},
 		{
-			Name: MetricNameIndexName,
-			Desc: locale.ApMetricNameIndexName,
+			Name: MetricNameGrantedLockConnectionId,
+			Desc: locale.ApMetricNameGrantedLockConnectionId,
+		},
+		{
+			Name: MetricNameGrantedLockSql,
+			Desc: locale.ApMetricNameGrantedLockSql,
+		},
+		{
+			Name: MetricNameWaitingLockId,
+			Desc: locale.ApMetricNameWaitingLockId,
+		},
+		{
+			Name: MetricNameWaitingLockTrxId,
+			Desc: locale.ApMetricNameWaitingLockTrxId,
+		},
+		{
+			Name: MetricNameWaitingLockTrxWaitStarted,
+			Desc: locale.ApMetricNameTrxWaitStarted,
+		},
+		{
+			Name: MetricNameWaitingLockConnectionId,
+			Desc: locale.ApMetricNameWaitingLockConnectionId,
+		},
+		{
+			Name: MetricNameWaitingLockSql,
+			Desc: locale.ApMetricNameWaitingLockSql,
 		},
 		{
 			Name: MetricNameLockType,
@@ -242,47 +288,87 @@ func (at *TDMySQLDistributedLock) Head(ap *AuditPlan) []Head {
 			Desc: locale.ApMetricNameLockMode,
 		},
 		{
-			Name: MetricNameGrantedLockConnectionId,
-			Desc: locale.ApMetricNameGrantedLockConnectionId,
+			Name: MetricNameDatabase,
+			Desc: locale.ApDatabase,
 		},
 		{
-			Name: MetricNameWaitingLockConnectionId,
-			Desc: locale.ApMetricNameWaitingLockConnectionId,
+			Name: MetricNameObjectName,
+			Desc: locale.ApMetricNameTable,
 		},
 		{
-			Name: MetricNameGrantedLockTrxId,
-			Desc: locale.ApMetricNameGrantedLockTrxId,
+			Name: MetricNameHost,
+			Desc: locale.ApMetricNameHost,
 		},
 		{
-			Name: MetricNameWaitingLockTrxId,
-			Desc: locale.ApMetricNameWaitingLockTrxId,
+			Name: MetricNameDBUser,
+			Desc: locale.ApMetricNameDBUser,
 		},
 		{
-			Name: MetricNameTrxStarted,
-			Desc: locale.ApMetricNameTrxStarted,
-		},
-		{
-			Name: MetricNameTrxWaitStarted,
-			Desc: locale.ApMetricNameTrxWaitStarted,
-		},
-		{
-			Name: MetricNameGrantedLockSql,
-			Desc: locale.ApMetricNameGrantedLockSql,
-		},
-		{
-			Name: MetricNameWaitingLockSql,
-			Desc: locale.ApMetricNameWaitingLockSql,
+			Name: MetricNameEngine,
+			Desc: locale.ApMetricEngine,
 		},
 	}
 }
 
+func (at *TDMySQLDistributedLock) Filters(ctx context.Context, logger *logrus.Entry, ap *AuditPlan, persist *model.Storage) []FilterMeta {
+	return []FilterMeta{
+		{
+			Name:            MetricNameLockType,
+			Desc:            locale.ApMetricNameLockType,
+			FilterInputType: FilterInputTypeString,
+			FilterOpType:    FilterOpTypeEqual,
+			FilterTips:      retrieveFilterTips(MetricNameLockType, persist, ap, logger),
+		},
+		{
+			Name:            MetricNameDatabase,
+			Desc:            locale.ApDatabase,
+			FilterInputType: FilterInputTypeString,
+			FilterOpType:    FilterOpTypeEqual,
+			FilterTips:      retrieveFilterTips(MetricNameDatabase, persist, ap, logger),
+		},
+		{
+			Name:            MetricNameObjectName,
+			Desc:            locale.ApMetricNameTable,
+			FilterInputType: FilterInputTypeString,
+			FilterOpType:    FilterOpTypeEqual,
+			FilterTips:      retrieveFilterTips(MetricNameObjectName, persist, ap, logger),
+		},
+	}
+}
+
+func retrieveFilterTips(filterType string, persist *model.Storage, ap *AuditPlan, logger *logrus.Entry) []FilterTip {
+	filterTypes := make([]string, 0)
+	var err error
+	if filterType == MetricNameLockType {
+		filterTypes, err = persist.SelectDistinctLockType(ap.ID, ap.InstanceAuditPlanId)
+	} else if filterType == MetricNameDatabase {
+		filterTypes, err = persist.SelectDistinctDatabase(ap.ID, ap.InstanceAuditPlanId)
+	} else if filterType == MetricNameObjectName {
+		filterTypes, err = persist.SelectDistinctObjectName(ap.ID, ap.InstanceAuditPlanId)
+	} else {
+		logger.Warnf("retrieve filter tips failed, filter type: %s is not supported", filterType)
+		return []FilterTip{}
+	}
+	if err != nil {
+		logger.Warnf("retrieve filter tips failed, err: %v", err)
+		return []FilterTip{}
+	}
+	filterTips := make([]FilterTip, 0, len(filterTypes))
+	for _, lockType := range filterTypes {
+		filterTips = append(filterTips, FilterTip{
+			Value: lockType,
+		})
+	}
+	return filterTips
+}
+
 // GetSQLData 获取分布式锁结果集接口
 func (at *TDMySQLDistributedLock) GetSQLData(ctx context.Context, ap *AuditPlan, persist *model.Storage, filters []Filter, orderBy string, isAsc bool, limit, offset int) ([]map[string] /* head name */ string, uint64, error) {
-	dataLocks, err := persist.GetDataLockList(limit, offset)
+	dataLocks, err := persist.GetDataLockList(genDataLockFilters(filters), limit, offset, ap.ID, ap.InstanceAuditPlanId)
 	if err != nil {
 		return nil, 0, err
 	}
-	count, err := persist.CountDataLock()
+	count, err := persist.CountDataLock(genDataLockFilters(filters), ap.ID, ap.InstanceAuditPlanId)
 	if err != nil {
 		return nil, 0, err
 	}
@@ -298,21 +384,32 @@ func (at *TDMySQLDistributedLock) GetSQLData(ctx context.Context, ap *AuditPlan,
 			trxWaitStarted = lock.TrxWaitStarted.Format(layout)
 		}
 		rows = append(rows, map[string]string{
-			MetricNameEngine:                  lock.Engine,
-			"schema":                          lock.Schema,
-			MetricNameObjectName:              lock.ObjectName,
-			MetricNameIndexName:               lock.IndexName,
-			MetricNameLockType:                lock.LockType,
-			MetricNameLockMode:                lock.LockMode,
-			MetricNameGrantedLockConnectionId: strconv.FormatInt(lock.GrantedLockConnectionId, 10),
-			MetricNameWaitingLockConnectionId: strconv.FormatInt(lock.WaitingLockConnectionId, 10),
-			MetricNameGrantedLockTrxId:        strconv.FormatInt(lock.GrantedLockTrxId, 10),
-			MetricNameWaitingLockTrxId:        strconv.FormatInt(lock.WaitingLockTrxId, 10),
-			MetricNameGrantedLockSql:          lock.GrantedLockSql,
-			MetricNameWaitingLockSql:          lock.WaitingLockSql,
-			MetricNameTrxStarted:              trxStarted,
-			MetricNameTrxWaitStarted:          trxWaitStarted,
+			MetricNameGrantedLockId:             lock.GrantedLockId,
+			MetricNameWaitingLockId:             lock.WaitingLockId,
+			MetricNameEngine:                    lock.Engine,
+			MetricNameDBUser:                    lock.DbUser,
+			MetricNameHost:                      lock.Host,
+			MetricNameDatabase:                  lock.DatabaseName,
+			MetricNameObjectName:                lock.ObjectName,
+			MetricNameLockType:                  lock.LockType,
+			MetricNameLockMode:                  lock.LockMode,
+			MetricNameGrantedLockConnectionId:   strconv.FormatInt(lock.GrantedLockConnectionId, 10),
+			MetricNameWaitingLockConnectionId:   strconv.FormatInt(lock.WaitingLockConnectionId, 10),
+			MetricNameGrantedLockTrxId:          strconv.FormatInt(lock.GrantedLockTrxId, 10),
+			MetricNameWaitingLockTrxId:          strconv.FormatInt(lock.WaitingLockTrxId, 10),
+			MetricNameGrantedLockSql:            lock.GrantedLockSql,
+			MetricNameWaitingLockSql:            lock.WaitingLockSql,
+			MetricNameGrantedLockTrxStarted:     trxStarted,
+			MetricNameWaitingLockTrxWaitStarted: trxWaitStarted,
 		})
 	}
 	return rows, uint64(count), nil
+}
+
+func genDataLockFilters(filters []Filter) map[string]string {
+	dataLockFilters := make(map[string]string, len(filters))
+	for _, filter := range filters {
+		dataLockFilters[filter.Name] = filter.FilterComparisonValue
+	}
+	return dataLockFilters
 }
