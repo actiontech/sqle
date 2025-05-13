94a2cf0301c38001dc32948fcd31d9f52a7b3a5b
littleniannian <jarviscrick@outlook.com>
2025-05-13 18:19:43 +0800

    feat(model): 添加实例审计计划物理读取页和逻辑读取页指标

--- sqle/model/instance_audit_plan_list.go
+++ sqle/model/instance_audit_plan_list.go
@@ -179,6 +179,8 @@ var OrderByMap = map[string] /* field name */ string /* field name with table*/ 
 	"lock_wait_counter":       "audit_plan_sqls.info->'$.lock_wait_counter'",
 	"act_wait_time_total":     "audit_plan_sqls.info->'$.act_wait_time_total'",
 	"act_time_total":          "audit_plan_sqls.info->'$.act_time_total'",
+	"phy_read_page_total":     "audit_plan_sqls.info->'$.phy_read_page_total'",
+	"logic_read_page_total":   "audit_plan_sqls.info->'$.logic_read_page_total'",
 }
 
 var instanceAuditPlanSQLQueryTpl = `
