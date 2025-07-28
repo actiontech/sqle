6c2f9f6c140ce8b001c9b04395b8875ff30e93a9
BugsGuru <lwqlwq888@gmail.com>
2025-07-28 16:52:38 +0800

    feat(server): add job to clean expired SQL insight records
    - Implement CleanExpiredSqlManageInsightRecords function in CleanJob
    - Add RemoveExpiredSqlInsightRecord method to Storage
    - Integrate new cleaning job into existing cleanup processes
    - Use existing GetSqlManageRawSqlExpiredHoursOrDefault method for expiration time

--- sqle/model/sql_manage_insight_ce.go
+++ sqle/model/sql_manage_insight_ce.go
@@ -3,6 +3,12 @@
 
 package model
 
+import "time"
+
 func (s *Storage) createSqlManageRawSQLs(sqls []*SQLManageRawSQL) error {
 	return nil
+}
+
+func (s *Storage) RemoveExpiredSqlInsightRecord(expiredTime time.Time) (int64, error) {
+	return 0, nil
 }
