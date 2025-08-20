87ec4cdc1202f145ff3bb7bb824977408d0a24ca
littleniannian <jarviscrick@outlook.com>
2025-08-20 18:32:20 +0800

    fix: upgrade dms version

--- vendor/github.com/actiontech/dms/pkg/dms-common/api/dms/v1/system_variable.go
+++ vendor/github.com/actiontech/dms/pkg/dms-common/api/dms/v1/system_variable.go
@@ -4,20 +4,24 @@ import base "github.com/actiontech/dms/pkg/dms-common/api/base/v1"
 
 // swagger:model UpdateSystemVariablesReqV1
 type UpdateSystemVariablesReqV1 struct {
-	Url                         *string `json:"url" form:"url" example:"http://10.186.61.32:8080" validate:"url"`
-	OperationRecordExpiredHours *int    `json:"operation_record_expired_hours" form:"operation_record_expired_hours" example:"2160"`
-	CbOperationLogsExpiredHours *int    `json:"cb_operation_logs_expired_hours" form:"cb_operation_logs_expired_hours" example:"2160"`
+	Url                                *string `json:"url" form:"url" example:"http://10.186.61.32:8080" validate:"url"`
+	OperationRecordExpiredHours        *int    `json:"operation_record_expired_hours" form:"operation_record_expired_hours" example:"2160"`
+	CbOperationLogsExpiredHours        *int    `json:"cb_operation_logs_expired_hours" form:"cb_operation_logs_expired_hours" example:"2160"`
+	SystemVariableSSHPrimaryKey        *string `json:"system_variable_ssh_primary_key"`
+	SystemVariableWorkflowExpiredHours *int    `json:"system_variable_workflow_expired_hours"`
 }
 
 // swagger:model GetSystemVariablesReply
 type GetSystemVariablesReply struct {
 	// Generic reply
 	base.GenericResp
-	Data    SystemVariablesResV1 `json:"data"`
+	Data SystemVariablesResV1 `json:"data"`
 }
 
 type SystemVariablesResV1 struct {
-	Url                         string `json:"url"`
-	OperationRecordExpiredHours int    `json:"operation_record_expired_hours"`
-	CbOperationLogsExpiredHours int    `json:"cb_operation_logs_expired_hours"`
+	Url                                string `json:"url"`
+	OperationRecordExpiredHours        int    `json:"operation_record_expired_hours"`
+	CbOperationLogsExpiredHours        int    `json:"cb_operation_logs_expired_hours"`
+	SystemVariableSSHPrimaryKey        string `json:"system_variable_ssh_primary_key"`
+	SystemVariableWorkflowExpiredHours int    `json:"system_variable_workflow_expired_hours"`
 }
