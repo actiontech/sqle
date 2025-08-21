594f548b92dcb1c7582ebcff1bfa24ac59bc7ccd
littleniannian <jarviscrick@outlook.com>
2025-08-21 10:49:48 +0800

    fix: upgrade vendor: dms

--- vendor/github.com/actiontech/dms/pkg/dms-common/api/dms/v1/system_variable.go
+++ vendor/github.com/actiontech/dms/pkg/dms-common/api/dms/v1/system_variable.go
@@ -4,7 +4,7 @@ import base "github.com/actiontech/dms/pkg/dms-common/api/base/v1"
 
 // swagger:model UpdateSystemVariablesReqV1
 type UpdateSystemVariablesReqV1 struct {
-	Url                                *string `json:"url" form:"url" example:"http://10.186.61.32:8080" validate:"url"`
+	Url                                *string `json:"url" form:"url" example:"http://10.186.61.32:8080" validate:"omitempty,url"`
 	OperationRecordExpiredHours        *int    `json:"operation_record_expired_hours" form:"operation_record_expired_hours" example:"2160"`
 	CbOperationLogsExpiredHours        *int    `json:"cb_operation_logs_expired_hours" form:"cb_operation_logs_expired_hours" example:"2160"`
 	SystemVariableSSHPrimaryKey        *string `json:"system_variable_ssh_primary_key"`
